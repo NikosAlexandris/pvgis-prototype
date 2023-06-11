639a93706d071c9b147126549dae5239cefc72a0
Nikos Alexandris <nikosalexandris@duck.com>
2023-06-11 23:24:22 +0300

    Add solar_geometry_constants.py
    - Function to calculate solar geometry constants for a given day in the year.
    - Attempt to translate the "original" com_par_const() function defined in rsun_base.c[pp]
    - Figured out that `com_par` stands likely for `compute_parameters` :-)

---
+++ pvgisprototype/solar_geometry_constants.py
@@ -0,0 +1,72 @@
+import logging
+
+from pydantic import BaseModel
+from pvgisprototype.data_structures import SunGeometryDayConstants
+from pvgisprototype.data_structures import GridGeometry
+from pvgisprototype.constants import EPS
+
+from math import acos, pi, fabs
+from typing import Optional
+import numpy as np
+
+
+# from :
+# function : com_par_const()
+def calculate_solar_geometry_constants(
+        local_solar_time: float,
+        time_offset: float = 0,
+        cosine_of_declination: float,
+        sine_of_declination: float,
+        grid_geometry: GridGeometry
+        EPS: float = 1e-5,  # Assume a default value for EPS
+) -> SunGeometryDayConstants:
+    """
+    Compute solar geometry constants for the day.
+
+    Parameters
+    ----------
+    local_solar_time : float
+        Longitude time.
+    cosine_of_declination : float
+        Cosine of the solar declination.
+    sine_of_declination : float
+        Sine of the solar declination.
+    grid_geometry : GridGeometry
+        Grid geometry constants.
+
+    Returns
+    -------
+    SunGeometryDayConstants
+        Sun geometry constants for the day.
+    """
+    solar_geometry_day_constants = SunGeometryDayConstants(
+            cosine_of_declination=cosine_of_declination,
+            sine_of_declination=sine_of_declination
+            )
+    solar_geometry_day_constants.lum_C11 = grid_geometry.sine_of_latitude * solar_geometry_day_constants.cosine_of_declination
+    solar_geometry_day_constants.lum_C13 = -grid_geometry.cosine_of_latitude * solar_geometry_day_constants.sine_of_declination
+    solar_geometry_day_constants.lum_C22 = solar_geometry_day_constants.cosine_of_declination
+    solar_geometry_day_constants.lum_C31 = grid_geometry.cosine_of_latitude * solar_geometry_day_constants.cosine_of_declination
+    solar_geometry_day_constants.lum_C33 = grid_geometry.sine_of_latitude * solar_geometry_day_constants.sine_of_declination
+
+    if fabs(solar_geometry_day_constants.lum_C31) >= EPS:
+        total_time_offset = time_offset + local_solar_time
+
+        pom = -solar_geometry_day_constants.lum_C33 / solar_geometry_day_constants.lum_C31
+        if fabs(pom) <= 1:
+            pom = acos(pom)
+            pom = (pom * 180) / pi
+            solar_geometry_day_constants.sunrise_time = (90 - pom) / 15 + 6
+            solar_geometry_day_constants.sunset_time = (pom - 90) / 15 + 18
+        else:
+            if pom < 0:
+                logging.warning('The sun is above the surface during the whole day')
+                solar_geometry_day_constants.sunrise_time = 0
+                solar_geometry_day_constants.sunset_time = 24
+            else:
+                logging.warning('The sun is below the surface during the whole day')
+                if fabs(pom) - 1 <= EPS:
+                    solar_geometry_day_constants.sunrise_time = 12
+                    solar_geometry_day_constants.sunset_time = 12
+
+    return solar_geometry_day_constants
