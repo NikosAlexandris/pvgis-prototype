c8e69795013871882bfafbab020205a4229cccfd
Nikos Alexandris <nikosalexandris@duck.com>
2023-06-13 02:17:05 +0300

    Update calculate_solar_declination()

--- pvgisprototype/solar_declination.py
+++ pvgisprototype/solar_declination.py
@@ -1,4 +1,5 @@
 import typer
+from typing_extensions import Annotated
 import math
 
 
@@ -12,28 +13,43 @@ app = typer.Typer(
 # from: rsun_base.cpp
 # function: com_declin(no_of_day)
 @app.callback(invoke_without_command=True, no_args_is_help=True)
+def calculate_solar_declination(
+        day_of_year: int,
+        days_in_a_year: float = 365.25,
+        orbital_eccentricity: float = 0.03344,
+        perigee_offset: float = 0.048869,
+        output_units: Annotated[str, typer.Option(
+            '-o',
+            '--output-units',
+            show_default=True,
+            case_sensitive=False,
+            help="Output units for solar declination (degrees or radians)")] = 'radians',
+        ) -> float:
     """Approximate the sun's declination for a given day of the year.
 
     The solar declination is the angle between the Sun's rays and the
-    equatorial plane of Earth. The formula uses the `asin` and `sin`
-    trigonometric functions to achieve this. 
+    equatorial plane of Earth. It varies throughout the year due to the tilt of
+    the Earth's axis and is an important parameter in determining the seasons
+    and the amount of solar radiation received at different latitudes.
 
     Parameters
     ----------
-    number_of_day (int): The day of the year (ranging from 1 to 365 or 366 in a leap year).
+    day_of_year: int
+        The day of the year (ranging from 1 to 365 or 366 in a leap year).
 
     Returns
     -------
-    float: The solar declination for the given day of the year.
+    float
+        The solar declination for the given day of the year.
 
     Notes
     -----
 
     - The function calculates the `proportion` of the way through the year (in
-      radians), which is given by `(2 * pi * number_of_day) / 365.25`.
+      radians), which is given by `(2 * pi * day_of_year) / 365.25`.
 
     - The `0.3978`, `1.4`, and `0.0355` are constants in the approximation
-      formula, wi the `0.0489` being an adjustment factor for the slight
+      formula, with the `0.0489` being an adjustment factor for the slight
       eccentricity of Earth's orbit.
   
     - Finally, the declination is negated (`declination = - declination`). This
@@ -46,9 +62,12 @@ app = typer.Typer(
       of solar position, comprehensive models like the Solar Position Algorithm
       (SPA) are typically used.
     """
-    declination = 2 * math.pi * number_of_day / 365.25
-    declination = math.asin(0.3978 * math.sin(declination - 1.4 + 0.0355 * math.sin(declination - 0.0489)))
-    declination = - declination
+    some_term = 2 * math.pi * day_of_year / days_in_a_year
+    declination = math.asin(0.3978 * math.sin(some_term - 1.4 + orbital_eccentricity * math.sin(some_term - perigee_offset)))
+    declination = - declination  # why? in the C source code: decl = - decl;
+    if output_units == 'degrees':
+        declination = math.degrees(declination)
 
     return declination
+
 
