2e906631df0502e81d933a601123e1fab9dddca1
Nikos Alexandris <nikosalexandris@duck.com>
2023-06-13 02:39:08 +0300

    Update and clean algorithm in calculate_solar_geometry_constants()
    Is it a correct one?

--- pvgisprototype/solar_geometry_constants.py
+++ pvgisprototype/solar_geometry_constants.py
@@ -41,34 +41,46 @@ def calculate_solar_geometry_constants(
     sine_of_solar_declination = np.sin(solar_declination)
     cosine_of_solar_declination = np.cos(solar_declination)
 
-    solar_geometry_day_constants = SolarGeometryDayConstants(
-            cosine_of_declination=cosine_of_declination,
-            sine_of_declination=sine_of_declination,
-    )
-    solar_geometry_day_constants.lum_C11 = grid_geometry.sine_of_latitude * solar_geometry_day_constants.cosine_of_declination
-    solar_geometry_day_constants.lum_C13 = -grid_geometry.cosine_of_latitude * solar_geometry_day_constants.sine_of_declination
-    solar_geometry_day_constants.lum_C22 = solar_geometry_day_constants.cosine_of_declination
-    solar_geometry_day_constants.lum_C31 = grid_geometry.cosine_of_latitude * solar_geometry_day_constants.cosine_of_declination
-    solar_geometry_day_constants.lum_C33 = grid_geometry.sine_of_latitude * solar_geometry_day_constants.sine_of_declination
+    lum_C11 = sine_of_latitude * cosine_of_solar_declination
+    lum_C13 = -cosine_of_latitude * sine_of_solar_declination
+    lum_C22 = cosine_of_solar_declination
+    lum_C31 = cosine_of_latitude * cosine_of_solar_declination
+    lum_C33 = sine_of_latitude * sine_of_solar_declination
 
-    if fabs(solar_geometry_day_constants.lum_C31) >= EPS:
+    sunrise_time = sunset_time = None
+
+    if fabs(lum_C31) >= EPS:
         total_time_offset = time_offset + local_solar_time
 
-        pom = -solar_geometry_day_constants.lum_C33 / solar_geometry_day_constants.lum_C31
+        pom = -lum_C33 / lum_C31
         if fabs(pom) <= 1:
             pom = acos(pom)
-            pom = (pom * 180) / pi
-            solar_geometry_day_constants.sunrise_time = (90 - pom) / 15 + 6
-            solar_geometry_day_constants.sunset_time = (pom - 90) / 15 + 18
+            pom = (pom * 180) / np.pi
+            sunrise_time = (90 - pom) / 15 + 6
+            sunset_time = (pom - 90) / 15 + 18
         else:
             if pom < 0:
                 logging.warning('The sun is above the surface during the whole day')
-                solar_geometry_day_constants.sunrise_time = 0
-                solar_geometry_day_constants.sunset_time = 24
+                sunrise_time = 0
+                sunset_time = 24
             else:
                 logging.warning('The sun is below the surface during the whole day')
                 if fabs(pom) - 1 <= EPS:
-                    solar_geometry_day_constants.sunrise_time = 12
-                    solar_geometry_day_constants.sunset_time = 12
+                    sunrise_time = 12
+                    sunset_time = 12
 
-    return solar_geometry_day_constants
+    return SolarGeometryDayConstants(
+        latitude=latitude,
+        # local_solar_time=local_solar_time,
+        solar_declination=solar_declination,
+        # time_offset=time_offset,
+        cosine_of_solar_declination=cosine_of_solar_declination,
+        sine_of_solar_declination=sine_of_solar_declination,
+        lum_C11=lum_C11,
+        lum_C13=lum_C13,
+        lum_C22=lum_C22,
+        lum_C31=lum_C31,
+        lum_C33=lum_C33,
+        sunrise_time=sunrise_time,
+        sunset_time=sunset_time,
+    )
