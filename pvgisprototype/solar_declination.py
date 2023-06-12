f5a9fd4e083b15d1df4c55982c0c2adbb43302a2
Nikos Alexandris <nikosalexandris@duck.com>
2023-06-13 02:17:48 +0300

    Add calculate_solar_declination_hargreaves()

--- pvgisprototype/solar_declination.py
+++ pvgisprototype/solar_declination.py
@@ -71,3 +71,35 @@ def calculate_solar_declination(
     return declination
 
 
+def calculate_solar_declination_hargreaves(
+        day_of_year: int,
+        days_in_a_year: float = 365.25,
+        ) -> float:
+    """Approximate the solar declination based on the Hargreaves formula.
+
+        δ = 23.45° × sin [360/365 × (284 + n + 0.4 × sin [360/365 × (n - 100)])]
+
+        Notes
+        -----
+
+        365.25: The number 365.25 represents the average number of days in a
+        year. This value is used to scale the orbital position of the Earth.
+
+        284: The number 284 represents a constant term added to the day of the
+        year. It adjusts the calculation to align with the Earth's position
+        during the winter solstice, which usually occurs around December 21st
+        (approximately 284 days into the year).
+
+        0.4: The number 0.4 is a constant that determines the amplitude of the
+        seasonal variation. It is multiplied by the second sine term to
+        modulate the seasonal change in the solar declination.
+
+        100: The number 100 represents an offset to the day of the year. It is
+        subtracted from the original day of the year before calculating the
+        second sine term. This offset helps adjust the timing of the seasonal
+        variation and is usually chosen to align with the summer solstice,
+        which typically occurs around June 21st.
+    """
+    declination = 23.45 * math.sin(math.radians(360/days_in_a_year * (284 + day_of_year + 0.4 * math.sin(math.radians(360/days_in_a_year * (day_of_year - 100))))))
+
+    return declination
