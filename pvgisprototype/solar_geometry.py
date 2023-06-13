8db9a2756e8602d6b108a0d6ae5f019cc06d83d1
Nikos Alexandris <nikosalexandris@duck.com>
2023-06-13 18:06:23 +0300

    Update solar_geometry.py
    Is it needed? Keep one for the CLI and set the others for constants and variables as API functions?

--- pvgisprototype/solar_geometry.py
+++ pvgisprototype/solar_geometry.py
@@ -1,19 +1,10 @@
 import logging
 from pvgisprototype.data_structures import SolarGeometryDayConstants
+from pvgisprototype.data_structures import SolarGeometryDayVariables
 import typer
 from typing import Annotated
 
 
-app = typer.Typer(
-    add_completion=False,
-    add_help_option=True,
-    help=f"PVGIS core CLI prototype",
-)
-
-
-# from :
-# function : com_par_const()
-@app.callback(invoke_without_command=True)
 def calculate_solar_geometry_constants(
         latitude: Annotated[float, typer.Argument(
             help='Latitude in decimal degrees, south is negative',
@@ -41,7 +32,7 @@ def calculate_solar_geometry_constants(
     SolarGeometryDayConstants
         Solar geometry constants for the day.
     """
-    solar_geometry_constants = SolarGeometryDayConstants.calculate_solar_geometry_constants(
+    solar_geometry_constants = calculate_solar_geometry_constants(
             latitude,
             local_solar_time,
             cosine_of_declination,
@@ -50,3 +41,40 @@ def calculate_solar_geometry_constants(
     typer.echo(solar_geometry_constants)
     return solar_geometry_constants
 
+
+def calculate_solar_geometry_variables(
+        solar_geometry_day_constants: SolarGeometryDayConstants,
+        year: int,
+        hour_of_year: int,
+        days_in_a_year: float = 365.25,
+        perigee_offset = 0.048869,
+        eccentricity = 0.01672,
+        hour_offset: float = 0,
+        output_units: Annotated[str, typer.Option(
+            '-o',
+            '--output-units',
+            show_default=True,
+            case_sensitive=False,
+            help="Output units for solar geometry variables (degrees or radians)")] = 'radians',
+        ) -> SolarGeometryDayVariables:
+    """Compute solar geometry variables for a given year and hour of year
+
+    Parameters
+    ----------
+    solar_geometry_day_constants : SolarGeometryDayConstants
+        The input solar geometry constants.
+    """
+    solar_geometry_constants = calculate_solar_geometry_constants(
+            latitude,
+            local_solar_time,
+            cosine_of_declination,
+            sine_of_declination,
+            )
+    solar_geometry_variables = calculate_solar_geometry_variables(
+            solar_geometry_day_constants,
+            year,
+            hour_of_year,
+            )
+
+    typer.echo(solar_geometry_variables)
+    return solar_geometry_variables
