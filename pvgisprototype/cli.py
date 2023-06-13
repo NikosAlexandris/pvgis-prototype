8450addaa8a320a2962c3d8d0e8839fbc9428901
Nikos Alexandris <nikosalexandris@duck.com>
2023-06-13 18:06:47 +0300

    Update cli.py

--- pvgisprototype/cli.py
+++ pvgisprototype/cli.py
@@ -19,7 +19,8 @@ from .time_series import app as timeseries
 from .utilities import app as utility
 from .solar_constant import app as solar_constant
 from .solar_geometry_constants import app as solar_geometry_constants
-
+from .solar_geometry_variables import app as solar_geometry_variables
+from .solar_position import app as solar_position
 
 state = {"verbose": False}
 
@@ -37,7 +38,9 @@ app.add_typer(tmy, name="tmy", help='Generate the Typical Meteorological Year')
 app.add_typer(timeseries, name="time-series", help='Retrieve time series of solar radiation and/or PV output power')
 app.add_typer(utility, name="utility", help='Diagnose data issues and more')
 app.add_typer(solar_constant, name="solar-constant", help='Compute the solar constant for a day of the year')
-app.add_typer(solar_geometry_constants, name="solar-geometry", help='Compute solar geometry constants for a day in a year')
+app.add_typer(solar_geometry_constants, name="geometry-constants", help='Compute solar geometry constants for a day in a year')
+app.add_typer(solar_geometry_variables, name="geometry-variables", help='Compute solar geometry variables for a day in a year')
+app.add_typer(solar_position, name="solar-position", help='Compute solar altitude and azimuth for a position and time in year and day')
 
 
 def _version_callback(value: bool) -> None:
