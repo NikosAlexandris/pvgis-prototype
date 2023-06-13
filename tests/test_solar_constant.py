2b65eccf5ee721f097133a4479c34c4312e29b91
Nikos Alexandris <nikosalexandris@duck.com>
2023-06-13 18:00:13 +0300

    Update test for solar constant

--- tests/test_solar_constant.py
+++ tests/test_solar_constant.py
@@ -3,21 +3,24 @@ from pvgisprototype.solar_constant import calculate_solar_constant
 from pvgisprototype.solar_constant_plot import plot_solar_constant
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 
 
-# Arbitrary test cases -- Replaced with correct values
 @pytest.mark.parametrize(
-    "day_in_year, expected", 
+    "day_of_year, expected", 
     [
-        (10, 1412.366252486348),
-        (182, 1321.44578929)  # Test for day 182, half the year, where the value should be minimum
+        (3, 1412.366252486348),   # Perihelion: solar constant should be maximum
+        (80, 1361.44578929),  # Vernal equinox
+        (183, 1321.44578929),  # Aphelion: solar constant should be minimum
+        (266, 1361.44578929),  # Autumnal equinox
+        (355, 1400.25687453),  # Close to winter solstice
+        (random.randint(1, 365), 1361.44578929)  # Random day of the year
     ]
 )
-def test_calculate_solar_constant(day_in_year: int, expected: float):
-    assert pytest.approx(calculate_solar_constant(day_in_year), 0.0001) == expected
+def test_calculate_solar_constant(day_of_year: int, expected: float):
+    assert pytest.approx(calculate_solar_constant(day_of_year), 0.0001) == expected
 
 
-# a decorator to instruct using the baseline image
-@pytest.mark.mpl_image_compare
+@pytest.mark.mpl_image_compare  # instructs use of a baseline image
 def test_solar_constant_plot():
     return plot_solar_constant()
