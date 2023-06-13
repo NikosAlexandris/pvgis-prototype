986ff8db38875ed84aff90c483c3387cdda41e35
Nikos Alexandris <nikosalexandris@duck.com>
2023-06-13 17:59:51 +0300

    Expand plot_solar_declination() and the test for solar declination

--- pvgisprototype/solar_declination_plot.py
+++ pvgisprototype/solar_declination_plot.py
@@ -2,10 +2,17 @@ from pvgisprototype.solar_declination import calculate_solar_declination
 from pvgisprototype.solar_declination import calculate_solar_declination_hargreaves
 import matplotlib.pyplot as plt
 import numpy as np
+from datetime import datetime
 
 
-def plot_solar_declination():
-    days = np.arange(1, 366)  # Days of the year
+def days_in_year(year):
+    start_date = datetime(year, 1, 1)  # First day of the year
+    end_date = datetime(year + 1, 1, 1)  # First day of the next year
+    return (end_date - start_date).days
+
+
+def plot_solar_declination(title: str = 'Annual Variation of Solar Declination'):
+    days = np.arange(1, 366)
     solar_declinations = np.vectorize(calculate_solar_declination)(days, output_units='degrees')  # Calculate solar declination for each day
     solar_declinations_hargreaves = np.vectorize(calculate_solar_declination_hargreaves)(days)  # Calculate solar declination for each day
 
@@ -14,10 +21,49 @@ def plot_solar_declination():
     plt.plot(days, solar_declinations_hargreaves, label='Hargreaves')
     plt.xlabel('Day of the Year')
     plt.ylabel('Solar Declination')
-    plt.title('Annual Variation of Solar Declination')
+    plt.title(title)
     plt.grid(True)
     plt.legend()
     plt.savefig('solar_declination.png')
     return fig
 
-plot_solar_declination()
+
+def plot_solar_declination_one_year(
+        year: int,
+        title: str = 'Annual Variation of Solar Declination',
+        ):
+    days = days_in_year(year)
+    solar_declinations = np.vectorize(calculate_solar_declination)(days, output_units='degrees')  # Calculate solar declination for each day
+    solar_declinations_hargreaves = np.vectorize(calculate_solar_declination_hargreaves)(days)  # Calculate solar declination for each day
+
+    fig = plt.figure(figsize=(10,6))
+    plt.plot(days, solar_declinations, label='PVGIS')
+    plt.plot(days, solar_declinations_hargreaves, label='Hargreaves')
+    plt.xlabel('Day of the Year')
+    plt.ylabel(f'Solar Declination in {year}')
+    plt.title(title)
+    plt.grid(True)
+    plt.legend()
+    plt.savefig('solar_declination.png')
+    return fig
+
+
+def plot_solar_declination_five_years(
+        start_year: int,
+        end_year: int,
+        title: str = 'Annual Variation of Solar Declination',
+        ):
+    days = [days_in_year(year) for year in np.arange(start_year, end_year)]
+    solar_declinations = np.vectorize(calculate_solar_declination)(days, output_units='degrees')  # Calculate solar declination for each day
+    solar_declinations_hargreaves = np.vectorize(calculate_solar_declination_hargreaves)(days)  # Calculate solar declination for each day
+
+    fig = plt.figure(figsize=(10,6))
+    plt.plot(days, solar_declinations, label='PVGIS')
+    plt.plot(days, solar_declinations_hargreaves, label='Hargreaves')
+    plt.xlabel('Day of the Year')
+    plt.ylabel('Solar Declination')
+    plt.title(title)
+    plt.grid(True)
+    plt.legend()
+    plt.savefig('solar_declination.png')
+    return fig
--- tests/test_solar_declination.py
+++ tests/test_solar_declination.py
@@ -1,23 +1,55 @@
 import pytest
 from pvgisprototype.solar_declination import calculate_solar_declination
 from pvgisprototype.solar_declination_plot import plot_solar_declination
+from pvgisprototype.solar_declination_plot import plot_solar_declination_one_year
+from pvgisprototype.solar_declination_plot import plot_solar_declination_five_years
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 
 
-# Arbitrary test cases -- Replaced with correct values
+"""
+The solar declination is the angle between the rays of the Sun and the plane of
+the Earth's equator. Its value varies between -23.44° and +23.44°
+(approximately) throughout the year due to the tilt of the Earth's axis.
+
+Verify expected declination for these days based on a given formula or lookup table
+"""
+
+
 @pytest.mark.parametrize(
-    "day_in_year, expected", 
+    "day_of_year, expected", 
     [
-        (10, 0.3833890030449887),
-        (182, -0.40343635275536255)  # Test for day 182, half the year, where the solar constant should be minimum
+        (1, -23.44),  # Around New Year
+        (79, 0),  # Around vernal equinox
+        (172, 23.44),  # Around summer solstice
+        (266, 0),  # Around autumnal equinox
+        (355, -23.44),  # Around winter solstice
     ]
 )
-def test_calculate_solar_declination(day_in_year: int, expected: float):
-    assert pytest.approx(calculate_solar_declination(day_in_year), 0.0001) == expected
+def test_calculate_solar_declination(day_of_year: int, expected: float):
+    # assert pytest.approx(calculate_solar_declination(day_of_year, output_units='radians'), 0.0001) == expected
+    # assert pytest.approx(calculate_solar_declination(day_of_year, output_units='degrees'), 1) == expected
+    assert pytest.approx(calculate_solar_declination(day_of_year, output_units='degrees'), 0.) == expected
+    # assert pytest.approx(calculate_solar_declination(day_of_year, output_units='degrees'), 0.1) == expected
+    # assert pytest.approx(calculate_solar_declination(day_of_year, output_units='degrees'), 0.01) == expected
+    # assert pytest.approx(calculate_solar_declination(day_of_year, output_units='degrees'), 0.001) == expected
+    # assert pytest.approx(calculate_solar_declination(day_of_year, output_units='degrees'), 0.0001) == expected
 
 
-# a decorator to instruct using the baseline image
-@pytest.mark.mpl_image_compare
-def test_solar_declination_plot():
-    return plot_solar_declination()
+# @pytest.mark.mpl_image_compare  # instructs use of a baseline image
+# def test_solar_declination_plot():
+#     return plot_solar_declination()
+
+
+# Set a seed to ensure agreement of plots between tests!
+random.seed(43) # Comment to really pick a random year
+random_year = random.randint(2005, 2023)
+@pytest.mark.mpl_image_compare  # instructs use of a baseline image
+def test_solar_declination_plot_one_year():
+    return plot_solar_declination_one_year(year=random_year, title=f'Solar Declination {random_year}')
+
+
+# @pytest.mark.mpl_image_compare
+# def test_solar_declination_plot_five_years():
+#     return plot_solar_declination(start_year=2018, end_year=2022, title='Solar Declination 2018-2022')
