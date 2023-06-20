5e2d031a2550c884efb1142d2a8c0b3940a1c5e7
Nikos Alexandris <nikosalexandris@duck.com>
2023-06-20 16:50:42 +0300

    Update irradiance.py

--- pvgisprototype/irradiance.py
+++ pvgisprototype/irradiance.py
@@ -2,8 +2,10 @@ import typer
 from typing_extensions import Annotated
 from typing import Optional
 from .direct_irradiance import app as direct_irradiance
-# from .diffuse_irradiance import app as diffuse_irradiance
-# from .reflected_irradiance import app as reflected_irradiance
+from .diffuse_irradiance import app as diffuse_irradiance
+from .reflected_irradiance import app as reflected_irradiance
+from .extraterrestrial_irradiance import app as extraterrestrial_irradiance
+from .angular_loss_factor import app as angular_loss_factor
 
 
 app = typer.Typer(
@@ -12,6 +14,8 @@ app = typer.Typer(
     help=f"Calculate solar irradiance",
 )
 
-app.add_typer(direct_irradiance, name="direct", help='Estimate the direct direct radiation')
-# app.add_typer(diffuse, name="diffuse", help='Estimate the direct diffuse radiation')
-# app.add_typer(reflected, name="reflected", help='Estimate the direct reflected radiation')
+app.add_typer(direct_irradiance, name="direct", help='Estimate the direct irradiance', no_args_is_help=True)
+app.add_typer(diffuse_irradiance, name="diffuse", help='Estimate the diffuse irradiance', no_args_is_help=True)
+app.add_typer(reflected_irradiance, name="reflected", help='Estimate the direct reflected radiation', no_args_is_help=True)
+app.add_typer(extraterrestrial_irradiance, name="extraterrestrial-irradiance", help='Compute the solar constant for a day of the year', no_args_is_help=True)
+app.add_typer(angular_loss_factor, name="angular-loss", help='Calculate the angular loss factor for the direct horizontal irradiance', no_args_is_help=True)
