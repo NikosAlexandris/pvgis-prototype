5d7dd94377ea33f6e7fd7d861f93044baaed112c
Nikos Alexandris <nikosalexandris@duck.com>
2023-06-20 17:17:37 +0300

    Clean imports, add apps in cli.py

--- pvgisprototype/cli.py
+++ pvgisprototype/cli.py
@@ -8,6 +8,7 @@ from importlib.metadata import version
 import warnings
 from pathlib import Path
 
+from rich import print
 import typer
 from typing import Optional
 
@@ -17,10 +18,10 @@ from .estimate_energy import app as estimate
 from .tmy import app as tmy
 from .time_series import app as timeseries
 from .utilities import app as utility
-from .solar_constant import app as solar_constant
 from .solar_geometry_constants import app as solar_geometry_constants
 from .solar_geometry_variables import app as solar_geometry_variables
 from .solar_position import app as solar_position
+
 
 state = {"verbose": False}
 
@@ -32,15 +33,15 @@ app = typer.Typer(
 )
 
 # app.add_typer(manual, name'manual', help='Manual for PVGIS commands, arguments and options')
-app.add_typer(irradiance, name="irradiance", help='Estimate the direct normal radiation')
-app.add_typer(estimate, name="estimate", help='Estimate the energy production of a PV system')
-app.add_typer(tmy, name="tmy", help='Generate the Typical Meteorological Year')
-app.add_typer(timeseries, name="time-series", help='Retrieve time series of solar radiation and/or PV output power')
-app.add_typer(utility, name="utility", help='Diagnose data issues and more')
-app.add_typer(solar_constant, name="solar-constant", help='Compute the solar constant for a day of the year')
-app.add_typer(solar_geometry_constants, name="geometry-constants", help='Compute solar geometry constants for a day in a year')
-app.add_typer(solar_geometry_variables, name="geometry-variables", help='Compute solar geometry variables for a day in a year')
-app.add_typer(solar_position, name="solar-position", help='Compute solar altitude and azimuth for a position and time in year and day')
+app.add_typer(irradiance, name="irradiance", help='Estimate the direct normal radiation', no_args_is_help=True)
+app.add_typer(estimate, name="energy", help='Estimate the energy production of a PV system', no_args_is_help=True)
+app.add_typer(tmy, name="tmy", help='Generate the Typical Meteorological Year', no_args_is_help=True)
+app.add_typer(timeseries, name="time-series", help='Retrieve time series of solar radiation and/or PV output power', no_args_is_help=True)
+app.add_typer(utility, name="utility", help='Diagnose data issues and more', no_args_is_help=True)
+# app.add_typer(solar_geometry_constants, name="geometry-constants", help='Compute solar geometry constants for a day in a year', no_args_is_help=True)
+# app.add_typer(solar_geometry_variables, name="geometry-variables", help='Compute solar geometry variables for a day in a year', no_args_is_help=True)
+
+app.add_typer(solar_position, name="solar-position", help='Compute solar altitude and azimuth for a position and time in year and day', no_args_is_help=True)
 
 
 def _version_callback(value: bool) -> None:
