de6bc0e860fd32c16d6f6cdee90c4ec8df918f93
Nikos Alexandris <nikosalexandris@duck.com>
2023-06-20 15:27:33 +0300

    Add calculate_solar_declination_rsun_base()
    To compare with new/alternative implementations.

--- pvgisprototype/solar_declination.py
+++ pvgisprototype/solar_declination.py
@@ -74,6 +74,48 @@ def calculate_solar_declination(
     return declination
 
 
+def calculate_solar_declination_rsun_base(
+        day_of_year: int,
+        days_in_a_year: float = 365.25,
+        orbital_eccentricity: float = 0.03344,
+        perigee_offset: float = 0.048869,
+        output_units: Annotated[str, typer.Option(
+            '-o',
+            '--output-units',
+            show_default=True,
+            case_sensitive=False,
+            help="Output units for solar declination (degrees or radians)")] = 'radians',
+        ) -> float:
+    """Approximate the sun's declination for a given day of the year.
+
+    This function is a 1:1 transfer of the solar declination calculation
+    implemented in PVGIS' r.sun C code, in form of the function `com_declin()`
+    in `rsun_base.c` (or/and `rsun_base.cpp`). It merely exists in comparing
+    with the new implementation and understanding the purpose of inverting the
+    sign.
+
+    IMPORTANT: In the original C source code, there is at the end of the
+
+    `com_declin` function:
+
+    ```
+    decl = - decl;
+    ```
+
+    which is actually : `declination = - declination`. Why? The value is
+    inverted again at some other part of the program when it gets to read data.
+    """
+    solar_declination = calculate_solar_declination(
+        day_of_year,
+        days_in_a_year,
+        orbital_eccentricity,
+        perigee_offset,
+        output_units,
+        )
+
+    return - solar_declination
+
+
 def calculate_solar_declination_hargreaves(
         day_of_year: int,
         days_in_a_year: float = 365.25,
