d18b36865ad6e389751bd79bb9b0bc32f7e59f62
Nikos Alexandris <nikosalexandris@duck.com>
2023-06-20 15:29:26 +0300

    Plot also solar declination implemented in the C code

--- pvgisprototype/solar_declination_plot.py
+++ pvgisprototype/solar_declination_plot.py
@@ -1,5 +1,6 @@
 from devtools import debug
 from pvgisprototype.solar_declination import calculate_solar_declination
+from pvgisprototype.solar_declination import calculate_solar_declination_rsun_base
 from pvgisprototype.solar_declination import calculate_solar_declination_hargreaves
 import matplotlib.pyplot as plt
 import numpy as np
@@ -15,10 +16,12 @@ def days_in_year(year):
 def plot_solar_declination(title: str = 'Annual Variation of Solar Declination'):
     days = np.arange(1, 366)
     solar_declinations = np.vectorize(calculate_solar_declination)(days, output_units='degrees')  # Calculate solar declination for each day
+    solar_declinations_rsun_base= np.vectorize(calculate_solar_declination_rsun_base)(days, output_units='degrees')  # Calculate solar declination for each day
     solar_declinations_hargreaves = np.vectorize(calculate_solar_declination_hargreaves)(days)  # Calculate solar declination for each day
 
     fig = plt.figure(figsize=(10,6))
     plt.plot(days, solar_declinations, label='PVGIS')
+    plt.plot(days, solar_declinations_rsun_base, label='PVGIS (current C code)')
     plt.plot(days, solar_declinations_hargreaves, label='Hargreaves')
     plt.xlabel('Day of the Year')
     plt.ylabel('Solar Declination')
