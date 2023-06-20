d077dc8fb6686a0cd6ca8cd0ce9dbc32f05c8d51
Nikos Alexandris <nikosalexandris@duck.com>
2023-06-20 15:23:55 +0300

    Update extraterrestrial_irradiance_plot.py

--- pvgisprototype/extraterrestrial_irradiance_plot.py
+++ pvgisprototype/extraterrestrial_irradiance_plot.py
@@ -1,29 +1,29 @@
-from pvgisprototype.solar_constant import calculate_solar_constant
+from pvgisprototype.extraterrestrial_irradiance import calculate_extraterrestrial_irradiance
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-def plot_solar_constant():
+def plot_extraterrestrial_irradiance():
     days = np.arange(1, 366)  # Days of the year
-    solar_constants = np.vectorize(calculate_solar_constant)(days)  # Calculate solar constant for each day
+    extraterrestrial_irradiances = np.vectorize(calculate_extraterrestrial_irradiance)(days)  # Calculate solar constant for each day
 
     fig = plt.figure(figsize=(10,6))
-    plt.plot(days, solar_constants, label='Solar Constant')
+    plt.plot(days, extraterrestrial_irradiances, label='Solar Constant')
 
     # Average solar constant
-    average_solar_constant = 1367
-    plt.axhline(y=average_solar_constant, color='gray', linestyle='--', label='Average Solar Constant')
+    average_extraterrestrial_irradiance = 1360.8  # Make it an external constant?
+    plt.axhline(y=average_extraterrestrial_irradiance, color='gray', linestyle='--', label='Average Solar Constant')
 
     # # Plot perigee
     # Perigee day : January 2 at 8:18pm or day number :
     perigee_day = 2.8408 
-    perigee_value = calculate_solar_constant(perigee_day)
+    perigee_value = calculate_extraterrestrial_irradiance(perigee_day)
     plt.scatter(perigee_day, perigee_value, c='red', marker='o', label='Perigee')
 
     # Plot apogee
     perigee_offset = 0.048869
     apogee_day = 0.5 * (365.25 + perigee_offset)
-    apogee_value = calculate_solar_constant(apogee_day)
+    apogee_value = calculate_extraterrestrial_irradiance(apogee_day)
     plt.scatter(apogee_day, apogee_value, c='blue', marker='o', label='Apogee')
     
     plt.xlabel('Day of the Year')
@@ -31,7 +31,7 @@ def plot_solar_constant():
     plt.title('Annual Variation of Solar Constant')
     plt.grid(True)
     plt.legend(loc='lower right')
-    plt.savefig('solar_constant.png')
+    plt.savefig('extraterrestrial_irradiance.png')
     return fig
 
-plot_solar_constant()
+plot_extraterrestrial_irradiance()
