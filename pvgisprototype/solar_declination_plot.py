52103c5484408d429dfb007b1fc4d8e142b49f05
Nikos Alexandris <nikosalexandris@duck.com>
2023-06-20 15:30:11 +0300

    Update plot_solar_declination_one_year(), plot_solar_declination_five_years()

--- pvgisprototype/solar_declination_plot.py
+++ pvgisprototype/solar_declination_plot.py
@@ -37,11 +37,13 @@ def plot_solar_declination_one_year(
         title: str = 'Annual Variation of Solar Declination',
         ):
     days = np.arange(days_in_year(year))
-    solar_declinations = np.vectorize(calculate_solar_declination)(days, output_units='degrees')  # Calculate solar declination for each day
-    solar_declinations_hargreaves = np.vectorize(calculate_solar_declination_hargreaves)(days)  # Calculate solar declination for each day
+    solar_declinations = np.vectorize(calculate_solar_declination)(days, output_units='degrees')
+    solar_declinations_rsun_base= np.vectorize(calculate_solar_declination_rsun_base)(days, output_units='degrees')
+    solar_declinations_hargreaves = np.vectorize(calculate_solar_declination_hargreaves)(days)
 
     fig = plt.figure(figsize=(10,6))
     plt.plot(days, solar_declinations, label='PVGIS')
+    plt.plot(days, solar_declinations_rsun_base, label='PVGIS (current C code)')
     plt.plot(days, solar_declinations_hargreaves, label='Hargreaves')
     plt.xlabel('Day of the Year')
     # plt.ylabel(f'Solar Declination')
@@ -55,19 +57,26 @@ def plot_solar_declination_one_year(
 def plot_solar_declination_five_years(
         start_year: int,
         end_year: int,
-        title: str = 'Annual Variation of Solar Declination',
+        title: str = 'Five-Year Variation of Solar Declination',
         ):
-    days = [days_in_year(year) for year in np.arange(start_year, end_year)]
-    solar_declinations = np.vectorize(calculate_solar_declination)(days, output_units='degrees')  # Calculate solar declination for each day
-    solar_declinations_hargreaves = np.vectorize(calculate_solar_declination_hargreaves)(days)  # Calculate solar declination for each day
+
+    years = np.arange(start_year, end_year + 1)
+    is_leap = np.vectorize(lambda y: datetime(y, 12, 31).timetuple().tm_yday == 366)(years)
+    days_per_year = np.where(is_leap, 366, 365)
+    sum_of_days = np.sum(days_per_year)
+    days = np.arange(1, sum_of_days + 1)
+    solar_declinations = np.vectorize(calculate_solar_declination)(days, output_units='degrees')
+    solar_declinations_rsun_base = np.vectorize(calculate_solar_declination_rsun_base)(days, output_units='degrees')
+    solar_declinations_hargreaves = np.vectorize(calculate_solar_declination_hargreaves)(days)
 
     fig = plt.figure(figsize=(10,6))
     plt.plot(days, solar_declinations, label='PVGIS')
+    plt.plot(days, solar_declinations_rsun_base, label='PVGIS (current C code)')
     plt.plot(days, solar_declinations_hargreaves, label='Hargreaves')
     plt.xlabel('Day of the Year')
-    plt.ylabel('Solar Declination')
+    # plt.ylabel('Solar Declination')
     plt.title(title)
     plt.grid(True)
     plt.legend()
-    plt.savefig('solar_declination.png')
+    plt.savefig(f'solar_declination_{start_year}_to_{end_year}.png')
     return fig
