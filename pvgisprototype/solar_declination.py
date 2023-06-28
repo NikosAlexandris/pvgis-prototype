da82a8701d776cc91c06db82d5655a3ff0fd4e00
Nikos Alexandris <nikosalexandris@duck.com>
2023-06-28 14:06:15 +0300

    Update docstring for calculate_solar_declination_hargreaves()

--- pvgisprototype/solar_declination.py
+++ pvgisprototype/solar_declination.py
@@ -165,19 +165,19 @@ def calculate_solar_declination_hargreaves(
         Notes
         -----
 
-        365.25: The number 365.25 represents the average number of days in a
+        - 365.25: The number 365.25 represents the average number of days in a
         year. This value is used to scale the orbital position of the Earth.
 
-        284: The number 284 represents a constant term added to the day of the
+        - 284: The number 284 represents a constant term added to the day of the
         year. It adjusts the calculation to align with the Earth's position
         during the winter solstice, which usually occurs around December 21st
         (approximately 284 days into the year).
 
-        0.4: The number 0.4 is a constant that determines the amplitude of the
+        - 0.4: The number 0.4 is a constant that determines the amplitude of the
         seasonal variation. It is multiplied by the second sine term to
         modulate the seasonal change in the solar declination.
 
-        100: The number 100 represents an offset to the day of the year. It is
+        - 100: The number 100 represents an offset to the day of the year. It is
         subtracted from the original day of the year before calculating the
         second sine term. This offset helps adjust the timing of the seasonal
         variation and is usually chosen to align with the summer solstice,
