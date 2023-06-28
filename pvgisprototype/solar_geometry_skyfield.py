e9f7146e04290ebbbbbf14944515a88b0592d60a
Nikos Alexandris <nikosalexandris@duck.com>
2023-06-28 14:10:07 +0300

    Add prototype solar_geometry_skyfield.py

---
+++ pvgisprototype/solar_geometry_skyfield.py
@@ -0,0 +1,76 @@
+import typer
+from typing import Annotated
+from typing import Optional
+from skyfield import api
+from skyfield import almanac
+from datetime import datetime
+from datetime import timedelta
+import dateutil.parser
+from calendar import monthrange
+from .conversions import convert_to_degrees_if_requested
+from .conversions import convert_to_radians_if_requested
+from .conversions import convert_to_radians
+from .timestamp import now_datetime
+from .timestamp import convert_to_timezone
+
+
+def calculate_solar_position_skyfield(
+        longitude: Annotated[float, typer.Argument(
+            callback=convert_to_radians,
+            min=-90, max=90)],
+        latitude: Annotated[Optional[float], typer.Argument(
+            callback=convert_to_radians,
+            min=-90, max=90)],
+        timestamp: Annotated[Optional[datetime], typer.Argument(
+            help='Timestamp',
+            default_factory=now_datetime)],
+        timezone: Annotated[Optional[str], typer.Option(
+            help='Timezone',
+            callback=convert_to_timezone)] = None,
+        # output_units: Annotated[str, typer.Option(
+        #     '-u',
+        #     '--units',
+        #     show_default=True,
+        #     case_sensitive=False,
+        #     help="Output units for solar geometry variables (degrees or radians)")] = 'radians',
+        ) -> float:
+    """Calculate sun position
+
+    Notes
+    -----
+
+    For the implementation, see also:
+    https://techoverflow.net/2022/06/19/how-to-compute-position-of-sun-in-the-sky-in-python-using-skyfield/
+
+    Factors influencing the accuracy of the calculation using Skyfield:
+
+    - Skyfield considers:
+
+        - The slight shift in position caused by light speed
+        - The very very slight shift in position caused by earth’s gravity
+
+    - Skyfield does not consider:
+
+        - Atmospheric distortions shifting the sun’s position
+        - The extent of the sun’s disk causing the sun to emanate not from a
+          point but apparently from an area
+    """
+    # Handle Me during input validation? -------------------------------------
+    try:
+        timestamp = timezone.localize(timestamp)
+    except Exception:
+        logging.warning(f'tzinfo already set for timestamp = {timestamp}')
+    # Handle Me during input validation? -------------------------------------
+
+    ephemeris = load('de421.bsp')
+    sun = ephemeris['Sun']
+    earth = ephemeris['Earth']
+    location = wgs84.latlon(latitude * N, longitude * W)  # W or E ?
+
+    # the sun position as seen from the observer
+    timescale = api.load.timescale()
+    solar_position = (earth + location).at(timescale.now()).observe(sun).apparent()
+    solar_altitude, solar_azimuth, distance_to_sun = solar_position.altaz()
+
+    # in degrees? -> solar_altitude.degrees, solar_azimuth.degrees
+    return solar_altitude, solar_azimuth, distance_to_sun
