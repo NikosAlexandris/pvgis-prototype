6dd7248a4cb286a62a9a1624b6eb3254e5767d02
Nikos Alexandris <nikosalexandris@duck.com>
2023-07-07 09:36:03 +0300

    Restructure solar_geometry_skyfield.py

--- pvgisprototype/api/geometry/solar_geometry_skyfield.py
+++ pvgisprototype/api/geometry/solar_geometry_skyfield.py
@@ -29,12 +29,12 @@ def calculate_solar_position_skyfield(
         timezone: Annotated[Optional[str], typer.Option(
             help='Timezone',
             callback=convert_to_timezone)] = None,
-        # output_units: Annotated[str, typer.Option(
-        #     '-u',
-        #     '--units',
-        #     show_default=True,
-        #     case_sensitive=False,
-        #     help="Output units for solar geometry variables (degrees or radians)")] = 'radians',
+        output_units: Annotated[str, typer.Option(
+            '-u',
+            '--units',
+            show_default=True,
+            case_sensitive=False,
+            help="Output units for solar geometry variables (degrees or radians)")] = 'radians',
         ) -> float:
     """Calculate sun position
 
@@ -56,13 +56,18 @@ def calculate_solar_position_skyfield(
         - Atmospheric distortions shifting the sun’s position
         - The extent of the sun’s disk causing the sun to emanate not from a
           point but apparently from an area
+
+    Notes
+    -----
+
+    To consider:
+
+    - https://rhodesmill.org/skyfield/almanac.html#elevated-vantage-points
+    - The `Time` class in Skyfield (i.e. `timescale()`) only uses UTC for input
+      and output
+    - https://rhodesmill.org/skyfield/time.html#utc-and-your-timezone
+    - https://rhodesmill.org/skyfield/time.html#utc-and-leap-seconds
     """
-    # Handle Me during input validation? -------------------------------------
-    try:
-        timestamp = timezone.localize(timestamp)
-    except Exception:
-        logging.warning(f'tzinfo already set for timestamp = {timestamp}')
-    # Handle Me during input validation? -------------------------------------
 
     ephemeris = skyfield.api.load('de421.bsp')
     sun = ephemeris['Sun']
@@ -73,8 +78,138 @@ def calculate_solar_position_skyfield(
 
     # the sun position as seen from the observer
     timescale = skyfield.api.load.timescale()
-    solar_position = (earth + location).at(timescale.now()).observe(sun).apparent()
+    requested_timestamp = timescale.from_datetime(timestamp)
+    solar_position = (earth + location).at(requested_timestamp).observe(sun).apparent()
+
+    return solar_position
+
+
+def calculate_solar_altitude_azimuth_skyfield(
+        longitude: Annotated[float, typer.Argument(
+            callback=convert_to_radians,
+            min=-90, max=90)],
+        latitude: Annotated[Optional[float], typer.Argument(
+            callback=convert_to_radians,
+            min=-90, max=90)],
+        timestamp: Annotated[Optional[datetime], typer.Argument(
+            help='Timestamp',
+            default_factory=now_datetime)],
+        timezone: Annotated[Optional[str], typer.Option(
+            help='Timezone',
+            callback=convert_to_timezone)] = None,
+        output_units: Annotated[str, typer.Option(
+            '-u',
+            '--units',
+            show_default=True,
+            case_sensitive=False,
+            help="Output units for solar geometry variables (degrees or radians)")] = 'radians',
+        ) -> float:
+    """Calculate sun position
+
+    Notes
+    -----
+
+    For the implementation, see also:
+    https://techoverflow.net/2022/06/19/how-to-compute-position-of-sun-in-the-sky-in-python-using-skyfield/
+
+    Factors influencing the accuracy of the calculation using Skyfield:
+
+    - Skyfield considers:
+
+        - The slight shift in position caused by light speed
+        - The very very slight shift in position caused by earth’s gravity
+
+    - Skyfield does not consider:
+
+        - Atmospheric distortions shifting the sun’s position
+        - The extent of the sun’s disk causing the sun to emanate not from a
+          point but apparently from an area
+
+    Notes
+    -----
+
+    To consider:
+
+    - https://rhodesmill.org/skyfield/almanac.html#elevated-vantage-points
+    - The `Time` class in Skyfield (i.e. `timescale()`) only uses UTC for input
+      and output
+    - https://rhodesmill.org/skyfield/time.html#utc-and-your-timezone
+    - https://rhodesmill.org/skyfield/time.html#utc-and-leap-seconds
+    """
+    solar_position = calculate_solar_position_skyfield(
+            longitude,
+            latitude,
+            timestamp,
+            timezone,
+            output_units,
+            )
     solar_altitude, solar_azimuth, distance_to_sun = solar_position.altaz()
 
-    # in degrees? -> solar_altitude.degrees, solar_azimuth.degrees
-    return solar_altitude.radians, solar_azimuth.radians, distance_to_sun
+    if output_units == 'radians':
+        solar_altitude = solar_altitude.radians
+        solar_azimuth = solar_azimuth.radians
+
+    if output_units == 'degrees':
+        solar_altitude = solar_altitude.degrees
+        solar_azimuth = solar_azimuth.degrees
+
+    return solar_altitude, solar_azimuth  # distance_to_sun
+
+
+def calculate_hour_angle_skyfield(
+        longitude: Annotated[float, typer.Argument(
+            callback=convert_to_radians,
+            min=-90, max=90)],
+        latitude: Annotated[Optional[float], typer.Argument(
+            callback=convert_to_radians,
+            min=-90, max=90)],
+        timestamp: Annotated[Optional[datetime], typer.Argument(
+            help='Timestamp',
+            default_factory=now_datetime)],
+        timezone: Annotated[Optional[str], typer.Option(
+            help='Timezone',
+            callback=convert_to_timezone)] = None,
+        output_units: Annotated[str, typer.Option(
+            '-u',
+            '--units',
+            show_default=True,
+            case_sensitive=False,
+            help="Output units for solar geometry variables (degrees or radians)")] = 'radians',
+        ) -> float:
+    """Calculate the hour angle ω'
+
+    Parameters
+    ----------
+
+
+    Returns
+    --------
+
+    hour_angle: float
+        Hour angle is the angle (ω) at any instant through which the earth has
+        to turn to bring the meridian of the observer directly in line with the
+        sun's rays measured in radian.
+    """
+    solar_position = calculate_solar_position_skyfield(
+            longitude,
+            latitude,
+            timestamp,
+            timezone,
+            output_units,
+            )
+    hour_angle, solar_declination, distance_to_sun = solar_position.hadec()
+
+    if output_units == 'minutes':
+        hour_angle = hour_angle.minutes
+        solar_declination = solar_declination.minutes
+
+    if output_units == 'hours':
+        hour_angle = hour_angle.hours
+        solar_declination = solar_declination.hours
+
+    if output_units == 'radians':
+        hour_angle = hour_angle.radians
+        solar_declination = solar_declination.radians
+
+    return hour_angle, solar_declination, output_units
+
