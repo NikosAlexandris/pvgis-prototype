6bc5bbe08966360276563ab9aa305d142d386503
Nikos Alexandris <nikosalexandris@duck.com>
2023-07-07 09:09:24 +0300

    Add global subcommand in irradiance.py
    The `global` app is currently a stub.

--- pvgisprototype/irradiance.py
+++ pvgisprototype/irradiance.py
@@ -2,6 +2,7 @@ import typer
 from typing_extensions import Annotated
 from typing import Optional
 
+from .api.irradiance.global_irradiance import app as global_irradiance
 from .api.irradiance.direct_irradiance import app as direct_irradiance
 from .api.irradiance.diffuse_irradiance import app as diffuse_irradiance
 from .api.irradiance.reflected_irradiance import app as reflected_irradiance
@@ -17,6 +18,7 @@ app = typer.Typer(
 )
 
 
+app.add_typer(global_irradiance, name="global", help='Estimate the global irradiance', no_args_is_help=True)
 app.add_typer(direct_irradiance, name="direct", help='Estimate the direct irradiance', no_args_is_help=True)
 app.add_typer(diffuse_irradiance, name="diffuse", help='Estimate the diffuse irradiance', no_args_is_help=True)
 app.add_typer(reflected_irradiance, name="reflected", help='Estimate the direct reflected radiation', no_args_is_help=True)
