cacf9184a3c4f5bfd460c41f4b28b306a5da4020
Nikos Alexandris <nikosalexandris@duck.com>
2023-07-08 16:51:47 +0300

    Rename calculate_hour_angle_noaa() to calculate_solar_hour_angle_noaa()
    Update arguments definition, function body, docstring, valid ouput range

--- pvgisprototype/api/geometry/solar_position_noaa.py
+++ pvgisprototype/api/geometry/solar_position_noaa.py
@@ -255,22 +255,30 @@ def calculate_true_solar_time_noaa(
     return true_solar_time, time_output_units
 
 
-@validate_with_pydantic(CalculateHourAngleNOAAInput)
-def calculate_hour_angle_noaa(
+@validate_with_pydantic(CalculateSolarHourAngleNOAAInput)
+def calculate_solar_hour_angle_noaa(
         longitude: float,
         timestamp: datetime, 
         timezone: Optional[str], 
-        output_units: Optional[str] = 'radians',
+        time_output_units: Optional[str] = 'minutes',
+        angle_output_units: Optional[str] = 'radians',
 
         ) -> float:
     """Calculate the solar hour angle in radians.
 
-    The Hour Angle converts the local solar time (LST) into the number of
-    degrees which the sun moves across the sky. By definition, the Hour Angle
-    is 0° at solar noon. Since the Earth rotates 15° per hour, each hour away
-    from solar noon corresponds to an angular motion of the sun in the sky of
-    15°. In the morning the hour angle is negative, in the afternoon the hour
-    angle is positive.
+    The solar hour angle calculation converts the local solar time (LST) into
+    the number of degrees which the sun moves across the sky. In other words,
+    it reflects the Earth's rotation and indicates the time of the day relative
+    to the position of the Sun. It bases on the longitude and timestamp and by
+    definition, the solar hour angle is :
+
+      - 0° at solar noon
+      - negative in the morning
+      - positive in the afternoon.
+
+    Since the Earth rotates 15° per hour, each hour away from solar noon
+    corresponds to an angular motion of the sun in the sky of 15°.
+    Practically, the calculation converts a timestamp into a solar time.
 
     Parameters
     ----------
@@ -283,23 +291,35 @@ def calculate_hour_angle_noaa(
 
     Returns:
     float: The solar hour angle
+    Notes
+    -----
+
+    In the "original" equation, the solar hour angle is measured in degrees.
+
+        `hour_angle = true_solar_time / 4 - 180`
+        
+        which is the same as
+
+        `hour_angle = true_solar_time * 0.25 - 180`
+
+    In the present implementation, we calculate the solar hour angle directly
+    to radians. A circle is 360 degrees, dividing by 1440 minutes in a day
+    equals to 0.25.
     """
     true_solar_time, _units = calculate_true_solar_time_noaa(
-        longitude, timestamp, timezone, output_units="minutes"
+        longitude, timestamp, timezone, time_output_units
     )  # in minutes
-    # hour_angle = true_solar_time / 4 - 180  # original equation in degrees!
-    # Here, going directly to radians
-    #  a circle is 360 degrees, divide by 1440 minutes in a day = 0.25
-    hour_angle = (true_solar_time - 720) * (pi / 720)
+
+    solar_hour_angle = (true_solar_time - 720) * (pi / 720)
 
     # Validate
-    if output_units == 'radians' and not -pi <= hour_angle <= pi:
+    if angle_output_units == 'radians' and not -pi <= solar_hour_angle <= pi:
         raise ValueError("The hour angle in radians must be within the range [-π, π]")
 
-    elif output_units == 'degrees' and not -180 <= hour_angle <= 180:
+    elif angle_output_units == 'degrees' and not -180 <= solar_hour_angle <= 180:
         raise ValueError("The hour angle in degrees must be within the range [-180, 180] degrees")
 
-    return hour_angle, output_units
+    return solar_hour_angle, angle_output_units
 
 
 def atmospheric_refraction_for_high_solar_altitude(
