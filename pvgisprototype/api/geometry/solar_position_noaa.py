9c118dd57b3708598d65236627743ab47f1d9c46
Nikos Alexandris <nikosalexandris@duck.com>
2023-07-08 17:02:15 +0300

    Add calculate_noaa_solar_position()

--- pvgisprototype/api/geometry/solar_position_noaa.py
+++ pvgisprototype/api/geometry/solar_position_noaa.py
@@ -748,4 +748,145 @@ def calculate_local_solar_time_noaa(
     if verbose:
         typer.echo(f'Local solar time: {local_solar_time}')
 
+    # debug(locals())
     return decimal_hours, time_output_units
+
+@validate_with_pydantic(CalculateSolarPositionNOAA)
+def calculate_noaa_solar_position(
+    longitude: float,
+    latitude: float,
+    timestamp: datetime,
+    timezone: str,
+    refracted_solar_zenith: float = 1.5853349194640094,  # radians
+    apply_atmospheric_refraction: bool = False,
+    time_output_units: str = 'minutes',
+    angle_units: str = 'radians',
+    angle_output_units: str = 'radians',
+) -> dict:
+    """
+    """
+    result = {}
+    fractional_year, _units = calculate_fractional_year_noaa(
+            timestamp,
+            angle_output_units
+            )
+    equation_of_time, _units = calculate_equation_of_time_noaa(
+            timestamp,
+            time_output_units,
+            angle_units,  # for calculate_fractional_year_noaa()
+            )
+    solar_declination, _units = calculate_solar_declination_noaa(
+            timestamp,
+            angle_units,  # for calculate_fractional_year_noaa()
+            angle_output_units
+            )
+    time_offset, _units = calculate_time_offset_noaa(
+            longitude,
+            timestamp,
+            time_output_units,  # also for calculate_equation_of_time_noaa()
+            angle_units,  # for calculate_equation_of_time_noaa()
+            )
+    true_solar_time, _units = calculate_true_solar_time_noaa(
+            longitude,
+            timestamp,
+            timezone,
+            time_output_units,
+            )
+    solar_hour_angle, _units = calculate_solar_hour_angle_noaa(
+            longitude,
+            timestamp,
+            timezone,
+            time_output_units,
+            angle_output_units,
+            )
+    solar_zenith, _units = calculate_solar_zenith_noaa(
+            latitude,
+            timestamp,
+            solar_hour_angle,
+            apply_atmospheric_refraction,
+            # time_output_units,
+            angle_units,
+            angle_output_units,
+            )
+    solar_altitude, _units = calculate_solar_altitude_noaa(
+            longitude,
+            latitude,
+            timestamp,
+            timezone,
+            apply_atmospheric_refraction,
+            time_output_units,
+            angle_units,
+            angle_output_units,
+    )
+    solar_azimuth, _units = calculate_solar_azimuth_noaa(
+            longitude,
+            latitude,
+            timestamp,
+            timezone,
+            apply_atmospheric_refraction,
+            time_output_units,
+            angle_units,
+            angle_output_units,
+            )
+    sunrise_time, _units = calculate_event_time_noaa(
+            longitude,
+            latitude,
+            timestamp,
+            timezone,
+            'sunrise',
+            refracted_solar_zenith,
+            apply_atmospheric_refraction,
+            time_output_units,
+            angle_units,
+            angle_output_units,
+            )
+    solar_noon_time, _units = calculate_event_time_noaa(
+            longitude,
+            latitude,
+            timestamp,
+            timezone,
+            'noon',
+            refracted_solar_zenith,
+            apply_atmospheric_refraction,
+            time_output_units,
+            angle_units,
+            angle_output_units,
+            )
+    local_solar_time, _units = calculate_local_solar_time_noaa(
+            longitude,
+            latitude,
+            timestamp,
+            timezone,
+            refracted_solar_zenith,
+            apply_atmospheric_refraction,
+            time_output_units,
+            angle_units,
+            angle_output_units,
+            )
+    sunset_time, _units = calculate_event_time_noaa(
+            longitude,
+            latitude,
+            timestamp,
+            timezone,
+            'sunset',
+            refracted_solar_zenith,
+            apply_atmospheric_refraction,
+            time_output_units,
+            angle_units,
+            angle_output_units,
+            )
+    result['fractional_year'] = fractional_year
+    result['equation_of_time'] = equation_of_time
+    result['solar_declination'] = solar_declination
+    result['time_offset'] = time_offset
+    result['true_solar_time'] = true_solar_time
+    result['solar_hour_angle'] = solar_hour_angle
+    result['solar_zenith'] = solar_zenith
+    result['solar_altitude'] = solar_altitude
+    result['solar_azimuth'] = solar_azimuth
+    result['sunrise_time'] = sunrise_time
+    result['noon_time'] = solar_noon_time
+    result['local_solar_time'] = local_solar_time
+    result['sunset_time'] = sunset_time
+
+    return result
