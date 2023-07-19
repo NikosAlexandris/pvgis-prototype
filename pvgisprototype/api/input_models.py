a9897ad0e6aa2060e0f76440b1e517c1b1b5144e
Nikos Alexandris <nikosalexandris@duck.com>
2023-07-19 14:58:17 +0300

    Add api/input_models.py

---
+++ pvgisprototype/api/input_models.py
@@ -0,0 +1,108 @@
+from devtools import debug
+from pydantic import BaseModel
+from pydantic import ConfigDict
+from pydantic import field_validator
+from pydantic import confloat
+from typing import Optional
+from zoneinfo import ZoneInfo
+from datetime import datetime
+
+
+class BaseTimestampInputModel(BaseModel):
+    timestamp: datetime
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+
+class BaseTimeInputModel(BaseTimestampInputModel):
+    timezone: Optional[ZoneInfo] = None
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    @field_validator('timezone')
+    @classmethod
+    def validate_timezone(cls, v):
+        if v is not None and not isinstance(v, ZoneInfo):
+            raise ValueError("The `timezone` must be a valid `zoneinfo.ZoneInfo` object.")
+        return v
+
+
+class RandomTimeInputModel(BaseModel):
+    random_time: bool
+
+
+class BaseTimeOutputUnitsModel(BaseModel):
+    time_output_units: Optional[str] = None
+
+    @field_validator('time_output_units')
+    @classmethod
+    def validate_time_output_units(cls, v):
+        valid_units = ['minutes', 'seconds', 'hours']
+        if v not in valid_units:
+            raise ValueError(f"time_output_units must be one of {valid_units}")
+        return v
+
+
+# class BaseAngleOutputUnitsModel(BaseModel):  # Use this as in models/noaa/noaa_models.py
+class BaseOutputUnitsModel(BaseModel):
+    angle_output_units: Optional[str]
+
+    @field_validator('angle_output_units')
+    @classmethod
+    def validate_angle_output_units(cls, v):
+        valid_units = ['radians', 'degrees']
+        if v not in valid_units:
+            raise ValueError(f"angle_output_units must be one of {valid_units}")
+        return v
+
+
+class Longitude(BaseModel):
+    longitude: confloat(ge=-180, le=180)
+
+
+class Latitude(BaseModel):
+    latitude: confloat(ge=-90, le=90)
+
+
+class BaseCoordinatesInputModel(Longitude, Latitude):
+    pass
+
+
+class SolarAltitudeInput(
+        BaseCoordinatesInputModel,
+        BaseTimeInputModel,
+        BaseOutputUnitsModel,
+        ):
+    pass
+
+
+class SolarAzimuthInput(SolarAltitudeInput):
+    pass
+
+
+class EarthOrbitInputModel(BaseModel):
+    days_in_a_year: float = 365.25
+    orbital_eccentricity: float = 0.03344
+    perigee_offset: float = 0.048869
+
+
+class SolarDeclinationInput(
+        # timestamp: Annotated[Optional[datetime], typer.Argument(
+        #     help='Timestamp',
+        #     default_factory=now_utc_datetimezone)],
+        # timezone: Annotated[Optional[str], typer.Option(
+        #     help='Timezone',
+        #     callback=convert_to_timezone)] = None,
+        BaseTimeInputModel,
+        # days_in_a_year: float = 365.25,
+        # orbital_eccentricity: float = 0.03344,
+        # perigee_offset: float = 0.048869,
+        EarthOrbitInputModel,
+        # output_units: Annotated[str, typer.Option(
+        #     '-o',
+        #     '--output-units',
+        #     show_default=True,
+        #     case_sensitive=False,
+        #     help="Output units for solar declination (degrees or radians)")] = 'radians',
+        BaseOutputUnitsModel,
+        ):
+    debug(locals())
+    pass
