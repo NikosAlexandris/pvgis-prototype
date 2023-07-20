5b6c05a51541582e4d1529abcd455abff8efd534
Nikos Alexandris <nikosalexandris@duck.com>
2023-07-20 14:29:06 +0300

    Update api/geometry/solar_declination.py
    Use SolarDeclinationInput and validate_with_pydantic.
    Note then the use of `input.timestamp.year` for example.

--- pvgisprototype/api/geometry/solar_declination.py
+++ pvgisprototype/api/geometry/solar_declination.py
@@ -1,58 +1,16 @@
-from devtools import debug
 import typer
-from typing import Annotated
-from typing import Optional
-import math
-import numpy as np
-from functools import partial
 from datetime import datetime
-from datetime import timezone
-from datetime import timedelta
-from ..utilities.timestamp import now_utc_datetimezone
-from ..utilities.timestamp import convert_to_timezone
-from ..utilities.timestamp import attach_timezone
+from math import pi
+from math import sin
+from math import asin
+from ..utilities.conversions import convert_to_degrees_if_requested
+
+from pvgisprototype.api.input_models import SolarDeclinationInput
+from pvgisprototype.api.decorators import validate_with_pydantic
 
 
-app = typer.Typer(
-    add_completion=False,
-    add_help_option=True,
-    help=f"Approximate the solar declination for a day in the year",
-)
-
-
-def convert_to_degrees_if_requested(angle: float, output_units: str) -> float:
-    """Convert angle from radians to degrees if requested."""
-    x = np.degrees(angle) if output_units == 'degrees' else angle
-    return x
-
-
-def convert_to_radians_if_requested(angle: float, output_units: str) -> float:
-    """Convert angle from degrees to radians if requested."""
-    return np.radians(angle) if output_units == 'radians' else angle
-
-
-@app.callback(invoke_without_command=True, no_args_is_help=True)
-def calculate_solar_declination(
-        timestamp: Annotated[Optional[datetime], typer.Argument(
-            help='Timestamp',
-            default_factory=now_utc_datetimezone)],
-        timezone: Annotated[Optional[str], typer.Option(
-            help='Timezone',
-            callback=convert_to_timezone)] = None,
-        days_in_a_year: float = 365.25,
-        orbital_eccentricity: float = 0.03344,
-        perigee_offset: float = 0.048869,
-        output_units: Annotated[str, typer.Option(
-            '-o',
-            '--output-units',
-            show_default=True,
-            case_sensitive=False,
-            help="Output units for solar declination (degrees or radians)")] = 'radians',
-        random_time: Annotated[bool, typer.Option(
-            '-r',
-            '--random-time',
-            help="Generate a random date, time and timezone to demonstrate calculation")] = False,
-        ) -> float:
+# @validate_with_pydantic(SolarDeclinationInput)
+def calculate_solar_declination(input: SolarDeclinationInput) -> float:
     """Approximate the sun's declination for a given day of the year.
 
     The solar declination is the angle between the Sun's rays and the
@@ -88,17 +46,17 @@ def calculate_solar_declination(
     For more accurate calculations of solar position, comprehensive models like
     the Solar Position Algorithm (SPA) are typically used.
     """
-    year = timestamp.year
+    year = input.timestamp.year
     start_of_year = datetime(year=year, month=1, day=1)
-    day_of_year = timestamp.timetuple().tm_yday
-    day_angle = 2 * math.pi * day_of_year / days_in_a_year
-    declination = math.asin(
-            0.3978 * math.sin(
-                day_angle - 1.4 + orbital_eccentricity * math.sin(
-                    day_angle - perigee_offset
+    day_of_year = input.timestamp.timetuple().tm_yday
+    day_angle = 2 * pi * day_of_year / input.days_in_a_year
+    declination = asin(
+            0.3978 * sin(
+                day_angle - 1.4 + input.orbital_eccentricity * sin(
+                    day_angle - input.perigee_offset
                     )
                 )
             )
-    declination = convert_to_degrees_if_requested(declination, output_units)
+    declination = convert_to_degrees_if_requested(declination, input.angle_output_units)
 
     return declination
