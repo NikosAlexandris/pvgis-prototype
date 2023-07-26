a0b5671a1366d2a93e3d2ad472c33619a3fe4888
Nikos Alexandris <nikosalexandris@duck.com>
2023-07-26 15:57:35 +0300

    Update cli/irradiance.py
    Use new module names after removing the _irradiance suffix!

--- pvgisprototype/cli/irradiance.py
+++ pvgisprototype/cli/irradiance.py
@@ -2,12 +2,13 @@ import typer
 from typing_extensions import Annotated
 from typing import Optional
 
-from ..api.irradiance.global_irradiance import app as global_irradiance
-from ..api.irradiance.direct_irradiance import app as direct_irradiance
-from ..api.irradiance.diffuse_irradiance import app as diffuse_irradiance
-from ..api.irradiance.reflected_irradiance import app as reflected_irradiance
-from ..api.irradiance.extraterrestrial_irradiance import app as extraterrestrial_irradiance
-from ..api.irradiance.angular_loss_factor import app as angular_loss_factor
+from ..api.irradiance.extraterrestrial import app as extraterrestrial_irradiance
+from ..api.irradiance.shortwave import app as global_irradiance
+from ..api.irradiance.direct import app as direct_irradiance
+from ..api.irradiance.diffuse import app as diffuse_irradiance
+from ..api.irradiance.reflected import app as reflected_irradiance
+from ..api.irradiance.loss import app as angular_loss_factor
+from ..api.irradiance.effective import app as effective_irradiance
 
 
 app = typer.Typer(
@@ -18,9 +19,10 @@ app = typer.Typer(
 )
 
 
-app.add_typer(global_irradiance, name="global", help='Estimate the global irradiance', no_args_is_help=True)
+app.add_typer(global_irradiance, name="global", help='Estimate the global (shortwave) irradiance', no_args_is_help=True)
 app.add_typer(direct_irradiance, name="direct", help='Estimate the direct irradiance', no_args_is_help=True)
 app.add_typer(diffuse_irradiance, name="diffuse", help='Calculate the diffuse irradiance', no_args_is_help=True)
-app.add_typer(reflected_irradiance, name="reflected", help='Estimate the direct reflected radiation', no_args_is_help=True)
+app.add_typer(reflected_irradiance, name="reflected", help='Estimate the reflected radiation', no_args_is_help=True)
 app.add_typer(extraterrestrial_irradiance, name="extraterrestrial", help='Compute the solar constant for a day of the year', no_args_is_help=True)
 app.add_typer(angular_loss_factor, name="angular-loss", help='Calculate the angular loss factor for the direct horizontal irradiance', no_args_is_help=True)
+app.add_typer(effective_irradiance, name="effective", help='Calculate the effective irradiance for a specific hour', no_args_is_help=True)
