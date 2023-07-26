cd10688ca9d631232be002862ae7d7db2b960864
Nikos Alexandris <nikosalexandris@duck.com>
2023-07-26 16:37:59 +0300

    Update noaa/fractional_year.py
    - Update angle_output_units argument definition
    - Set fractional_year to 0 if it's calculation is only slightly below 0
    - Update ValueError message

--- pvgisprototype/models/noaa/fractional_year.py
+++ pvgisprototype/models/noaa/fractional_year.py
@@ -9,7 +9,7 @@ from ...api.utilities.conversions import convert_to_degrees_if_requested
 @validate_with_pydantic(CalculateFractionalYearNOAAInput)
 def calculate_fractional_year_noaa(
         timestamp: datetime,
-        angle_output_units: Optional[str] = "radians",
+        angle_output_units: str = "radians",  # Returned, not used!
         ) -> float:
     """Calculate fractional year in radians """
     fractional_year = (
@@ -19,12 +19,11 @@ def calculate_fractional_year_noaa(
         * (timestamp.timetuple().tm_yday - 1 + float(timestamp.hour - 12) / 24)
     )
 
-    if not 0 <= fractional_year < 2 * pi:
-        raise ValueError('Fractional year (in radians) must be in the range [0, 2*pi]')
+    # slightly less than 0 ?
+    if -pi/365 <= fractional_year < 0:  # for example, consider values > -1e-6 as close enough to 0
+        fractional_year = 0
 
-    # fractional_year = convert_to_degrees_if_requested(fractional_year, angle_output_units)
-    # if angle_output_units == 'degrees':
-    #     if not 0 <= fractional_year < 360:
-    #         raise ValueError('Fractional year (in degrees) must be in the range [0, 360]')
-            
+    if not 0 <= fractional_year < 2 * pi:
+        raise ValueError(f'The calculated fractional year {fractional_year} is outside the expected range [0, 2*pi] radians')
+
     return fractional_year, angle_output_units
