bc8055e90c14305034b57f9e15c7dbb61a4bb996
Nikos Alexandris <nikosalexandris@duck.com>
2023-08-24 19:13:32 +0300

    Update api/irradiance/shortwave.py
    Another way to calculate global irradiance!

--- pvgisprototype/api/irradiance/shortwave.py
+++ pvgisprototype/api/irradiance/shortwave.py
@@ -114,4 +114,87 @@ def estimate_global_irradiance(
         # elevation_file_number_ns,
         # elevation_file_number_ew,
     """
-    pass
+    direct_irradiance = calculate_direct_inclined_irradiance_pvgis(
+        longitude=longitude,
+        latitude=latitude,
+        elevation=elevation,
+        timestamp=timestamp,
+        timezone=timezone,
+        direct_horizontal_component=direct_horizontal_irradiance,
+        mask_and_scale=mask_and_scale,
+        inexact_matches_method=inexact_matches_method,
+        tolerance=tolerance,
+        in_memory=in_memory,
+        surface_tilt=surface_tilt,
+        surface_orientation=surface_orientation,
+        linke_turbidity_factor=linke_turbidity_factor,
+        solar_incidence_angle_model=solar_incidence_angle_model,
+        solar_time_model=solar_time_model,
+        time_offset_global=time_offset_global,
+        hour_offset=hour_offset,
+        days_in_a_year=days_in_a_year,
+        perigee_offset=perigee_offset,
+        eccentricity_correction_factor=eccentricity_correction_factor,
+        angle_output_units=angle_output_units,
+        verbose=verbose,
+    )
+    # diffuse horizontal
+    # G0
+    extraterrestial_normal_irradiance = calculate_extraterrestrial_normal_irradiance(timestamp)
+
+    # extraterrestrial on a horizontal surface requires the solar altitude
+    solar_altitude = calculate_solar_altitude(
+        longitude=longitude,
+        latitude=latitude,
+        timestamp=timestamp,
+        timezone=timezone,
+        apply_atmospheric_refraction=apply_atmospheric_refraction,
+        refracted_solar_zenith=refracted_solar_zenith,
+        days_in_a_year=days_in_a_year,
+        perigee_offset=perigee_offset,
+        eccentricity_correction_factor=eccentricity_correction_factor,
+        time_offset_global=time_offset_global,
+        hour_offset=hour_offset,
+        solar_time_model=solar_time_model,
+        time_output_units=time_output_units,
+        angle_units=angle_units,
+        angle_output_units=angle_output_units,
+        )
+    # on a horizontal surface : G0h = G0 sin(h0)
+    extraterrestial_horizontal_irradiance = extraterrestial_normal_irradiance * sin(
+        solar_altitude.value
+    )
+    # Dhc [W.m-2]
+    diffuse_horizontal_component = (
+        extraterrestial_normal_irradiance
+        * diffuse_transmission_function(solar_altitude.value)
+        * diffuse_solar_altitude_function(solar_altitude.value, linke_turbidity_factor)
+    )
+
+    reflected_irradiance = calculate_ground_reflected_inclined_irradiance(
+        longitude=longitude,
+        latitude=latitude,
+        elevation=elevation,
+        timestamp=timestamp,
+        timezone=timezone,
+        surface_tilt=surface_tilt,
+        surface_orientation=surface_orientation,
+        linke_turbidity_factor=linke_turbidity_factor,
+        apply_atmospheric_refraction=apply_atmospheric_refraction,
+        albedo=albedo,
+        direct_horizontal_component=direct_horizontal_irradiance, # time series, optional
+        apply_angular_loss_factor=apply_angular_loss_factor,
+        solar_time_model=solar_time_model,
+        time_offset_global=time_offset_global,
+        hour_offset=hour_offset,
+        solar_constant=solar_constant,
+        days_in_a_year=days_in_a_year,
+        perigee_offset=perigee_offset,
+        eccentricity_correction_factor=eccentricity_correction_factor,
+        time_output_units=time_output_units,
+        angle_units=angle_units,
+        angle_output_units=angle_output_units,
+    )
+    shortwave_irradiance = direct_irradiance + diffuse_irradiance
+
+    return shortwave_irradiance
