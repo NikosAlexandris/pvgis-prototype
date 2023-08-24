35604b9d32b1633ea1b48f8c694427714a91689b
Nikos Alexandris <nikosalexandris@duck.com>
2023-08-24 17:13:20 +0300

    Add model_solar_declination(), update calculate_solar_declination()
    Commit concerns api/geometry/solar_declination.py and is the same work as previous commits in solar_altitude.py, solar_azimuth.ch

--- pvgisprototype/api/geometry/solar_declination.py
+++ pvgisprototype/api/geometry/solar_declination.py
@@ -1,112 +1,99 @@
 from devtools import debug
-import typer
-from typing import Optional
+from typing import List
 from datetime import datetime
 from zoneinfo import ZoneInfo
 from math import pi
 from math import sin
 from math import asin
-from ..utilities.conversions import convert_to_degrees_if_requested
-
-from pvgisprototype.api.data_classes import SolarDeclination
-from pvgisprototype.api.data_classes import FractionalYear
-
-from pvgisprototype.api.input_models import SolarDeclinationInput
-from pvgisprototype.api.input_models import FractionalYearInput
 
 from pvgisprototype.api.decorators import validate_with_pydantic
+from pvgisprototype.api.function_models import CalculateFractionalYearPVISInputModel
+from pvgisprototype.api.data_classes import FractionalYear
+from pvgisprototype.api.function_models import CalculateSolarDeclinationPVISInputModel
+from pvgisprototype.api.data_classes import SolarDeclination
+from .models import SolarDeclinationModels
+from ..utilities.conversions import convert_to_degrees_if_requested
+from pvgisprototype.models.pvis.solar_declination import calculate_solar_declination_pvis
+from pvgisprototype.models.noaa.solar_declination import calculate_solar_declination_noaa
+from pvgisprototype.models.hargreaves.solar_declination import calculate_solar_declination_hargreaves
 
 
-@validate_with_pydantic(FractionalYearInput, expand_args=True)
-def calculate_fractional_year_pvis(
-            timestamp: datetime,
-            days_in_a_year: float = 365.25,
-            angle_units: str = 'radians',
-            angle_output_units: str = 'radians',
-        ) -> FractionalYear:
-    """Calculate fractional year in radians
-
-    Notes
-    -----
-    In PVGIS' source code, this is called `day_angle`"""
-    year = timestamp.year
-    start_of_year = datetime(year=year, month=1, day=1)
-    day_of_year = timestamp.timetuple().tm_yday
-    fractional_year = 2 * pi * day_of_year / days_in_a_year
-
-    # NOAA's corresponding equation
-    # fractional_year = (
-    #     2
-    #     * pi
-    #     / 365
-    #     * (timestamp.timetuple().tm_yday - 1 + float(timestamp.hour - 12) / 24)
-    # )
-
-    if not 0 <= fractional_year < 2 * pi:
-        raise ValueError('Fractional year (in radians) must be in the range [0, 2*pi]')
-    fractional_year = FractionalYear(value=fractional_year, unit='radians')
-    # fractional_year = convert_to_degrees_if_requested(fractional_year, angle_output_units)
-            
-    return fractional_year
-
-
-@validate_with_pydantic(SolarDeclinationInput, expand_args=True)
-def calculate_solar_declination(
+def model_solar_declination(
     timestamp: datetime,
-    timezone: ZoneInfo,
+    timezone: ZoneInfo = None,
+    model: SolarDeclinationModels = SolarDeclinationModels.pvis,
     days_in_a_year: float = 365.25,
-    orbital_eccentricity: float = 0.03344,
     perigee_offset: float = 0.048869,
-    angle_output_units: str = "radians",
+    eccentricity_correction_factor: float = 0.01672,
+    angle_output_units: str = 'radians',
 ) -> SolarDeclination:
-    """Approximate the sun's declination for a given day of the year.
+    """ """
+    if model.value == SolarDeclinationModels.noaa:
 
-    The solar declination is the angle between the Sun's rays and the
-    equatorial plane of Earth. It varies throughout the year due to the tilt of
-    the Earth's axis and is an important parameter in determining the seasons
-    and the amount of solar radiation received at different latitudes.
+        solar_declination = calculate_solar_declination_noaa(
+            timestamp=timestamp,
+            angle_output_units=angle_output_units
+        )
+        solar_declination = convert_to_degrees_if_requested(
+            solar_declination,
+            angle_output_units,
+        )
 
-    The function calculates the `proportion` of the way through the year (in
-    radians), which is given by `(2 * pi * day_of_year) / 365.25`.
-    The `0.3978`, `1.4`, and `0.0355` are constants in the approximation
-    formula, with the `0.0489` being an adjustment factor for the slight
-    eccentricity of Earth's orbit.
-  
-    Parameters
-    ----------
-    day_of_year: int
-        The day of the year (ranging froSolarAzimuthm 1 to 365 or 366 in a leap year).
+    if model.value  == SolarDeclinationModels.pvis:
 
-    Returns
-    -------
-    solar_declination: float
-        The solar declination in radians for the given day of the year.
+        solar_declination = calculate_solar_declination_pvis(
+            timestamp=timestamp,
+            timezone=timezone,
+            days_in_a_year=days_in_a_year,
+            eccentricity_correction_factor=eccentricity_correction_factor,
+            perigee_offset=perigee_offset,
+            angle_output_units=angle_output_units,
+        )
+        solar_declination = convert_to_degrees_if_requested(
+            solar_declination,
+            angle_output_units,
+        )
 
-    Notes
-    -----
+    if model.value  == SolarDeclinationModels.hargreaves:
 
-    The equation used here is a simple approximation and bases upon a direct
-    translation from PVGIS' rsun3 source code:
-
-      - from file: rsun_base.cpp
-      - function: com_declin(no_of_day)
-
-    For more accurate calculations of solar position, comprehensive models like
-    the Solar Position Algorithm (SPA) are typically used.
-    """
-    fractional_year = calculate_fractional_year_pvis(
+        solar_declination = calculate_solar_declination_hargreaves(
             timestamp=timestamp,
             days_in_a_year=days_in_a_year,
-            angle_output_units='radians',
-            )
-    declination = asin(
-            0.3978 * sin(
-                fractional_year.value - 1.4 + orbital_eccentricity * sin(
-                    fractional_year.value - perigee_offset
-                    )
-                )
-            )
-    declination = SolarDeclination(value=declination, unit='radians')
-    # declination = convert_to_degrees_if_requested(declination, angle_output_units)
+            angle_output_units=angle_output_units,
+        ) # returns values in degrees by default
 
-    return declination
+    return solar_declination
+
+
+def calculate_solar_declination(
+    timestamp: datetime,
+    timezone: ZoneInfo = None,
+    local_time: bool = False,
+    random_time: bool = False,
+    models: List[SolarDeclinationModels] = [SolarDeclinationModels.pvis],
+    days_in_a_year: float = 365.25,
+    perigee_offset: float = 0.048869,
+    eccentricity_correction_factor: float = 0.01672,
+    angle_output_units: str = 'radians',
+) -> List:
+    """Calculate the solar declination angle"""
+    debug(locals())
+    results = []
+    for model in models:
+        if model != SolarDeclinationModels.all:  # ignore 'all' in the enumeration
+            solar_declination = model_solar_declination(
+                timestamp=timestamp,
+                timezone=timezone,
+                model=model,
+                days_in_a_year=days_in_a_year,
+                perigee_offset=perigee_offset,
+                eccentricity_correction_factor=eccentricity_correction_factor,
+                angle_output_units=angle_output_units,
+            )
+            results.append({
+                'Model': model.value,
+                'Declination': solar_declination.value,
+                'Units': solar_declination.unit,  # Don't trust me -- Redesign Me!
+            })
+
+    return results
