743e01f9f5cf4cf271afe1400a9341e8db4cc607
Nikos Alexandris <nikosalexandris@duck.com>
2023-08-24 17:04:40 +0300

    Move models in models.py
    - Add : SolarIncidenceModels, SolarDeclinationModels
    - Remove solar_models.py, move content to models.py
    - Remove time_models.py move content to models.py

---
+++ pvgisprototype/api/geometry/models.py
@@ -0,0 +1,56 @@
+from enum import Enum
+
+
+class SolarIncidenceModels(str, Enum):
+    all = 'all'
+    jenco = 'Jenco'
+    effective = 'effective'
+    # pvis = 'pvis'
+
+
+class SolarDeclinationModels(str, Enum):
+    all = 'all'
+    noaa = 'NOAA'
+    pvis = 'pvis'
+    hargreaves = 'Hargreaves'
+    # pvgis = 'PVGIS'
+
+
+class SolarPositionModels(str, Enum):
+    all = 'all'
+    noaa = 'NOAA'
+    pysolar = 'pysolar'
+    pvis = 'pvis'
+    # pvgis = 'PVGIS'
+    suncalc = 'suncalc'
+    skyfield = 'Skyfield'
+
+
+class SolarTimeModels(str, Enum):
+    all = 'all'
+    eot = 'EoT'
+    ephem = 'ephem'
+    noaa = 'NOAA'
+    pvgis = 'PVGIS'
+    skyfield = 'Skyfield'
+
+
+# def _parse_model(
+#         ctx: typer.Context,
+#         model: List[SolarPositionModels],
+#         param: typer.CallbackParam,
+#     ) -> List[SolarPositionModels]:
+
+#     print(f'ctx : {ctx}')
+#     print(f'param : {param}')
+#     print(f'model : {model}')
+#     print()
+#     if ctx.resilient_parsing:
+#         return
+#     if SolarPositionModels.all in model:
+#         # Return all models except for the 'all' itself!
+#         model = [model for model in SolarPositionModels if model != SolarPositionModels.all]
+#     print(f"Return model: {model}")
+#     return model
+
+
