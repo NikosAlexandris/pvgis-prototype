61d600676360c735e6f5b6140d786915b217e76b
Nikos Alexandris <nikosalexandris@duck.com>
2023-08-28 10:21:39 +0300

    Update plot_solar_declination.py

--- pvgisprototype/plot/plot_solar_declination.py
+++ pvgisprototype/plot/plot_solar_declination.py
@@ -1,8 +1,10 @@
 from devtools import debug
+from typing import List
 import matplotlib.pyplot as plt
 from datetime import datetime
 from datetime import timedelta
 from datetime import timezone
+from zoneinfo import ZoneInfo
 import numpy as np
 from bokeh.embed import components
 from bokeh.embed import json_item
@@ -12,7 +14,10 @@ from bokeh.models import LegendItem
 from bokeh.plotting import figure
 from bokeh.plotting import output_file
 from bokeh.plotting import save
+from pvgisprototype.api.geometry.models import SolarDeclinationModels
+
 from pvgisprototype.api.geometry.solar_declination import calculate_solar_declination
+from pvgisprototype.models.noaa.solar_declination import calculate_solar_declination_noaa
 from pvgisprototype.models.hargreaves.solar_declination import calculate_solar_declination_hargreaves
 from pvgisprototype.models.pvgis.solar_declination import calculate_solar_declination_pvgis
 
@@ -27,20 +32,40 @@ def generate_timestamps(start_date: datetime, end_date: datetime):
     return [start_date + timedelta(days=i) for i in range((end_date - start_date).days)]
 
 
-def calculate_declinations(timestamps, output_units="radians"):
+def calculate_declinations(
+    timestamps: List, timezone: ZoneInfo = None, output_units="radians"
+):
     solar_declinations = np.vectorize(calculate_solar_declination)(
-        timestamps, angle_output_units=output_units
-    )  # output_units='degrees'
+        timestamp=timestamps,
+        timezone=ZoneInfo('UTC'),
+        # models=[SolarDeclinationModels.noaa],
+        angle_output_units=output_units, # in degrees
+    )
+    # restructure for plotting
+    solar_declinations = [item[0]['Declination'] for item in solar_declinations]
 
     solar_declinations_pvgis = np.vectorize(calculate_solar_declination_pvgis)(
-        timestamps, angle_output_units=output_units
-    )  # output_units='degrees'
+        timestamp=timestamps,
+        angle_output_units=output_units, # in degrees
+    )
+    # restructure for plotting - SolarDeclination objects with `value`, `unit` attributes
+    solar_declinations_pvgis = [item.value for item in solar_declinations_pvgis]
 
-    solar_declinations_hargreaves = np.vectorize(
-        calculate_solar_declination_hargreaves
-    )(timestamps, angle_output_units=output_units)
+    solar_declinations_noaa = np.vectorize(calculate_solar_declination_noaa)(
+        timestamp=timestamps,
+        angle_output_units=output_units, # in degrees
+    )
+    # restructure for plotting - SolarDeclination objects with `value`, `unit` attributes
+    solar_declinations_noaa = [item.value for item in solar_declinations_noaa]
 
-    return solar_declinations, solar_declinations_pvgis, solar_declinations_hargreaves
+    solar_declinations_hargreaves = np.vectorize(calculate_solar_declination_hargreaves)(
+        timestamp=timestamps,
+        angle_output_units=output_units, # in degrees
+    )
+    # restructure for plotting
+    solar_declinations_hargreaves = [item.value for item in solar_declinations_hargreaves]
+
+    return solar_declinations, solar_declinations_pvgis, solar_declinations_noaa, solar_declinations_hargreaves
     
 
 def plot_solar_declination(
@@ -57,13 +82,48 @@ def plot_solar_declination(
         raise ValueError('Either `start_date` and `end_date` or only `year` should be provided')
 
     timestamps = generate_timestamps(start_date, end_date)
-    solar_declinations, solar_declinations_pvgis, solar_declinations_hargreaves = calculate_declinations(timestamps, output_units)
+    solar_declinations, solar_declinations_pvgis, solar_declinations_noaa, solar_declinations_hargreaves = calculate_declinations(timestamps, output_units)
 
     fig = plt.figure(figsize=(10,6))
     # plt.plot(timestamps, solar_declinations, linewidth=4, alpha=0.7, label='PVIS', linestyle='-', color='#66CCCC')
-    plt.plot(timestamps, solar_declinations, linewidth=4, alpha=0.7, label='PVIS', linestyle='-', color='#00BFFF')
-    plt.plot(timestamps, solar_declinations_pvgis, linewidth=2.0, alpha=0.35, label='PVGIS (current C code)', linestyle='--', color='red')
-    plt.plot(timestamps, solar_declinations_hargreaves, linewidth=2.0, alpha=1.0, label='Hargreaves', linestyle=':', color='#9966CC')
+
+    plt.plot(
+        timestamps,
+        solar_declinations,
+        linewidth=4,
+        alpha=0.7,
+        label="PVIS",
+        linestyle="-",
+        color="#00BFFF",
+    )
+    plt.plot(
+        timestamps,
+        solar_declinations_pvgis,
+        linewidth=2.0,
+        alpha=0.35,
+        label="PVGIS (current C code)",
+        linestyle="--",
+        color="red",
+    )
+    plt.plot(
+        timestamps,
+        solar_declinations_noaa,
+        linewidth=2.0,
+        alpha=0.75,
+        label="NOAA",
+        linestyle="--",
+        color="#004600",
+    )
+    plt.plot(
+        timestamps,
+        solar_declinations_hargreaves,
+        linewidth=2.0,
+        alpha=1.0,
+        label="Hargreaves",
+        linestyle=":",
+        color="#9966CC",
+    )
+
     plt.xlabel('Day of the Year')
     plt.ylabel(f'{output_units}')
     plt.title(title)
@@ -96,6 +156,7 @@ def plot_solar_declination_one_year_bokeh(
     timestamps_float = [timestamp.toordinal() for timestamp in timestamps]  # Bokeh doesn't handle datetime
     solar_declinations = calculate_solar_declination(timestamp=timestamps, angle_output_units=angle_output_units)
     solar_declinations_pvgis = calculate_solar_declination_pvgis(timestamps, angle_output_units)
+    # debug(locals())
     solar_declinations_hargreaves = calculate_solar_declination_hargreaves(timestamps, angle_output_units)
 
     fig = figure(width=800, height=600, title=title, x_axis_type="datetime")
