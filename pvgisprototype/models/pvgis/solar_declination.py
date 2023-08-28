347309c00fda011d8526d2de98da3fea7e06880d
Nikos Alexandris <nikosalexandris@duck.com>
2023-08-28 09:58:54 +0300

    Update models/pvgis/solar_declination.py
    - Clean imports
    - Update use of pydantic model and parameters for calculate_solar_declination_pvgis()

--- pvgisprototype/models/pvgis/solar_declination.py
+++ pvgisprototype/models/pvgis/solar_declination.py
@@ -6,23 +6,22 @@ from datetime import datetime
 from datetime import timezone
 from pvgisprototype.api.utilities.timestamp import now_utc_datetimezone
 from pvgisprototype.api.utilities.timestamp import ctx_convert_to_timezone
-from pvgisprototype.api.geometry.solar_declination import calculate_solar_declination
+from pvgisprototype.models.pvis.solar_declination import calculate_solar_declination_pvis
 
-from pvgisprototype.api.data_classes import SolarDeclination
-from pvgisprototype.api.input_models import SolarDeclinationInput
 from pvgisprototype.api.decorators import validate_with_pydantic
+from pvgisprototype.api.function_models import CalculateSolarDeclinationPVISInputModel
+from pvgisprototype.api.data_classes import SolarDeclination
 
 
-@validate_with_pydantic(SolarDeclinationInput, expand_args=True)
+@validate_with_pydantic(CalculateSolarDeclinationPVISInputModel, expand_args=True)
 def calculate_solar_declination_pvgis(
-        timestamp: datetime,
-        timezone: str = None,
-        days_in_a_year: float = 365.25,
-        eccentricity: float = 0.03344,
-        perigee_offset: float = 0.048869,
-        output_units: str = 'radians',
-    ) -> SolarDeclination:
+    timestamp: datetime,
+    timezone: str = None,
+    days_in_a_year: float = 365.25,
     eccentricity_correction_factor: float = 0.03344,
+    perigee_offset: float = 0.048869,
+    angle_output_units: str = 'radians',
+) -> SolarDeclination:
     """Approximate the sun's declination for a given day of the year.
 
     This function is a 1:1 transfer of the solar declination calculation
@@ -43,18 +42,18 @@ def calculate_solar_declination_pvgis(
     inverted again at some other part of the program when it gets to read data.
     """
     day_of_year = timestamp.timetuple().tm_yday
-    solar_declination = calculate_solar_declination(
+    solar_declination = calculate_solar_declination_pvis(
         timestamp,
         timezone,
         days_in_a_year,
         eccentricity_correction_factor,
         perigee_offset,
-        output_units,
+        angle_output_units,
         )
     
     solar_declination = SolarDeclination(
         value=-solar_declination.value,
-        unit=solar_declination.units,
+        unit=solar_declination.unit,
         )
 
     return solar_declination
