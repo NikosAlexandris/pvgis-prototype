f5d85946e1ae7823b6f8b1d211abac098b13b81a
Nikos Alexandris <nikosalexandris@duck.com>
2023-08-28 10:11:50 +0300

    Update cli/time.py

--- pvgisprototype/cli/time.py
+++ pvgisprototype/cli/time.py
@@ -22,12 +22,37 @@ from ..api.utilities.timestamp import now_utc_datetimezone
 from ..api.utilities.timestamp import random_datetimezone
 from ..api.utilities.conversions import convert_to_radians
 from ..api.utilities.conversions import convert_to_degrees_if_requested
-from ..api.geometry.time_models import SolarTimeModels
+from ..api.geometry.models import SolarTimeModels
 from ..api.geometry.solar_time import calculate_solar_time
 from ..api.geometry.solar_hour_angle import calculate_hour_angle
 
 from .typer_parameters import OrderCommands
+from .typer_parameters import typer_argument_longitude
+from .typer_parameters import typer_argument_latitude
+from .typer_parameters import typer_argument_timestamp
+from .typer_parameters import typer_option_timezone
+from .typer_parameters import typer_option_local_time
+from .typer_parameters import typer_option_random_time
+from .typer_parameters import typer_argument_solar_declination
+from .typer_parameters import typer_argument_surface_tilt
+from .typer_parameters import typer_argument_surface_orientation
+from .typer_parameters import typer_argument_hour_angle
+from .typer_parameters import typer_argument_solar_time
+from .typer_parameters import typer_option_solar_position_model
+from .typer_parameters import typer_option_solar_time_model
+from .typer_parameters import typer_option_global_time_offset
+from .typer_parameters import typer_option_hour_offset
+from .typer_parameters import typer_option_days_in_a_year
+from .typer_parameters import typer_option_perigee_offset
 from .typer_parameters import typer_option_eccentricity_correction_factor
+from .typer_parameters import typer_option_apply_atmospheric_refraction
+from .typer_parameters import typer_option_refracted_solar_zenith
+from .typer_parameters import typer_option_time_output_units
+from .typer_parameters import typer_option_angle_units
+from .typer_parameters import typer_option_angle_output_units
+from .typer_parameters import typer_option_rounding_places
+from .typer_parameters import typer_option_verbose
+
 
 app = typer.Typer(
     cls=OrderCommands,
@@ -83,61 +108,23 @@ def offset(
     help='⦩ Calculate the solar time'
 )
 def solar_time(
-        longitude: Annotated[float, typer.Argument(
-            callback=convert_to_radians,
-            min=-180, max=180)],
-        latitude: Annotated[float, typer.Argument(
-            callback=convert_to_radians,
-            min=-90, max=90)],
-        timestamp: Annotated[Optional[datetime], typer.Argument(
-            help='Timestamp',
-            default_factory=now_utc_datetimezone,
-            callback=ctx_attach_requested_timezone,
-            )],
-        timezone: Annotated[Optional[str], typer.Option(
-            help='Specify timezone (e.g., "Europe/Athens"). Use "local" to use the system\'s time zone',
-            callback=ctx_convert_to_timezone)] = None,
-        model: Annotated[List[SolarTimeModels], typer.Option(
-            '-m',
-            '--model',
-            help="Model to calculate solar time",
-            show_default=True,
-            show_choices=True,
-            case_sensitive=False)] = [SolarTimeModels.skyfield],
-        refracted_solar_zenith: float = 1.5853349194640094,  # radians
-        apply_atmospheric_refraction: Annotated[Optional[bool], typer.Option(
-            '-a',
-            '--atmospheric-refraction',
-            help='Apply atmospheric refraction functions',
-            )] = True,
-        days_in_a_year: Annotated[float, typer.Option(
-            help='Days in a year')] = 365.25,
-        perigee_offset: Annotated[float, typer.Option(
-            help='Perigee offset')] = 0.048869,
-        time_offset_global: Annotated[float, typer.Option(
-            help='Global time offset')] = 0,
-        hour_offset: Annotated[float, typer.Option(
-            help='Hour offset')] = 0,
-        time_output_units: Annotated[str, typer.Option(
-            '-u',
-            '--output-units',
-            show_default=True,
-            case_sensitive=False,
-            help="Time units for output and internal calculations (seconds, minutes or hours) - :warning: [bold red]Keep fingers away![/bold red]")] = 'minutes',
-        angle_units: Annotated[str, typer.Option(
-            '-u',
-            '--units',
-            show_default=True,
-            case_sensitive=False,
-            help="Angular units for internal calculations (degrees or radians) - :warning: [bold red]Keep fingers away![/bold red]")] = 'radians',
-        angle_output_units: Annotated[str, typer.Option(
-            '-u',
-            '--units',
-            show_default=True,
-            case_sensitive=False,
-            help="Angular units for solar position calculations output (degrees or radians)")] = 'radians',
-        ):
+    longitude: Annotated[float, typer_argument_longitude],
+    latitude: Annotated[float, typer_argument_latitude],
+    timestamp: Annotated[Optional[datetime], typer_argument_timestamp],
+    timezone: Annotated[Optional[str], typer_option_timezone] = None,
+    solar_time_model: Annotated[List[SolarTimeModels], typer_option_solar_time_model] = [SolarTimeModels.skyfield],
+    apply_atmospheric_refraction: Annotated[Optional[bool], typer_option_apply_atmospheric_refraction] = True,
+    refracted_solar_zenith: Annotated[Optional[float], typer_option_refracted_solar_zenith] = 1.5853349194640094,  # radians
+    days_in_a_year: Annotated[float, typer_option_days_in_a_year] = 365.25,
+    perigee_offset: Annotated[float, typer_option_perigee_offset] = 0.048869,
     eccentricity_correction_factor: Annotated[float, typer_option_eccentricity_correction_factor] = 0.03344,
+    time_offset_global: Annotated[float, typer_option_global_time_offset] = 0,
+    hour_offset: Annotated[float, typer_option_hour_offset] = 0,
+    time_output_units: Annotated[str, typer_option_time_output_units] = 'minutes',
+    angle_units: Annotated[str, typer_option_angle_units] = 'radians',
+    angle_output_units: Annotated[str, typer_option_angle_output_units] = 'radians',
+    verbose: Annotated[Optional[bool], typer_option_verbose]= False,
+):
     """Calculate the solar time.
 
     1. Map the day of the year onto the circumference of a circle, essentially
@@ -163,36 +150,36 @@ def solar_time(
         typer.echo(f'The requested timestamp - zone {user_requested_timestamp} {user_requested_timezone} has been converted to {timestamp} for all internal calculations!')
     
     # Why does the callback function `_parse_model` not work? 
-    if SolarTimeModels.all in model:
-        model = [model for model in SolarTimeModels if model != SolarTimeModels.all]
+    if SolarTimeModels.all in solar_time_model:
+        solar_time_model = [model for model in SolarTimeModels if model != SolarTimeModels.all]
 
-    solar_time  = calculate_solar_time(
-            longitude,
-            latitude,
-            timestamp,
-            timezone,
-            model,
-            refracted_solar_zenith,
-            apply_atmospheric_refraction,
-            time_output_units,
-            angle_output_units,
-            days_in_a_year,
-            perigee_offset,
-            time_offset_global,
-            hour_offset,
-            )
-    solar_time_table = Table('Model', 'Solar time', 'Units',
+    solar_time = calculate_solar_time(
+        longitude=longitude,
+        latitude=latitude,
+        timestamp=timestamp,
+        timezone=timezone,
+        models=solar_time_model,  # keep the CLI simple
+        apply_atmospheric_refraction=apply_atmospheric_refraction,
+        refracted_solar_zenith=refracted_solar_zenith,
+        days_in_a_year=days_in_a_year,
+        perigee_offset=perigee_offset,
         eccentricity_correction_factor=eccentricity_correction_factor,
+        time_offset_global=time_offset_global,
+        hour_offset=hour_offset,
+        time_output_units=time_output_units,
+        angle_output_units=angle_output_units,
+    )
+    solar_time_table = Table('Model', 'Solar time', #'Units',
                                  box=box.SIMPLE_HEAD)
     for model_result in solar_time:
         # typer.echo(f'Solar time: {solar_time} {units} ({timezone})')
         model_name = model_result.get('Model', '')
         solar_time = model_result.get('Solar time', '')
-        units = model_result.get('Units', '')
+        # units = model_result.get('Units', '')
         solar_time_table.add_row(
                 model_name,
                 str(solar_time),
-                str(units),
+                # str(units),
         )
     console.print(solar_time_table)
 
