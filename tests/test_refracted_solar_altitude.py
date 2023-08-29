52eff18f8ae7a3d9a8836f166be82067480a0ca4
Nikos Alexandris <nikosalexandris@duck.com>
2023-08-29 20:13:47 +0300

    Add test_refracted_solar_altitude.py

---
+++ tests/test_refracted_solar_altitude.py
@@ -0,0 +1,43 @@
+import pytest
+from pvgisprototype.api.irradiance.direct import calculate_refracted_solar_altitude_time_series
+from pvgisprototype.api.data_classes import SolarAltitude
+import numpy as np
+
+
+tolerances = [1, 0, 0.5, 0.1, 0.01, 0.001, 0.0001]
+@pytest.mark.parametrize(
+    "input_solar_altitude_values, expected_solar_altitude_values, angle_input_units, angle_output_units",
+    [
+        (
+            [30.0, 40.0, 50.0],
+            [30.1, 40.1, 50.1],
+            "degrees",
+            "radians",
+        ),  # Add more test cases as needed
+    ],
+)
+@pytest.mark.parametrize('tolerance', tolerances)
+def test_calculate_refracted_solar_altitude_time_series(
+    input_solar_altitude_values,
+    expected_solar_altitude_values,
+    angle_input_units,
+    angle_output_units,
+    tolerance,
+):
+    input_solar_altitude_series = np.array(
+        [
+            SolarAltitude(value=value, unit=angle_input_units)
+            for value in input_solar_altitude_values
+        ],
+        dtype=object,
+    )
+
+    calculated_refracted_solar_altitude_series = calculate_refracted_solar_altitude_time_series(
+        input_solar_altitude_series,
+        angle_input_units,
+        angle_output_units,
+    )
+    calculated_values = [item.value for item in calculated_refracted_solar_altitude_series]
+
+    assert len(expected_solar_altitude_values) == len(calculated_values)
+    assert np.allclose(expected_solar_altitude_values, calculated_values, atol=tolerance)
