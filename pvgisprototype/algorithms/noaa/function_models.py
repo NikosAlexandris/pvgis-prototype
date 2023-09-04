9552aed22b7750b7e6aabef57c6c1b7abdb331e7
Nikos Alexandris <nikosalexandris@duck.com>
2023-09-04 14:45:31 +0300

    [SquashMe] Add minor comment for BaseTimestampSeriesModel

--- pvgisprototype/algorithms/noaa/function_models.py
+++ pvgisprototype/algorithms/noaa/function_models.py
@@ -55,7 +55,7 @@ class CalculateEquationOfTimeNOAAInput(
 
 
 class CalculateEquationOfTimeTimeSeriesNOAAInput(
-    BaseTimestampSeriesModel,
+    BaseTimestampSeriesModel,  # != BaseTimestampModel
     BaseTimeOutputUnitsModel,
 ):
     pass
