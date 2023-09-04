f3d1ddab254a1db8838eb2b8a3d69959b9738c9b
Olyna <oly.gounari@gmail.com>
2023-09-04 14:04:36 +0300

    Add tests for: -solar altitude -solar azimuth -solar declination

--- tests/test_pvlib_solar_altitude.py
+++ tests/test_pvlib_solar_altitude.py
@@ -9,60 +9,51 @@ from pvgisprototype import SolarAltitude
 import numpy as np
 import matplotlib.pyplot as plt
 from pvgisprototype.api.utilities.timestamp import random_datetimezone
+from .cases_solar_altitude import cases_for_solar_altitude
+
+
+tolerances = [
+    1,
+    0.5,
+    0.1,
+    # 0.01,
+    # 0.001,
+    # 0.0001,
+]
+
+@pytest.mark.parametrize(
+    "longitude, latitude, timestamp, timezone, angle_output_units, expected",
+    cases_for_solar_altitude,
+)
+@pytest.mark.parametrize('tolerance', tolerances)
+def test_calculate_solar_altitude_pvlib(
+    longitude,
+    latitude,
+    timestamp,
+    timezone,
+    angle_output_units,
+    expected,
+    tolerance,
+):
+    """ """
+    calculated = calculate_solar_altitude_pvlib(
+        longitude=longitude,
+        latitude=latitude,
+        timestamp=timestamp,
+        timezone=timezone,
+        angle_output_units=angle_output_units,
+    )
+    assert pytest.approx(expected, abs=tolerance) == calculated.value
+    assert angle_output_units == calculated.unit
+    assert (-np.pi/2 <= calculated.value <= np.pi/2) or (-90 <= calculated.value <= 90)
+
+
+
 
 
 def generate_timestamps_for_a_year(start_date=datetime(2023, 1, 1), frequency_minutes=60):
     timestamps = [start_date + timedelta(minutes=i * frequency_minutes) for i in range(365 * 24 * 60 // frequency_minutes)]
     return timestamps
-
-
-longitude = Longitude(value=0.1, unit='radians')
-latitude = Latitude(value=0.1, unit='radians')
-timestamps_for_a_year = generate_timestamps_for_a_year()
-timezone = ZoneInfo('UTC')
-# apply_atmospheric_refraction = False
-# time_output_units = 'minutes'
-angle_output_units = 'radians'
-
-
-test_cases = [
-    (
-        Longitude(value=0.5, unit='radians'),
-        Latitude(value=0.5, unit='radians'),
-        [datetime(2023, 8, 25, 0, 0), datetime(2023, 8, 26, 0, 0)],
-        ZoneInfo('UTC'),
-        'radians',
-    ),
-    # TODO: Add more test cases here
-]
-
-
-@pytest.mark.parametrize(
-    "longitude, latitude, timestamps, timezone, angle_output_units",
-    test_cases,
-)
-def test_calculate_solar_altitude_pvlib(
-    longitude,
-    latitude,
-    timestamps,
-    timezone,
-    angle_output_units,
-):
-    """ """
-    calculated_altitude_series = calculate_solar_altitude_pvlib(
-        longitude=longitude,
-        latitude=latitude,
-        timestamp=timestamps,
-        timezone=timezone,
-        angle_output_units=angle_output_units,
-    )
-    # assert np.ndarray == type(calculated_altitude_series)
-    assert len(timestamps) == len(calculated_altitude_series)
-    assert all(SolarAltitude == type(altitude) for altitude in calculated_altitude_series)
-    assert all((-np.pi/2 <= altitude.value <= np.pi/2) for altitude in calculated_altitude_series)
-    assert all(angle_output_units == altitude.unit for altitude in calculated_altitude_series)
-
-
 
 
 def plot_solar_altitude_pvlib(
@@ -73,13 +64,14 @@ def plot_solar_altitude_pvlib(
     angle_output_units,
     expected_solar_altitude_series,
 ):
-    calculated_solar_altitude_series = calculate_solar_altitude_pvlib(
-        longitude=longitude,
-        latitude=latitude,
-        timestamps=timestamps,
-        timezone=timezone,
-        angle_output_units=angle_output_units,
-    )
+    calculated_solar_altitude_series = [calculate_solar_altitude_pvlib(
+        longitude,
+        latitude,
+        timestamp,
+        timezone,
+        angle_output_units,
+    ) for timestamp in timestamps]
+
     figure, ax = plt.subplots()
     ax.plot(
         timestamps,
@@ -103,6 +95,16 @@ def get_expected_solar_altitudes(timestamps, longitude, latitude):
     # Here, you can use a reference model or data to get the expected solar altitudes
     # For demonstration purposes, I'm returning an array of SolarAltitude objects with dummy values
     return [SolarAltitude(value=0.5, unit='radians') for _ in timestamps]
+
+
+
+
+timestamps_for_a_year = generate_timestamps_for_a_year()
+longitude = Longitude(value=0.5585053606381855, unit='radians')
+latitude = Latitude(value=0.47123889803846897, unit='radians')
+# timestamp = datetime(2024, 9, 4, 7, 32)
+timezone = ZoneInfo('UTC')
+angle_output_units = 'degrees'
 
 
 @pytest.mark.mpl_image_compare
