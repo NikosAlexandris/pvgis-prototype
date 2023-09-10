fa01bc74ccc936a1599e54625436a9cc567d9df1
Olyna <oly.gounari@gmail.com>
2023-09-10 22:25:53 +0300

    Update calculate_solar_hour_angle_pvlib()

--- pvgisprototype/algorithms/pvlib/solar_hour_angle.py
+++ pvgisprototype/algorithms/pvlib/solar_hour_angle.py
@@ -7,6 +7,7 @@ from datetime import datetime
 from pvgisprototype import SolarHourAngle
 from math import pi
 import pvlib
+import pandas as pd
 
 
 @validate_with_pydantic(SolarHourAnglePVLIBInput)
@@ -17,16 +18,24 @@ def calculate_solar_hour_angle_pvlib(
 ) -> SolarHourAngle:
     """Calculate the solar hour angle in radians.
     """
+    equation_of_time = pvlib.solarposition.equation_of_time_spencer71(timestamp.timetuple().tm_yday)
+    
     longitude = convert_to_degrees_if_requested(longitude, 'degrees')
 
-    solar_hour_angle = pvlib.solarposition.hour_angle(timestamp, longitude.value, equation_of_time='apparent').values[0]
+    timestamp = pd.DatetimeIndex([timestamp.strftime("%Y/%m/%d %H:%M:%S.%f%z")])
 
-    if angle_output_units == 'radians' and not -pi <= solar_hour_angle <= pi:
-        raise ValueError(f'The calculated hour angle {solar_hour_angle} is out of the expected range [{-pi}, {pi}] radians')
+    solar_hour_angle = pvlib.solarposition.hour_angle(
+        timestamp,
+        longitude.value,
+        equation_of_time=equation_of_time
+        )
+
+    if angle_output_units == 'degrees' and not -180 <= solar_hour_angle <= 180:
+        raise ValueError(f'The calculated hour angle {solar_hour_angle} is out of the expected range [{-180}, {180}] degrees')
 
     solar_hour_angle = SolarHourAngle(
         value=solar_hour_angle,
-        unit='radians',
+        unit='degrees',
     )
     solar_hour_angle = convert_to_degrees_if_requested(solar_hour_angle, angle_output_units)
 
