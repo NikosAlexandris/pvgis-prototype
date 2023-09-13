58ba84a90a102330836704a9f21666bcf9077d63
Olyna <oly.gounari@gmail.com>
2023-09-13 15:27:07 +0300

    Fix values in overview table: - noaa azimuth - skyfield azimuth - noaa altitude - skyfield altitude - noaa zenith - skyfield zenith - noaa hour angle - skyfield hour angle

--- pvgisprototype/api/geometry/solar_position.py
+++ pvgisprototype/api/geometry/solar_position.py
@@ -57,7 +57,7 @@ def convert_south_to_north_degrees_convention(azimuth_south_degrees):
 
 
 def convert_south_to_north_radians_convention(azimuth_south_radians):
-    return (azimuth_south_radians + pi) % 2 * pi
+    return (azimuth_south_radians + pi) % (2 * pi)
 
 
 @validate_with_pydantic(ModelSolarPositionInputModel)
