d39c9233056bae058de90f45987ae4021bf410d9
Nikos Alexandris <nikosalexandris@duck.com>
2023-09-27 13:36:26 +0300

    Fix solar azimuth for NOAA!
    See also : https://gitlab.com/NikosAlexandris/photovoltaic-information-system/-/issues/23 (private repository currently!)

--- pvgisprototype/algorithms/noaa/solar_azimuth.py
+++ pvgisprototype/algorithms/noaa/solar_azimuth.py
@@ -76,9 +76,11 @@ def calculate_solar_azimuth_noaa(
     numerator = sin(latitude.value) * cos(solar_zenith.value) - sin(solar_declination.value)
     denominator = cos(latitude.value) * sin(solar_zenith.value)
     # try else raise ... ?
-    cosine_solar_azimuth = numerator / denominator
+    cosine_solar_azimuth = -1 * numerator / denominator
     solar_azimuth = acos(cosine_solar_azimuth)
 
+    if solar_hour_angle.value > 0:
+        solar_azimuth = 2 * pi - solar_azimuth
     if not isfinite(solar_azimuth) or not 0 <= solar_azimuth <= 2*pi:
         raise ValueError('The `solar_azimuth` should be a finite number ranging in [0, 2π] radians')
 
