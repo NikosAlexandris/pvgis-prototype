64525a958a4726f7b03ade952494654ea001fe26
Nikos Alexandris <nikosalexandris@duck.com>
2023-09-27 13:40:17 +0300

    Update calculate_true_solar_time_noaa()
    - Apply some formatting
    - Add notes
    - Simplify
    - Clean imports

--- pvgisprototype/algorithms/noaa/solar_time.py
+++ pvgisprototype/algorithms/noaa/solar_time.py
@@ -3,23 +3,25 @@ from pvgisprototype.validation.functions import validate_with_pydantic
 from pvgisprototype.algorithms.noaa.function_models import CalculateTrueSolarTimeNOAAInput
 from pvgisprototype.algorithms.noaa.function_models import CalculateTrueSolarTimeTimeSeriesNOAAInput
 from pvgisprototype import Longitude
-from datetime import datetime
 from typing import Optional
-from zoneinfo import ZoneInfo
-from .time_offset import calculate_time_offset_noaa
-from datetime import timedelta
 from typing import Union
 from typing import Sequence
+from .time_offset import calculate_time_offset_noaa
+from datetime import datetime
+from datetime import timedelta
+from zoneinfo import ZoneInfo
 
 
 @validate_with_pydantic(CalculateTrueSolarTimeNOAAInput)
 def calculate_true_solar_time_noaa(
-        longitude: Longitude,   # radians
-        timestamp: datetime, 
-        timezone: Optional[ZoneInfo],
-        time_output_units: str = 'minutes',
-        angle_units: str = 'radians',
-    ) -> datetime:
+    longitude: Longitude,   # radians
+    timestamp: datetime, 
+    timezone: Optional[ZoneInfo],
+    # time_offset_global: float,
+    time_output_units: str = 'minutes',
+    angle_units: str = 'radians',
+    verbose: int = VERBOSE_LEVEL_DEFAULT,
+) -> datetime:
     """Calculate the true solar time.
 
     The true solar time is the sum of the mean solar time and the equation of
@@ -54,6 +56,30 @@ def calculate_true_solar_time_noaa(
         4 * longitude_deg + equation_of_time(math.tm_yday(when)))
         ```
         part.
+
+    Additional notes:
+
+    From https://gml.noaa.gov/grad/solcalc/solareqns.PDF :
+
+    Next, the true solar time is calculated in the following two equations.
+    First the time offset is found, in minutes, and then the true solar time,
+    in minutes.
+
+        time_offset = eqtime + 4*longitude – 60*timezone
+
+        where :
+            - eqtime is in minutes,
+            - longitude is in degrees (positive to the east of the Prime
+              Meridian),
+            - timezone is in hours from UTC (U.S. Mountain Standard Time = –7
+              hours).
+
+        tst = hr*60 + mn + sc/60 + time_offset
+
+        where : 
+            - hr is the hour (0 - 23),
+            - mn is the minute (0 - 59),
+            - sc is the second (0 - 59).
     """
     time_offset = calculate_time_offset_noaa(
         longitude=longitude,
@@ -62,28 +88,20 @@ def calculate_true_solar_time_noaa(
         time_output_units=time_output_units,
         angle_units=angle_units,
         )  # in minutes
-    true_solar_time = (
-        timestamp.hour * 60 + timestamp.minute + timestamp.second / 60 + time_offset.value
-    )
 
+    time_offset_timedelta = timedelta(minutes=time_offset.value)
+    true_solar_time = timestamp + time_offset_timedelta
     if time_output_units == 'minutes':
         # if not 0 <= true_solar_time <= 1440:
         # if not 0 <= true_solar_time <= 1580:
         # if not -790 <= true_solar_time <= 790:
-        if not -1580 <= true_solar_time <= 1580:
-            raise ValueError(f'The calculated true solar time `{true_solar_time}` is out of the expected range [-720, 720] minutes!')
-
-    time_offset_timedelta = timedelta(minutes=true_solar_time)
-    true_solar_datetime = timestamp + time_offset_timedelta
-    true_solar_time = datetime(
-            year=true_solar_datetime.year,
-            month=true_solar_datetime.month,
-            day=true_solar_datetime.day,
-            hour=int(true_solar_datetime.hour),
-            minute=int(true_solar_datetime.minute),
-            second=int(true_solar_datetime.second),
-            tzinfo=true_solar_datetime.tzinfo,
-            )
+        true_solar_time_minutes = (
+            true_solar_time.hour * 60
+            + true_solar_time.minute
+            + true_solar_time.second / 60
+        )
+        if not -1580 <= true_solar_time_minutes <= 1580:
+            raise ValueError(f'The calculated true solar time `{true_solar_time_minutes}` is out of the expected range [-1580, 1580] minutes!')
 
     return true_solar_time
 
