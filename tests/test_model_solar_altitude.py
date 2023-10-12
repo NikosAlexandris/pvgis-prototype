9ed7db3650bd2dc0cb87967330031874acb424fb
Olyna <oly.gounari@gmail.com>
2023-10-12 12:02:23 +0300

    Tests for solar-geometry related functions: - calculate_apparent_solar_time_milne1921 - calculate_apparent_solar_time_noaa - calculate_equation_of_time_noaa - calculate_hour_angle - calculate_solar_altitude - calculate_solar_altitude_azimuth_skyfield - calculate_solar_altitude_noaa - calculate_solar_altitude_pvis - calculate_solar_altitude_pvlib - calculate_solar_azimuth - calculate_solar_azimuth_noaa - calculate_solar_azimuth_pvis - calculate_solar_azimuth_pvlib - calculate_solar_declination - calculate_solar_declination_hargreaves - calculate_solar_declination_noaa - calculate_solar_declination_pvgis - calculate_solar_declination_pvis - calculate_solar_declination_pvlib - calculate_solar_geometry_overview - calculate_solar_hour_angle_declination_skyfield - calculate_solar_hour_angle_noaa - calculate_solar_hour_angle_pvis - calculate_solar_hour_angle_pvlib - calculate_solar_time - calculate_solar_time_ephem - calculate_solar_time_pvgis - calculate_solar_time_skyfield - calculate_solar_zenith_noaa - calculate_solar_zenith_pvlib - model_solar_altitude - model_solar_azimuth - model_solar_declination - model_solar_geometry_overview - model_solar_time

---
+++ tests/test_model_solar_altitude.py
@@ -0,0 +1,73 @@
+import pytest
+from pvgisprototype.api.geometry.solar_altitude import model_solar_altitude
+from pvgisprototype.api.geometry.models import SolarTimeModels, SolarPositionModels
+from .helpers import read_noaa_spreadsheet, test_cases_from_data
+from pvgisprototype.constants import ALTITUDE_NAME
+from pvgisprototype import SolarAltitude
+
+
+test_cases_data = read_noaa_spreadsheet(
+    './tests/data/test_cases_noaa_spreadsheet.xlsx'
+)
+test_cases = test_cases_from_data(
+    test_cases_data,
+    against_unit='degrees',
+    longitude='longitude',
+    latitude='latitude',
+    timestamp='timestamp',
+    timezone='timezone',
+    altitude=ALTITUDE_NAME,
+)
+tolerances = [0.1]
+
+# FIXME: The combinations of the timing/position models are repeated
+solar_time_models = [
+    # SolarTimeModels.ephem,
+    SolarTimeModels.milne,
+    SolarTimeModels.noaa,
+    # SolarTimeModels.pvgis,
+    # SolarTimeModels.skyfield,
+]
+solar_position_models = [
+    SolarPositionModels.noaa,
+    SolarPositionModels.pvis,
+    SolarPositionModels.pvlib,
+    SolarPositionModels.pysolar,
+    SolarPositionModels.skyfield,
+    SolarPositionModels.suncalc,
+]
+
+@pytest.mark.parametrize(
+    "longitude, latitude, timestamp, timezone,\
+    expected_solar_altitude, against_unit", test_cases,
+)
+@pytest.mark.parametrize('solar_time_model', solar_time_models)
+@pytest.mark.parametrize('solar_position_model', solar_position_models)
+@pytest.mark.parametrize('tolerance', tolerances)
+def test_model_solar_altitude(
+    longitude,
+    latitude,
+    timestamp,
+    timezone,
+    expected_solar_altitude,
+    against_unit,
+    solar_position_model,
+    solar_time_model,
+    tolerance,
+):
+    calculated_solar_altitude = model_solar_altitude(
+        longitude=longitude,
+        latitude=latitude,
+        timestamp=timestamp,
+        timezone=timezone,
+        solar_position_model=solar_position_model,
+        solar_time_model=solar_time_model,
+    )
+
+    # Check types
+    assert isinstance(calculated_solar_altitude, SolarAltitude)
+
+    # Assert output
+    assert pytest.approx(
+        getattr(expected_solar_altitude, against_unit), tolerance) == getattr(
+            calculated_solar_altitude, against_unit)
--- tests/test_model_solar_geometry_overview.py
+++ tests/test_model_solar_geometry_overview.py
@@ -1,8 +1,7 @@
 import pytest
 from pvgisprototype.api.geometry.solar_position import model_solar_geometry_overview
-from pvgisprototype.api.geometry.models import SolarPositionModels
-from .helpers import read_test_cases_file, test_cases_from_data
-from pvgisprototype import RefractedSolarZenith
+from pvgisprototype.api.geometry.models import SolarTimeModels, SolarPositionModels
+from .helpers import read_noaa_spreadsheet, test_cases_from_data
 from pvgisprototype import SolarDeclination
 from pvgisprototype import SolarHourAngle
 from pvgisprototype import SolarZenith
@@ -12,48 +11,68 @@ from pvgisprototype.constants import (
     ALTITUDE_NAME,
     AZIMUTH_NAME,
     DECLINATION_NAME,
+    HOUR_ANGLE_NAME,
+    ZENITH_NAME,
 )
 
 
-test_cases_data = read_test_cases_file(
-    '/mnt/c/Users/olygo/Documents/projects/JRC/PVGIS/modernize/test_cases/Test_cases_extracted_from_NOAA_Solar_Calculator_copy.xlsx'
+test_cases_data = read_noaa_spreadsheet(
+    './tests/data/test_cases_noaa_spreadsheet.xlsx'
 )
 test_cases = test_cases_from_data(
     test_cases_data,
+    against_unit='radians',
+    longitude='longitude',
+    latitude='latitude',
+    timestamp='timestamp',
+    timezone='timezone',
     declination=DECLINATION_NAME,
+    hour_angle=HOUR_ANGLE_NAME,
+    zenith=ZENITH_NAME,
     altitude=ALTITUDE_NAME,
     azimuth=AZIMUTH_NAME,
 )
 
 tolerances = [0.1]      # 1, 0.5, 
 
-models = [
+# FIXME: The combinations of the timing/position models are repeated
+solar_time_models = [
+    # SolarTimeModels.ephem,
+    SolarTimeModels.milne,
+    SolarTimeModels.noaa,
+    # SolarTimeModels.pvgis,
+    # SolarTimeModels.skyfield,
+]
+solar_position_models = [
     SolarPositionModels.noaa,
-    SolarPositionModels.pysolar,
-    # SolarPositionModels.pvis,
-    # SolarPositionModels.pvgis,
-    SolarPositionModels.suncalc,
-    SolarPositionModels.skyfield,
+    SolarPositionModels.pvis,
     SolarPositionModels.pvlib,
+    SolarPositionModels.pysolar,
+    SolarPositionModels.skyfield,
+    SolarPositionModels.suncalc,
 ]
 
-
 @pytest.mark.parametrize(
-    "longitude, latitude, timestamp, timezone, angle_output_units,\
-    expected_declination, expected_altitude, expected_azimuth", test_cases,
+    "longitude, latitude, timestamp, timezone,\
+    expected_declination, expected_hour_angle, expected_zenith, expected_altitude,\
+    expected_azimuth, against_unit", test_cases,
 )
-@pytest.mark.parametrize('model', models)
+@pytest.mark.parametrize('solar_time_model', solar_time_models)
+@pytest.mark.parametrize('solar_position_model', solar_position_models)
 @pytest.mark.parametrize('tolerance', tolerances)
 def test_model_solar_geometry_overview(
     longitude,
     latitude,
     timestamp,
     timezone,
-    angle_output_units,
     expected_declination,
+    expected_hour_angle,
+    expected_zenith,
     expected_altitude,
     expected_azimuth,
-    model,
+    against_unit,
+    solar_position_model,
+    solar_time_model,
     tolerance,
 ):
     calculated_solar_declination, calculated_solar_hour_angle, calculated_solar_zenith, calculated_solar_altitude, calculated_solar_azimuth = model_solar_geometry_overview(
@@ -61,10 +80,8 @@ def test_model_solar_geometry_overview(
         latitude = latitude,
         timestamp = timestamp,
         timezone = timezone,
-        angle_output_units = angle_output_units,
-        model = model,
-        refracted_solar_zenith = RefractedSolarZenith(),
-        time_output_units = 'minutes',
+        solar_position_model = solar_position_model,
+        solar_time_model = solar_time_model,
     )
 
     # Check types
@@ -75,20 +92,18 @@ def test_model_solar_geometry_overview(
     assert isinstance(calculated_solar_azimuth, SolarAzimuth)
 
     # Assert output
-    assert calculated_solar_declination is None or  getattr(calculated_solar_declination, angle_output_units) == pytest.approx(
-        getattr(expected_declination, angle_output_units),
-        tolerance,
-        )
-    assert getattr(calculated_solar_altitude, angle_output_units)  == pytest.approx(
-        getattr(expected_altitude, angle_output_units),
-        tolerance,
-        )
+    assert calculated_solar_declination is None or pytest.approx(
+        getattr(expected_declination, against_unit), tolerance) == getattr(calculated_solar_declination, against_unit)
+    
+    assert calculated_solar_hour_angle is None or pytest.approx(
+        getattr(expected_hour_angle, against_unit), tolerance) == getattr(calculated_solar_hour_angle, against_unit)
+
+    assert pytest.approx(
+        getattr(expected_zenith, against_unit), tolerance) == getattr(calculated_solar_zenith, against_unit)
+
+    assert pytest.approx(
+        getattr(expected_altitude, against_unit), tolerance) == getattr(calculated_solar_altitude, against_unit)
         
-    if model == SolarPositionModels.noaa:          # FIXME: Remove this when noaa-azimuth is fixed
-        pass
-    else:
-        assert getattr(calculated_solar_azimuth, angle_output_units) == pytest.approx(
-            getattr(expected_azimuth, angle_output_units),
-            tolerance,
-            )
+    assert pytest.approx(
+        getattr(expected_azimuth, against_unit), tolerance) == getattr(calculated_solar_azimuth, against_unit)
 
