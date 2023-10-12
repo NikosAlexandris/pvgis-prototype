5478dc5208940b4182f9dbd334ec7cb594fe5820
Olyna <oly.gounari@gmail.com>
2023-10-12 16:21:16 +0300

    Add individual `solar_time_model` in printed table

--- pvgisprototype/api/geometry/solar_time.py
+++ pvgisprototype/api/geometry/solar_time.py
@@ -141,7 +141,7 @@ def calculate_solar_time(
             )
             debug(locals())
             results.append({
-                TIME_ALGORITHM_NAME: solar_time_model,
+                TIME_ALGORITHM_NAME: solar_time.timing_algorithm,
                 SOLAR_TIME_NAME: solar_time,
                 UNITS_NAME: time_output_units,  # Don't trust me -- Redesign Me!
             })
