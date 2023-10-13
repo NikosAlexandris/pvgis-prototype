4eb7b75a847a66b9d10a87233a14eff4e209c97b
Nikos Alexandris <nikosalexandris@duck.com>
2023-10-13 17:08:24 +0300

    Clean unused parameters in algorithms/pyephem/solar_time.py

--- pvgisprototype/algorithms/pvlib/solar_declination.py
+++ pvgisprototype/algorithms/pvlib/solar_declination.py
@@ -1,7 +1,6 @@
 from typing import Optional
 from datetime import datetime
 import pvlib
-# from ...api.utilities.conversions import convert_to_degrees_if_requested
 from pvgisprototype.validation.functions import validate_with_pydantic
 from pvgisprototype.validation.functions import CalculateSolarDeclinationPVLIBInput
 from pvgisprototype import SolarDeclination
@@ -11,15 +10,10 @@ from pvgisprototype import SolarDeclination
 @validate_with_pydantic(CalculateSolarDeclinationPVLIBInput)
 def calculate_solar_declination_pvlib(
         timestamp: datetime,
-        # angle_output_units: Optional[str] = 'radians'
 ) -> SolarDeclination:
         """Calculate the solar declination in radians"""
         doy = timestamp.timetuple().tm_yday
         solar_declination = pvlib.solarposition.declination_spencer71(doy)
-
         solar_declination = SolarDeclination(value=solar_declination, unit='radians')
-        # solar_declination = convert_to_degrees_if_requested(
-        #         solar_declination,
-        #         angle_output_units,
-        #         )
+
         return solar_declination
