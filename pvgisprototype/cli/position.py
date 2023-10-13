c91ad12def0ee8c7bed60271cd62586877771ad8
Nikos Alexandris <nikosalexandris@duck.com>
2023-10-13 19:07:56 +0300

    Make text for position introduction a constant
    Import A_PRIMER_ON_SOLAR_GEOMETRY from cli/documentation.py

--- pvgisprototype/cli/position.py
+++ pvgisprototype/cli/position.py
@@ -92,6 +92,7 @@ from pvgisprototype.constants import VERBOSE_LEVEL_DEFAULT
 from .rich_help_panel_names import rich_help_panel_geometry_noaa
 from pvgisprototype.constants import ZENITH_NAME
 from pvgisprototype.constants import ALTITUDE_NAME
+from pvgisprototype.cli.documentation import A_PRIMER_ON_SOLAR_GEOMETRY
 
 from .print import print_table
 from .print import print_solar_position_table
@@ -158,53 +159,12 @@ def intro():
         title="[bold cyan]Important Note[/bold cyan]",
         width=78,
     )
-    solar_geometry_primer = """
-    The amount of solar irradiance incident on a solar surface at a location
-    and a moment in time, depends on the [cyan]Solar Incidence[/cyan] angle.
-
-    To calculcate the critical solar incidence angle, PVGIS requires the
-    relative [cyan]Latitude[/cyan] and [cyan]Longitude[/cyan] coordinates of
-    the surface in question, the [cyan]Surface Tilt[/cyan] and [cyan]Surface
-    Orientation[/cyan] angles, the [cyan]Solar Declination[/cyan] and the
-    [cyan]Solar Hour[/cyan] angles both of which are derived from the
-    [cyan]Timestamp[/cyan] of interest.
-
-    First in order is the calculation of the position of the Earth in its orbit
-    around the sun expressed through the angle [cyan]Fractional Year[/cyan]
-    measured in radians.
-
-    Second is the [cyan]Equation of Time[/cyan] measured in minutes that
-    corrects for the eccentricity of the Earth's orbit and axial tilt.
-
-    The [cyan]Time Offset[/cyan] measured in minutes, incorporates the
-    [italic]Equation of Time[/italic] and accounts for the variation of the
-    Local Solar Time (LST) within a given time zone due to the longitude
-    variations within the time zone.
-
-    Next if the [cyan]True solar time[/cyan], also known as the [cyan]Apparent
-    solar time[/cyan] upon which depends the calculation of the [cyan]Solar
-    hour angle[/cyan].
-
-    The [cyan]Solar Hour angle[/cyan] measures the Earth's rotation and
-    indicates the time of the day relative to the position of the sun. It bases
-    on the longitude and timestamp and by definition, the solar hour angle is :
-
-        - 0° at solar noon
-        - negative in the morning
-        - positive in the afternoon
-
-    The order of dependency is :
-
-    - [cyan]Fractional year[/cyan]  < [cyan]Equation of time[/cyan]  < [cyan]Time offset[/cyan]  < [cyan]True solar time[/cyan]  < [cyan]Solar hour angle[/cyan]
-    - Solar declination  < Solar zenith  < Solar altitude  < Solar azimuth
-    """
-
     from rich.console import Console
     console = Console()
     # introduction.wrap(console, 30)
     console.print(introduction)
     console.print(note_in_a_panel)
-    console.print(solar_geometry_primer)
+    console.print(A_PRIMER_ON_SOLAR_GEOMETRY)
 
 
 @app.command(
