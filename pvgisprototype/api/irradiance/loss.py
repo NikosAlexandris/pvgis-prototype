a89f10532566d0ef6b8b81c3de29ef573b55b6e8
Nikos Alexandris <nikosalexandris@duck.com>
2023-10-14 09:25:56 +0300

    [Critical] Update the angular loss due to reflectivity functions
    Clean imports
    PVGIS' implementation is wrong! Fix and update docstrings :
    - calculate_angular_loss_factor_for_direct_irradiance()
    - calculate_angular_loss_factor_for_direct_irradiance_time_series()
    
    Update also :
    - calculate_angular_loss_factor_for_nondirect_irradiance()
    -

--- pvgisprototype/api/irradiance/loss.py
+++ pvgisprototype/api/irradiance/loss.py
@@ -9,12 +9,23 @@ logging.basicConfig(
     ]
 )
 import typer
-from typing_extensions import Annotated
+from typing import Annotated
+from typing import List
 import math
 from math import exp
 from math import pi
+from math import sin
+from math import cos
+from math import pow
 from pvgisprototype.cli.typer_parameters import OrderCommands
+from pvgisprototype.cli.typer_parameters import typer_argument_solar_incidence
+from pvgisprototype.cli.typer_parameters import typer_argument_solar_incidence_series
+from pvgisprototype.cli.typer_parameters import typer_option_verbose
+from pvgisprototype.constants import VERBOSE_LEVEL_DEFAULT
+from pvgisprototype.constants import ANGULAR_LOSS_COEFFICIENT
 from pvgisprototype.cli.messages import NOT_IMPLEMENTED_CLI
+import numpy as np
+from rich import print
 
 
 app = typer.Typer(
@@ -32,13 +43,12 @@ AOIConstants.append(0.155)
 
 # @app.callback(invoke_without_command=True)
 def calculate_angular_loss_factor_for_direct_irradiance(
-        sine_solar_incidence_angle: Annotated[float, typer.Argument(
-            help='Solar altitude in degrees °',
-            min=0, max=90)],
-        angle_of_incidence_constant: float = 0.155,
-    ):
+    solar_incidence: Annotated[float, typer_argument_solar_incidence],
+    angular_loss_coefficient: float = ANGULAR_LOSS_COEFFICIENT,
+    verbose: Annotated[int, typer_option_verbose] = VERBOSE_LEVEL_DEFAULT,
+):
     """Calculate the angular loss factor for the direct horizontal radiation based
-    on the solar altitude and declination
+    on the solar incident angle
 
     The adjustment factor represents the fraction of the original
     `direct_radiation` that is retained after accounting for the loss of
@@ -48,10 +58,9 @@ def calculate_angular_loss_factor_for_direct_irradiance(
     Parameters
     ----------
 
-    angle_of_incidence_constant: float
-        In degrees (°).
-    sine_solar_incidence_angle: float
-        sine of solar incidence angle in degrees (°).
+    solar_incidence_angle: float
+        solar incidence angle
+    angular_loss_coefficient: float
 
     Returns
     -------
@@ -83,60 +92,114 @@ def calculate_angular_loss_factor_for_direct_irradiance(
 
     ensuring no excessive amplification or diminishing the effect
     (over-amplification or under-amplification).
+
+    The paper :
+
+        -                            -
+        |  1 - exp( -cos(a) / a_r )  |
+    1 - | -------------------------- |
+        |     1 - exp( -1 / a_r)     |
+        -                            -
+
+    In PVGIS :
+
+        ``` c
+        angular_loss_denom = 1. / (1 - exp(-1. / a_r));
+        br *= (1 - exp(-sh / a_r)) * angular_loss_denom;
+        ```
+
+
+        - `cos (aoi)`  !=  `(-sin(aoi))`
+
+        where:
+
+        - br : direct inclined irradiance
+        - sh : _sine_ of solar incidence angle (as per Jenco)
+        - a_r : angular loss coefficient
+
+
+    In pvlib :
+
+        ``` python
+        with np.errstate(invalid='ignore'):
+            iam = (1 - np.exp(-cosd(aoi) / a_r)) / (1 - np.exp(-1 / a_r))
+            iam = np.where(np.abs(aoi) >= 90.0, 0.0, iam)
+        ```
+
+        where:
+
+        - iam : incidence angle modifier
+        - aoi : angle of incidence
+        - a_r : angular loss coefficient
     """
     try:
-        # The `-` in `-sine_solar_incidence_angle` is maybe the _workaround_ for the trigonometry error!
-        angular_loss = 1 - exp( - sine_solar_incidence_angle / angle_of_incidence_constant )
+        numerator = 1 - exp(-cos(solar_incidence) / angular_loss_coefficient)
         # --------------------------------------------------------------------
-        normalisation_term =  1 / ( 1 - exp( -1 / angle_of_incidence_constant))
-        angular_loss_factor = angular_loss / normalisation_term
-        typer.echo(f'Angular loss factor : {angular_loss_factor}')
-        return angular_loss_factor
+        denominator =  1 / ( 1 - exp( -1 / angular_loss_coefficient))
+        incidence_angle_modifier = numerator / denominator
+        if verbose > 0:
+            print(f'Angular loss factor : {angular_loss_factor}')
+        return incidence_angle_modifier
 
     except ZeroDivisionError as e:
         logging.error(f"Zero Division Error: {e}")
-        typer.echo("Error: Division by zero in calculating the angular loss factor.")
+        print("Error: Division by zero in calculating the angular loss factor.")
         return 1
 
 
-# Not complete ---------------------------------------------------------------
 def calculate_angular_loss_factor_for_direct_irradiance_time_series(
-        sine_solar_incidence_angle_series: Annotated[float, typer.Argument(
-            help='Solar altitude in degrees °',
-            min=0, max=90)],
-        angle_of_incidence_constant: float = 0.155,
-    ):
+    solar_incidence_series: Annotated[ List[float], typer_argument_solar_incidence_series],
+    angular_loss_coefficient: float = ANGULAR_LOSS_COEFFICIENT,
+    verbose: Annotated[int, typer_option_verbose] = VERBOSE_LEVEL_DEFAULT,
+):
     try:
-        angular_loss_series = 1 - np.exp( - sine_solar_incidence_angle_series / angle_of_incidence_constant )
-        normalisation_term =  1 / ( 1 - exp( -1 / angle_of_incidence_constant))
-        angular_loss_factor_series = angular_loss_factor_series / normalisation_term
-        typer.echo(f'Angular loss factor series : {angular_loss_factor_series}')
-        return angular_loss_factor_series
+        # numerator = 1 - np.exp( - np.sin(solar_incidence_series) / angular_loss_coefficient )
+        numerator = 1 - np.exp( - np.cos(solar_incidence_series) / angular_loss_coefficient )
+        denominator =  1 / ( 1 - exp( -1 / angular_loss_coefficient))
+        incidence_angle_modifier_series = numerator / denominator
+        if verbose == 3:
+            debug(locals())
+        return incidence_angle_modifier_series
 
     except ZeroDivisionError as e:
         logging.error(f"Zero Division Error: {e}")
-        typer.echo("Error: Division by zero in calculating the angular loss factor.")
-        return 1
-# Not complete ---------------------------------------------------------------
+        print("Error: Division by zero in calculating the angular loss factor.")
+        return np.array([1])  # Return an array with a single element as 1
 
 
 def calculate_angular_loss_factor_for_nondirect_irradiance(
-    angular_loss_coefficient,
-    solar_incidence_angle_1 = AOIConstants[0],
-    solar_incidence_angle_2 = AOIConstants[1],
+    indirect_angular_loss_coefficient,
+    angular_loss_coefficient = ANGULAR_LOSS_COEFFICIENT,
+    verbose: Annotated[int, typer_option_verbose] = VERBOSE_LEVEL_DEFAULT,
 ):
+    """Calculate the effect of reflectivity at small angles of incidence
+
+    In PVGIS :
+
+    - AOIConstants[0]
+    - AOIConstants[1]
+
+    c1 = 4./(3.*M_PI);
+    diff_coeff_angleloss = sinslope + (M_PI-sunSlopeGeom->slope - sinslope)
+                           /
+                           (1+cosslope);
+    diff_loss_factor =
+                       1. - exp(
+                                 - (   c1 * diff_coeff_angleloss
+                                     + AOIConstants[0]
+                                     * diff_coeff_angleloss
+                                     * diff_coeff_angleloss
+                                   ) /
+                                   AOIConstants[1]);
     """
-    Here,
-    - solar_incidence_angle_1 == AOIConstants[0]
-    - solar_incidence_angle_2 == AOIConstants[1]
-    """
+    angular_loss_coefficient_product = angular_loss_coefficient / 2 - 0.154
     c1 = 4 / (3 * pi)
     loss_factor = 1 - exp(
         -(
-            c1 * angular_loss_coefficient
-            + solar_incidence_angle_1 * angular_loss_coefficient ** 2
+            c1 * indirect_angular_loss_coefficient
+            + angular_loss_coefficient_product * pow(angular_loss_coefficient, 2)
         )
-        / solar_incidence_angle_2
+        / angular_loss_coefficient
     )
         
     return loss_factor
