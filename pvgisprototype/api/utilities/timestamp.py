443715510d000cacc44285866e5cecb15d01f0c2
Nikos Alexandris <nikosalexandris@duck.com>
2023-10-15 22:26:48 +0300

    [Important] Update api/utilities/timestamp.py
    Restructure content overall!

--- pvgisprototype/api/utilities/timestamp.py
+++ pvgisprototype/api/utilities/timestamp.py
@@ -62,50 +62,34 @@ import numpy as np
 from pvgisprototype.constants import TIMESTAMPS_FREQUENCY_DEFAULT
 
 
-def parse_timestamp(
-    ctx: typer.Context,
-    timestamp_string: str,
-    param: typer.CallbackParam,
-) -> datetime:
-    print(f'[yellow]i[/yellow] Context: {ctx}')
-    print(f'[yellow]i[/yellow] Context: {ctx.params}')
-    print(f'[yellow]i[/yellow] typer.CallbackParam: {param}')
-    if timestamp_string:
-        formats = ['%Y-%m-%d %H:%M:%S', '%Y-%m-%dT%H:%M:%S', '%Y-%m-%d', '%Y-%m', '%Y']
-        for fmt in formats:
-            try:
-                return datetime.strptime(timestamp_string, fmt)
-            except ValueError:
-                pass
-        raise ValueError(f"Invalid timestamp format: {timestamp_string}")
-    else:
-        return None
+# Random time
+
+def random_day_of_year(days_in_a_year) -> int:
+    """
+    Generate a random datetime and timezone object
+    """
+    return random.randint(1, days_in_a_year)
 
 
-def parse_timestamp_series(
-    timestamps: Union[str, datetime],
-    # ctx: typer.Context,
-    # param: typer.CallbackParam,
-) -> List[datetime]:
-    # print(f"[yellow]i[/yellow] Context: {ctx}")
-    # print(f"[yellow]i[/yellow] Context: {ctx.params}")
-    # print(f"[yellow]i[/yellow] typer.CallbackParam: {param}")
-    print("[yellow]i[/yellow] Runnning the parse_timestamp_series() function!")
-    if isinstance(timestamps, datetime):
-        return [timestamps]  # return a list in case of a single datetime object
+def random_datetimezone() -> tuple:
+    """
+    Generate a random datetime and timezone object
+    """
+    year = datetime.now().year
+    month = random.randint(1, 12)
+    _, days_in_month = calendar.monthrange(year, month)
+    day = random.randint(1, days_in_month)
+    hour = random.randint(0, 23)
+    minute = random.randint(0, 59)
+    second = random.randint(0, 59)
+    datetimestamp = datetime(year, month, day, hour, minute, second, tzinfo=ZoneInfo('UTC'))
+    timezone_str = random.choice(list(zoneinfo.available_timezones()))
+    timezone = ZoneInfo(timezone_str)
 
-    datetime_strings = timestamps.split(",")
-    try:
-        datetime_series = [
-            # datetime.fromisoformat(string) for string in datetime_strings
-            datetime.fromisoformat(string.strip())
-            for string in datetime_strings
-        ]
-    except Exception as e:
-        raise ValueError(f"Error parsing datetime: {e}")
+    return datetimestamp, timezone
 
-    return datetime_series
 
+# Time
 
 def now_local_datetimezone():
     """Get current local date and time and zone
@@ -114,6 +98,8 @@ def now_local_datetimezone():
     return datetime.now().astimezone()
 
 
+# Timezone
+
 def now_utc_datetimezone() -> datetime:
     """Returns the current datetime in UTC.
 
@@ -121,6 +107,24 @@ def now_utc_datetimezone() -> datetime:
     to UTC timezone. 
     """
     return datetime.now(ZoneInfo('UTC'))
+
+
+def attach_timezone(
+        timestamp: Optional[datetime] = None,
+        timezone_string: Optional[str] = None
+) -> datetime:
+    """Convert datetime object to timezone-aware."""
+    if timestamp is None:
+        timestamp = datetime.now(ZoneInfo('UTC'))  # Default to UTC
+
+    if isinstance(timezone_string, str):
+        try:
+            tzinfo = convert_to_timezone(timezone_string)
+            timestamp = timestamp.replace(tzinfo=tzinfo)
+        except Exception as e:
+            raise ValueError(f"Could not convert timezone: {e}")
+
+    return timestamp
 
 
 def attach_requested_timezone(
@@ -169,112 +173,6 @@ def ctx_attach_requested_timezone(
     return attach_requested_timezone(timestamp, timezone)
 
 
-# def ctx_attach_requested_timezone(
-#         ctx: typer.Context,
-#         timestamp: datetime,
-#         param: typer.CallbackParam,
-#         # verbose: bool = False,
-#     ) -> datetime:
-#     """Returns the current datetime in the user-requested timezone."""
-
-#     # if verbose:
-#     print(f'[yellow]i[/yellow] Executing `ctx_attach_requested_timezone()`')
-#     timezone = ctx.params.get('timezone')
-
-#     if timestamp.tzinfo is not None:
-#         # --------------------------------------------------------------------
-#         print("WARNING: The provided timestamp already has a timezone.")  # Convert to warning!
-#         print("Please ensure the timestamp is provided as a naive datetime object.")
-#         print("Usage example: YYYY-MM-DDTHH:MM:SS or YYYY-MM-DD")
-#         # --------------------------------------------------------------------
-#         return timestamp
-
-#     if timezone is None:
-#         # --------------------------------------------------------------------
-#         print(f'[yellow]No timezone requested! Set to[/yellow] [red]UTC[/red].')  # Convert to warning!
-#         # --------------------------------------------------------------------
-#         timezone_aware_timestamp = timestamp.replace(tzinfo=ZoneInfo('UTC'))
-
-#     else:
-#         try:
-#             # --------------------------------------------------------------------
-#             print(f'Attaching the {timezone} to the {timestamp}')  # Convert to warning!
-#             # --------------------------------------------------------------------
-#             timezone_aware_timestamp = timestamp.replace(tzinfo=timezone)
-
-#         except Exception as e:
-#             print(f'Failed to attach the requested timezone \'{timezone}\' to the timestamp: {e}')
-#             print("Defaulting to UTC timezone.")
-#             timezone_aware_timestamp = timestamp.replace(tzinfo=ZoneInfo('UTC'))
-
-#     # if verbose:
-#     #     print(f'Input timestamp    : {timestamp}')
-#     #     print(f'Requested timezone : {timezone} of type \'{type(timezone)}\'')
-#     #     # add verbosity...
-#     return timezone_aware_timestamp
-
-
-def get_days_in_year(year):
-    """ Calculate the number of days in a given year, accounting for leap years.
-
-    Parameters
-    ----------
-    year : int
-        The year for which to calculate the number of days.
-
-    Returns
-    -------
-    int
-        The number of days in the given year.
-
-    Examples
-    --------
-    >>> get_days_in_year(2020)
-    366
-
-    >>> get_days_in_year(2021)
-    365
-    """
-    start_date = datetime(year, 1, 1)  # First day of the year
-    end_date = datetime(year + 1, 1, 1)  # First day of the next year
-    return (end_date - start_date).days
-
-
-def generate_timestamps_for_a_year(year, frequency_minutes=60):
-    start_date = datetime(year, 1, 1)
-    days_in_year = get_days_in_year(year)
-    end_date = start_date + timedelta(days=days_in_year)
-    total_minutes = int((end_date - start_date).total_seconds() // 60)
-    intervals = total_minutes // frequency_minutes
-    
-    return [start_date + timedelta(minutes=(idx * frequency_minutes)) for idx in range(intervals)]
-
-
-def random_day_of_year(days_in_a_year) -> int:
-    """
-    Generate a random datetime and timezone object
-    """
-    return random.randint(1, days_in_a_year)
-
-
-def random_datetimezone() -> tuple:
-    """
-    Generate a random datetime and timezone object
-    """
-    year = datetime.now().year
-    month = random.randint(1, 12)
-    _, days_in_month = calendar.monthrange(year, month)
-    day = random.randint(1, days_in_month)
-    hour = random.randint(0, 23)
-    minute = random.randint(0, 59)
-    second = random.randint(0, 59)
-    datetimestamp = datetime(year, month, day, hour, minute, second, tzinfo=ZoneInfo('UTC'))
-    timezone_str = random.choice(list(zoneinfo.available_timezones()))
-    timezone = ZoneInfo(timezone_str)
-
-    return datetimestamp, timezone
-
-
 def convert_to_timezone(timezone_string: str) -> ZoneInfo:
     """Convert string to ZoneInfo object."""
     print(f'[yellow]i[/yellow] Executing convert_to_timezone()')
@@ -303,35 +201,12 @@ def ctx_convert_to_timezone(ctx: typer.Context, param: typer.CallbackParam, valu
     return convert_to_timezone(value)
 
 
-def attach_timezone(
-        timestamp: Optional[datetime] = None,
-        timezone_string: Optional[str] = None
-) -> datetime:
-    """Convert datetime object to timezone-aware."""
-    if timestamp is None:
-        timestamp = datetime.now(ZoneInfo('UTC'))  # Default to UTC
-
-    if isinstance(timezone_string, str):
-        try:
-            tzinfo = convert_to_timezone(timezone_string)
-            timestamp = timestamp.replace(tzinfo=tzinfo)
-        except Exception as e:
-            raise ValueError(f"Could not convert timezone: {e}")
-
-    return timestamp
-
+# Conversions
 
 def convert_hours_to_seconds(hours: float):
     """
     """
     return hours * 3600
-
-
-def datetime_to_decimal_hours(timestamp: float, timezone_string: str = 'UTC') -> float:
-    dt = datetime.fromtimestamp(timestamp, ZoneInfo(timezone_string))
-    decimal_hours = dt.hour + dt.minute / 60 + dt.second / 3600
-
-    return decimal_hours
 
 
 def convert_hours_to_datetime_time(value: float) -> time:
@@ -343,25 +218,6 @@ def convert_hours_to_datetime_time(value: float) -> time:
     seconds = int(((value - hours) * 60 - minutes) * 60)
 
     return time(hours, minutes, seconds)
-
-
-def timestamp_to_decimal_hours(t):  # NOTE: Integrated in dateclasses
-    return t.hour + t.minute / 60 + t.second / 3600 + t.microsecond / 3600000000
-
-
-def timestamp_to_decimal_hours_time_series(
-    timestamps: Union[datetime, Sequence[datetime]]
-) -> np.ndarray:
-    if isinstance(timestamps, datetime):
-        timestamps = [timestamps]
-
-    timestamps_array = np.array(timestamps)
-    hours = (timestamps_array.astype('datetime64[h]') - timestamps_array.astype('datetime64[D]')).astype('timedelta64[h]').astype(float)
-    minutes = (timestamps_array.astype('datetime64[m]') - timestamps_array.astype('datetime64[h]')).astype('timedelta64[m]').astype(float) / 60
-    seconds = (timestamps_array.astype('datetime64[s]') - timestamps_array.astype('datetime64[m]')).astype('timedelta64[s]').astype(float) / 3600
-    microseconds = (timestamps_array.astype('datetime64[us]') - timestamps_array.astype('datetime64[s]')).astype('timedelta64[us]').astype(float) / 3600000000
-
-    return hours + minutes + seconds + microseconds
 
 
 def timestamp_to_minutes(timestamp: datetime) -> float:
@@ -381,6 +237,40 @@ def timestamp_to_minutes(timestamp: datetime) -> float:
     return total_seconds / 60
 
 
+def timestamp_to_decimal_hours(t):  # NOTE: Integrated in dateclasses
+    return t.hour + t.minute / 60 + t.second / 3600 + t.microsecond / 3600000000
+
+
+def datetime_to_decimal_hours(timestamp: float, timezone_string: str = 'UTC') -> float:
+    dt = datetime.fromtimestamp(timestamp, ZoneInfo(timezone_string))
+    decimal_hours = dt.hour + dt.minute / 60 + dt.second / 3600
+
+    return decimal_hours
+
+
+def timestamp_to_decimal_hours_time_series(
+    timestamps: Union[datetime, Sequence[datetime]]
+) -> np.ndarray:
+    if isinstance(timestamps, datetime):
+        timestamps = [timestamps]
+
+    timestamps_array = np.array(timestamps)
+    hours = (timestamps_array.astype('datetime64[h]') - timestamps_array.astype('datetime64[D]')).astype('timedelta64[h]').astype(float)
+    minutes = (timestamps_array.astype('datetime64[m]') - timestamps_array.astype('datetime64[h]')).astype('timedelta64[m]').astype(float) / 60
+    seconds = (timestamps_array.astype('datetime64[s]') - timestamps_array.astype('datetime64[m]')).astype('timedelta64[s]').astype(float) / 3600
+    microseconds = (timestamps_array.astype('datetime64[us]') - timestamps_array.astype('datetime64[s]')).astype('timedelta64[us]').astype(float) / 3600000000
+
+    return hours + minutes + seconds + microseconds
+
+
+def hour_of_year_to_datetime(year, hour):
+    start_of_year = datetime.datetime(year, 1, 1)
+    timedelta_hours = datetime.timedelta(hours=hour)
+    desired_datetime = start_of_year + timedelta_hours
+
+    return desired_datetime
+
+
 def get_day_from_hour_of_year(year: int, hour_of_year: int):
     """Get day of year from hour of year."""
     start_of_year = np.datetime64(f'{year}-01-01')
@@ -394,12 +284,8 @@ def get_day_from_hour_of_year(year: int, hour_of_year: int):
     return day_of_year
 
 
-def hour_of_year_to_datetime(year, hour):
-    start_of_year = datetime.datetime(year, 1, 1)
-    timedelta_hours = datetime.timedelta(hours=hour)
-    desired_datetime = start_of_year + timedelta_hours
+# Single timestamp
 
-    return desired_datetime
 
 
 def generate_datetime_series(
