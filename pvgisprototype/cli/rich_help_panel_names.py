925d4e62ae586d9a0f5911c327d2add0f5d9289f
Nikos Alexandris <nikosalexandris@duck.com>
2023-10-21 16:46:30 +0300

    Update time series related options + typer definitions
    Set timstamps to None to make it an optional Argument in functions in cli/series.py

--- pvgisprototype/cli/rich_help_panel_names.py
+++ pvgisprototype/cli/rich_help_panel_names.py
@@ -2,8 +2,9 @@ rich_help_panel_output = 'Input / Output'                           # 1
 rich_help_panel_advanced_options = 'Advanced'                       # 3
 rich_help_panel_performance = 'Performance'                         # 1
 
-rich_help_panel_time_series = 'Time series'                         # 1
 rich_help_panel_series = 'Time series'                              # 1
+rich_help_panel_time_series = 'Time series'                         # 1
+rich_help_panel_time_series_selection = 'Selection'                         # 1
 rich_help_panel_series_irradiance = 'Solar irradiance'              # level 2
 rich_help_panel_series_meteorology = 'Meteorology'                  # level 2
 
