[Mathematical] Fix optical air mass calculation to use degrees
In api/irradiance/direct_time_series.py

pvgisprototype/api/irradiance/direct_time_series.py
@@ -234,7 +234,7 @@ def calculate_optical_air_mass_time_series(
     optical_air_mass_series = adjusted_elevation.value / (
         np.sin(refracted_solar_altitude_series.radians)
         + 0.50572
-        * np.power((refracted_solar_altitude_series.radians + 6.07995), -1.6364)
+        * np.power((refracted_solar_altitude_series.degrees + 6.07995), -1.6364)
     )
 
     optical_air_mass_series = OpticalAirMass(