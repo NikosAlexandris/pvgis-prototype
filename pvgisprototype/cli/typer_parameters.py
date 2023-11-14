84c26cc38b5b5da5dd4560237f86d871890b091f
Nikos Alexandris <nikosalexandris@duck.com>
2023-11-14 11:29:00 +0200

    Add a groupby Typer parameter definition

--- pvgisprototype/cli/typer_parameters.py
+++ pvgisprototype/cli/typer_parameters.py
@@ -702,6 +702,11 @@ typer_option_statistics = typer.Option(
     rich_help_panel=rich_help_panel_output,
     # default=False
 )
+typer_option_groupby = typer.Option(
+    help=f"Produce statistics on groups (see Xarray\'s group-by operations)",
+    rich_help_panel=rich_help_panel_output,
+    # default_factory='h'
+)
 
 typer_option_output_filename = typer.Option(
     help='Output filename for the generated figure',
