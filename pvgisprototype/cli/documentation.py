4075361c1fdd3a57365efa43ca65bdaa60221d6c
Nikos Alexandris <nikosalexandris@duck.com>
2023-11-22 08:08:33 +0200

    Add an intro subcommand under irradiance
    Add A_PRIMER_ON_SOLAR_IRRADIANCE in cli/documentation.py

--- pvgisprototype/cli/documentation.py
+++ pvgisprototype/cli/documentation.py
@@ -49,3 +49,9 @@ A_PRIMER_ON_SOLAR_GEOMETRY = """
     or ([cyan]Fractional year[/cyan], [cyan]Eccentricity correction[/cyan], [cyan]Perigee offset[/cyan]) ⊂ [cyan]Solar declination[/cyan]  [bold]PVIS[/bold]
     """
 
+A_PRIMER_ON_SOLAR_IRRADIANCE = """
+    Solar irradiance is the power per unit area (surface power density)
+    received from the Sun in the form of electromagnetic radiation in the
+    wavelength range of the measuring instrument. Solar irradiance is measured
+    in watts per square metre (W/m2) in SI units. (source: Wikipedia)
+"""
