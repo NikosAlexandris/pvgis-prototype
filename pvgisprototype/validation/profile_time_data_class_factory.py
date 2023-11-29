f568da23ae4b633d55d14a23da1afffb89318741
Nikos Alexandris <nikosalexandris@duck.com>
2023-11-29 18:38:28 +0200

    [Profiling] Add time + memory profiling scripts (in main branch)
    Profiles using the current generate_dataclass_models(parameters_yaml_file_path) implementation
    
    ❯ python pvgisprototype/validation/profile_time_data_class_factory.py
    Wed Nov 29 18:35:16 2023    output_stats
    
             165419 function calls (156899 primitive calls) in 0.077 seconds
    
       Ordered by: cumulative time
       List reduced from 430 to 10 due to restriction <10>
    
       ncalls  tottime  percall  cumtime  percall filename:lineno(function)
            1    0.000    0.000    0.078    0.078 {built-in method builtins.exec}
            1    0.000    0.000    0.077    0.077 /spacetime/pvgis/pvgis-prototype/pvgisprototype/validation/profile_time_data_class_factory.py:9(main)
            1    0.000    0.000    0.077    0.077 /spacetime/pvgis/pvgis-prototype/pvgisprototype/__init__.py:143(generate_dataclass_models)
            1    0.000    0.000    0.040    0.040 /spacetime/pvgis/pvgis-prototype/.pvgis-prototype_virtual_environment/lib/python3.11/site-packages/yaml/__init__.py:117(safe_load)
            1    0.000    0.000    0.040    0.040 /spacetime/pvgis/pvgis-prototype/.pvgis-prototype_virtual_environment/lib/python3.11/site-packages/yaml/__init__.py:74(load)
            1    0.000    0.000    0.039    0.039 /spacetime/pvgis/pvgis-prototype/.pvgis-prototype_virtual_environment/lib/python3.11/site-packages/yaml/constructor.py:47(get_single_data)
            1    0.000    0.000    0.037    0.037 /spacetime/pvgis/pvgis-prototype/.pvgis-prototype_virtual_environment/lib/python3.11/site-packages/yaml/composer.py:29(get_single_node)
            1    0.000    0.000    0.037    0.037 /spacetime/pvgis/pvgis-prototype/.pvgis-prototype_virtual_environment/lib/python3.11/site-packages/yaml/composer.py:50(compose_document)
        613/1    0.001    0.000    0.037    0.037 /spacetime/pvgis/pvgis-prototype/.pvgis-prototype_virtual_environment/lib/python3.11/site-packages/yaml/composer.py:63(compose_node)
        123/1    0.001    0.000    0.037    0.037 /spacetime/pvgis/pvgis-prototype/.pvgis-prototype_virtual_environment/lib/python3.11/site-packages/yaml/composer.py:117(compose_mapping_node)
    
    ❯ python pvgisprototype/validation/profile_memory_data_class_factory.py
    Filename: /spacetime/pvgis/pvgis-prototype/pvgisprototype/validation/profile_memory_data_class_factory.py
    
    Line #    Mem usage    Increment  Occurrences   Line Contents
    =============================================================
         8     52.7 MiB     52.7 MiB           1   @profile
         9                                         def main():
        10                                             # Call your function here
        11     52.7 MiB      0.0 MiB           1       package_root = Path(__file__).resolve()
        12     52.7 MiB      0.0 MiB           1       parameters_yaml_file_path = package_root.parent / PARAMETERS_YAML_FILE
        13                                             # generate_custom_data_classes(parameters_yaml_file_path)
        14     53.7 MiB      1.0 MiB           1       generate_dataclass_models(parameters_yaml_file_path)

---
+++ pvgisprototype/validation/profile_time_data_class_factory.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from pvgisprototype.constants import PARAMETERS_YAML_FILE
+# from pvgisprototype import generate_custom_data_classes
+from pvgisprototype import generate_dataclass_models
+import cProfile
+import pstats
+
+
+def main():
+    # Call your function here
+    package_root = Path(__file__).resolve()
+    parameters_yaml_file_path = package_root.parent / PARAMETERS_YAML_FILE
+    # generate_custom_data_classes(parameters_yaml_file_path)
+    generate_dataclass_models(parameters_yaml_file_path)
+
+
+cProfile.run('main()', 'output_stats')
+p = pstats.Stats('output_stats')
+p.sort_stats('cumulative').print_stats(10)  # Adjust parameters as needed
