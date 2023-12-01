9c8dc33a35326b1423cd581e53463a8021815510
Nikos Alexandris <nikosalexandris@duck.com>
2023-12-01 12:03:27 +0200

    [Major] Add and use pvgisprototype/validation/parameters_dictionary.py
    The idea is to have the YAML file describing custom data classes be converted at installation time into a native Python dictionary. The goal is to reduce every possible split of a second required to get the command line interface, yet not only, up and running.

--- pvgisprototype/__init__.py
+++ pvgisprototype/__init__.py
@@ -1,19 +1,11 @@
-from devtools import debug
-import yaml
-from pathlib import Path
 from pvgisprototype.data_class_factory import DataClassFactory
-from pvgisprototype.constants import PARAMETERS_YAML_FILE
+from pvgisprototype.validation.parameters_dictionary import parameters
 
 
-def generate_custom_data_classes(yaml_file: str):
-    with open(yaml_file, 'r') as f:
-        parameters = yaml.safe_load(f)
-
+def generate_custom_data_classes(parameters: dict):
     for model_name in parameters.keys():
         generated_class = DataClassFactory.get_data_class(model_name, parameters)
         globals()[model_name] = generated_class
 
 
-package_root = Path(__file__).resolve().parent
-parameters_yaml_file_path = package_root / 'validation' / PARAMETERS_YAML_FILE
-generate_custom_data_classes(parameters_yaml_file_path)
+generate_custom_data_classes(parameters)
