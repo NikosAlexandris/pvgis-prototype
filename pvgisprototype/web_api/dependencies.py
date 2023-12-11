0268a12b054147d8f5a423e29748b8cce0560788
Nikos Alexandris <nikosalexandris@duck.com>
2023-12-11 12:56:21 +0200

    Add web_api/fastapi_parameters.py, update web_api/dependencies.py
    - Add reusable fastapi_query_??? like objects
    - Update imports
    - Import stuff from pvgisprototype.web_api.fastapi_parameters
    - Update :
      - process_series_timestamp()
      - process_longitude()
      - process_latitude()
    - Create reusable 'fastapi_dependable_???' like objects

--- pvgisprototype/web_api/dependencies.py
+++ pvgisprototype/web_api/dependencies.py
@@ -2,12 +2,19 @@ import pandas as pd
 from datetime import datetime
 from zoneinfo import ZoneInfo
 from typing import Optional
-from fastapi import Query, HTTPException
+from fastapi import Query
+from fastapi import HTTPException
+from fastapi import Depends
 from pvgisprototype.api.utilities.conversions import convert_to_radians_fastapi
+from pvgisprototype.api.utilities.timestamp import now_utc_datetimezone
+from pvgisprototype.api.utilities.timestamp import convert_to_timezone
 from pvgisprototype.api.utilities.timestamp import attach_requested_timezone
 from pvgisprototype.api.utilities.timestamp import parse_timestamp_series
 from pvgisprototype.api.utilities.timestamp import generate_datetime_series
-from pvgisprototype.api.utilities.timestamp import now_utc_datetimezone
+
+from pvgisprototype.web_api.fastapi_parameters import fastapi_query_longitude
+from pvgisprototype.web_api.fastapi_parameters import fastapi_query_latitude
+from typing import Annotated
 
 
 async def process_series_timestamp(
@@ -15,28 +22,28 @@ async def process_series_timestamp(
     start_time: Optional[str] = Query(None),
     frequency: Optional[str] = Query('h'),
     end_time: Optional[str] = Query(None),
+    timezone: Optional[str] = None,
 ):
+    """
+    """
+    timezone = convert_to_timezone(timezone)
+    from devtools import debug
+    debug(locals())
     if timestamps is not None and not start_time and not end_time:
-        timestamps = parse_timestamp_series(timestamps)
+        timestamps = parse_timestamp_series(timestamps=timestamps)
 
-    if start_time and end_time:
+    if start_time is not None and end_time is not None:
         try:
-            generated_timestamps = generate_datetime_series(start_time, end_time, frequency)
+            timestamps = generate_datetime_series(
+                start_time=start_time,
+                frequency=frequency,
+                end_time=end_time,
+                timezone=timezone,
+            )
         except Exception as e:
             raise HTTPException(status_code=400, detail=str(e))
-        return generated_timestamps
-
-    elif timestamps:
-        try:
-            # timestamps = timestamps.strip().split(",")
-            parsed_timestamps = pd.to_datetime(timestamps, format='%Y-%m-%d %H:%M:%S')
-            parsed_timestamps = [ts.to_pydatetime() for ts in parsed_timestamps]
-        except ValueError as e:
-            raise HTTPException(status_code=400, detail="Invalid timestamp format")
-        return parsed_timestamps
-
-    else:
-        raise HTTPException(status_code=400, detail="Either timestamps or start_time and end_time must be provided")
+    debug(locals())
+    return timestamps
 
 
 async def process_single_timestamp(
@@ -65,15 +72,20 @@ async def process_single_timestamp(
             # print(f'Input timestamp & zone ({user_requested_timestamp} & {user_requested_timezone}) converted to {timestamp} for all internal calculations!')
         
         return timestamp
-    
-    
+
+
 async def process_longitude(
-    longitude: float = Query(..., ge=-180, le=180),
+    longitude: Annotated[float, fastapi_query_longitude],
 ) -> float:
     return convert_to_radians_fastapi(longitude)
 
 
 async def process_latitude(
-    latitude: float = Query(..., ge=-90, le=90),
+    latitude: Annotated[float, fastapi_query_latitude],
 ) -> float:
     return convert_to_radians_fastapi(latitude)
+
+
+fastapi_dependable_longitude = Depends(process_longitude)
+fastapi_dependable_latitude = Depends(process_latitude)
+fastapi_dependable_timestamps = Depends(process_series_timestamp)
