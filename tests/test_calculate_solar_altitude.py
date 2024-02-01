53f580d29c162eda3fd6a48a544c467c06e19e4f
Nikos Alexandris <nikosalexandris@duck.com>
2024-02-01 20:31:34 +0200

    [Test] Update path to noaa.xlsx in a series of relevant tests

--- tests/test_calculate_solar_altitude.py
+++ tests/test_calculate_solar_altitude.py
@@ -6,7 +6,7 @@ from pvgisprototype.constants import ALTITUDE_NAME, TIME_ALGORITHM_NAME, POSITIO
 
 
 test_cases_data = read_noaa_spreadsheet(
-    './tests/data/test_cases_noaa_spreadsheet.xlsx'
+    './tests/cases/noaa.xlsx'
 )
 test_cases = test_cases_from_data(
     test_cases_data,
--- tests/test_calculate_solar_altitude_pvlib.py
+++ tests/test_calculate_solar_altitude_pvlib.py
@@ -8,7 +8,7 @@ from pvgisprototype.constants import ALTITUDE_NAME, RADIANS
 
 
 test_cases_data = read_noaa_spreadsheet(
-    './tests/data/test_cases_noaa_spreadsheet.xlsx'
+    './tests/cases/noaa.xlsx'
 )
 test_cases = test_cases_from_data(
     test_cases_data,
