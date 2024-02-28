16869c6fba7acd94326b9dd7d6c9d34719ea9e14
Nikos Alexandris <nikosalexandris@duck.com>
2024-02-28 13:07:44 +0200

    Update pytests
    - Use ./tests/cases/noaa.xlsx
    - Fix some imports, like SolarPositionModel instead of SolarPositionModels (and similar) or calculate_refracted_solar_altitude_time_series instead of calculate_refracted_solar_altitude

--- tests/test_calculate_solar_hour_angle_declination_skyfield.py
+++ tests/test_calculate_solar_hour_angle_declination_skyfield.py
@@ -6,7 +6,7 @@ from pvgisprototype.constants import HOUR_ANGLE_NAME, DECLINATION_NAME, RADIANS
 
 
 test_cases_data = read_noaa_spreadsheet(
-    './tests/data/test_cases_noaa_spreadsheet.xlsx'
+    './tests/cases/noaa.xlsx'
 )
 test_cases = test_cases_from_data(
     test_cases_data,
