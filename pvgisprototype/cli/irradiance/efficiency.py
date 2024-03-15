d41e2e1b0f746e0ad900471e29ed8ba0e95ddee5
Nikos Alexandris <nikosalexandris@duck.com>
2024-03-15 13:31:36 +0200

    Use photovoltaic_module in get_pv_efficiency_time_series()

--- pvgisprototype/cli/irradiance/efficiency.py
+++ pvgisprototype/cli/irradiance/efficiency.py
@@ -26,9 +26,12 @@ from pvgisprototype.cli.typer_parameters import typer_option_csv
 from pvgisprototype.cli.typer_parameters import typer_option_verbose
 from pvgisprototype.constants import VERBOSE_LEVEL_DEFAULT
 from pvgisprototype.cli.typer_parameters import typer_option_index
+from pvgisprototype.cli.typer_parameters import typer_option_photovoltaic_module_model
+from pvgisprototype.constants import PHOTOVOLTAIC_MODULE_DEFAULT
 from pvgisprototype.constants import EFFICIENCY
 from pvgisprototype.constants import EFFICIENCY_COLUMN_NAME
 from pvgisprototype.cli.print import print_quantity_table
+from pvgisprototype.api.irradiance.photovoltaic_module import PhotovoltaicModuleModel
 
 
 app = typer.Typer(
@@ -52,7 +55,8 @@ def get_pv_efficiency_time_series(
     irradiance_series: Annotated[List[float], typer_argument_irradiance_series],
     spectral_factor=None,
     temperature_series: Annotated[TemperatureSeries, typer_option_temperature_series] = TEMPERATURE_DEFAULT,
-    model_constants: List[float] = EFFICIENCY_MODEL_COEFFICIENTS_DEFAULT,
+    photovoltaic_module: Annotated[PhotovoltaicModuleModel, typer_option_photovoltaic_module_model] = PHOTOVOLTAIC_MODULE_DEFAULT, #PhotovoltaicModuleModel.CSI_FREE_STANDING, 
+    # model_constants: List[float] = EFFICIENCY_MODEL_COEFFICIENTS_DEFAULT,
     standard_test_temperature: float = TEMPERATURE_DEFAULT,
     wind_speed_series: Annotated[WindSpeedSeries, typer_option_wind_speed_series] = WIND_SPEED_DEFAULT,
     power_model: Annotated[PVModuleEfficiencyAlgorithm, typer_option_pv_power_algorithm] = PVModuleEfficiencyAlgorithm.king,
@@ -70,7 +74,8 @@ def get_pv_efficiency_time_series(
         irradiance_series=irradiance_series,
         spectral_factor=spectral_factor,
         temperature_series=temperature_series,
-        model_constants=model_constants,
+        photovoltaic_module=photovoltaic_module,
+        # model_constants=model_constants,
         standard_test_temperature=standard_test_temperature,
         wind_speed_series=wind_speed_series,
         power_model=power_model,
