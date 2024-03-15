b1785d4bd96ef03c63816b6f5b36f7f92152322a
Nikos Alexandris <nikosalexandris@duck.com>
2024-03-15 13:41:15 +0200

    Rework on the logger, again!
    Not easy if you have no Know-How.-

--- pvgisprototype/cli/power/broadband.py
+++ pvgisprototype/cli/power/broadband.py
@@ -112,6 +112,8 @@ from pvgisprototype.api.irradiance.photovoltaic_module import PhotovoltaicModule
 
 import typer
 
+from pvgisprototype.cli.typer_parameters import typer_option_log
+
 @log_function_call
 def photovoltaic_power_output_series(
     longitude: Annotated[float, typer_argument_longitude],
@@ -165,7 +167,7 @@ def photovoltaic_power_output_series(
     uniplot: Annotated[bool, typer_option_uniplot] = False,
     terminal_width_fraction: Annotated[float, typer_option_uniplot_terminal_width] = TERMINAL_WIDTH_FRACTION,
     verbose: Annotated[int, typer_option_verbose] = VERBOSE_LEVEL_DEFAULT,
-    log: Annotated[int, typer.Option('--log', help='Log internal operations')] = 0,
+    log: Annotated[int, typer_option_log] = 0,
     index: Annotated[bool, typer_option_index] = False,
     fingerprint: Annotated[bool, typer.Option('--fingerprint', '--fp', help='Fingerprint the photovoltaic power output time series')] = False,
     quiet: Annotated[bool, typer.Option('--quiet', help='Do not print out the output')] = False,
