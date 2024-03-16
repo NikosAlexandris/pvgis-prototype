dec16ebb221f08f2dede71acd8e4708da91a1477
Nikos Alexandris <nikosalexandris@duck.com>
2024-03-16 05:53:50 +0200

    [Optimisation] Rework reading (in parallel) SARAH3 data
    See pvgisprototype/api/irradiance/diffuse.py :
    - add read_horizontal_irradiance_components_from_sarah() and use ThreadPoolExecutor because reading time series data is an I/O-bound task.
    - modify calculate_diffuse_horizontal_component_from_sarah()
    - restructure calculate_diffuse_inclined_irradiance_time_series()

--- pvgisprototype/cli/power/broadband.py
+++ pvgisprototype/cli/power/broadband.py
@@ -136,6 +136,7 @@ def photovoltaic_power_output_series(
     in_memory: Annotated[bool, typer_option_in_memory] = False,
     dtype: str = DATA_TYPE_DEFAULT,
     array_backend: str = ARRAY_BACKEND_DEFAULT,
+    multi_thread: bool = True,
     surface_orientation: Annotated[Optional[float], typer_option_surface_orientation] = SURFACE_ORIENTATION_DEFAULT,
     surface_tilt: Annotated[Optional[float], typer_option_surface_tilt] = SURFACE_TILT_DEFAULT,
     linke_turbidity_factor_series: Annotated[LinkeTurbidityFactor, typer_option_linke_turbidity_factor_series] = None,  # Changed this to np.ndarray
@@ -218,6 +219,7 @@ def photovoltaic_power_output_series(
         in_memory=in_memory,
         dtype=dtype,
         array_backend=array_backend,
+        multi_thread=multi_thread,
         surface_tilt=surface_tilt,
         surface_orientation=surface_orientation,
         linke_turbidity_factor_series=linke_turbidity_factor_series,
