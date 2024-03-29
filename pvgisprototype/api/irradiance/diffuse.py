d876e2345e7f8d06511e7fd051a3e80e93a7bf47
Nikos Alexandris <nikosalexandris@duck.com>
2024-03-29 22:54:23 +0200

    Add calculate_diffuse_horizontal_irradiance_time_series()

--- pvgisprototype/api/irradiance/diffuse.py
+++ pvgisprototype/api/irradiance/diffuse.py
@@ -488,6 +488,153 @@ def diffuse_solar_altitude_function_time_series(
         + a3_series * np.power(np.sin(solar_altitude_series.radians), 2)
     )
 
+@log_function_call
+def calculate_diffuse_horizontal_irradiance_time_series(
+    longitude: float,
+    latitude: float,
+    timestamps: BaseTimestampSeriesModel = None,
+    start_time: Optional[datetime] = None,
+    end_time: Optional[datetime] = None,
+    timezone: Optional[str] = None,
+    random_time_series: bool = False,
+    linke_turbidity_factor_series: LinkeTurbidityFactor = None,  # Changed this to np.ndarray
+    apply_atmospheric_refraction: Optional[bool] = True,
+    refracted_solar_zenith: Optional[float] = REFRACTED_SOLAR_ZENITH_ANGLE_DEFAULT,  # radians
+    apply_angular_loss_factor: Optional[bool] = True,
+    solar_position_model: SolarPositionModel = SOLAR_POSITION_ALGORITHM_DEFAULT,
+    solar_time_model: SolarTimeModel = SOLAR_TIME_ALGORITHM_DEFAULT,
+    time_offset_global: float = 0,
+    hour_offset: float = 0,
+    solar_constant: float = SOLAR_CONSTANT,
+    perigee_offset: float = PERIGEE_OFFSET,
+    eccentricity_correction_factor: float = ECCENTRICITY_CORRECTION_FACTOR,
+    time_output_units: str = "minutes",
+    angle_units: str = RADIANS,
+    angle_output_units: str = RADIANS,
+    random_days: bool = RANDOM_DAY_FLAG_DEFAULT,
+    dtype: str = DATA_TYPE_DEFAULT,
+    array_backend: str = ARRAY_BACKEND_DEFAULT,
+    multi_thread: bool = True,
+    verbose: int = VERBOSE_LEVEL_DEFAULT,
+    log: int = 0,
+    fingerprint: bool = False,
+        ):
+    """
+    """
+    extraterrestrial_normal_irradiance_series = (
+        calculate_extraterrestrial_normal_irradiance_time_series(
+            timestamps=timestamps,
+            solar_constant=solar_constant,
+            perigee_offset=perigee_offset,
+            eccentricity_correction_factor=eccentricity_correction_factor,
+            random_days=random_days,
+            dtype=dtype,
+            array_backend=array_backend,
+            verbose=0,  # no verbosity here by choice!
+            log=log,
+        )
+    )
+    # extraterrestrial on a horizontal surface requires the solar altitude
+    solar_altitude_series = model_solar_altitude_time_series(
+        longitude=longitude,
+        latitude=latitude,
+        timestamps=timestamps,
+        timezone=timezone,
+        solar_position_model=solar_position_model,
+        apply_atmospheric_refraction=apply_atmospheric_refraction,
+        refracted_solar_zenith=refracted_solar_zenith,
+        solar_time_model=solar_time_model,
+        time_offset_global=time_offset_global,
+        hour_offset=hour_offset,
+        perigee_offset=perigee_offset,
+        eccentricity_correction_factor=eccentricity_correction_factor,
+        time_output_units=time_output_units,
+        angle_units=angle_units,
+        angle_output_units=angle_output_units,
+        dtype=dtype,
+        array_backend=array_backend,
+        verbose=verbose,  # Is this wanted here ? i.e. not setting = 0 ?
+        log=log,
+    )
+    diffuse_horizontal_irradiance_series = (
+        extraterrestrial_normal_irradiance_series
+        * diffuse_transmission_function_time_series(linke_turbidity_factor_series)
+        * diffuse_solar_altitude_function_time_series(
+            solar_altitude_series, linke_turbidity_factor_series
+        )
+    )
+
+    out_of_range = (
+        (diffuse_horizontal_irradiance_series < LOWER_PHYSICALLY_POSSIBLE_LIMIT)
+        | (diffuse_horizontal_irradiance_series > UPPER_PHYSICALLY_POSSIBLE_LIMIT)
+    )
+    if out_of_range.size:
+        warning = f"{WARNING_OUT_OF_RANGE_VALUES} in `diffuse_horizontal_irradiance_series`!"
+        logger.warning(warning)
+        stub_array = np.full(out_of_range.shape, -1, dtype=int)
+        index_array = np.arange(len(out_of_range))
+        out_of_range_indices = np.where(out_of_range, index_array, stub_array)
+
+    components_container = {
+        'main': lambda: {
+            TITLE_KEY_NAME: DIFFUSE_HORIZONTAL_IRRADIANCE,
+            DIFFUSE_HORIZONTAL_IRRADIANCE_COLUMN_NAME: diffuse_horizontal_irradiance_series,
+        },# if verbose > 0 else {},
+
+        'extended': lambda: {
+            RADIATION_MODEL_COLUMN_NAME: HOFIERKA_2002,
+        } if verbose > 1 else {},
+
+        'more_extended': lambda: {
+            TITLE_KEY_NAME: DIFFUSE_HORIZONTAL_IRRADIANCE + ' & relevant components',
+            EXTRATERRESTRIAL_NORMAL_IRRADIANCE_COLUMN_NAME: extraterrestrial_normal_irradiance_series,
+            ALTITUDE_COLUMN_NAME: getattr(solar_altitude_series, angle_output_units) if solar_altitude_series else None,
+            LINKE_TURBIDITY_COLUMN_NAME: linke_turbidity_factor_series.value,
+        } if verbose > 2 else {},
+
+        'even_more_extended': lambda: {
+        } if verbose > 3 else {},
+
+        'and_even_more_extended': lambda: {
+        } if verbose > 4 else {},
+
+        'extra': lambda: {
+        } if verbose > 5 else {},
+
+        'out-of-range': lambda: {
+            # OUT_OF_RANGE_INDICES_COLUMN_NAME: out_of_range,
+            # OUT_OF_RANGE_INDICES_COLUMN_NAME + ' i': out_of_range_indices,
+        } if out_of_range_indices[0].size > 0 else {},
+
+        'fingerprint': lambda: {
+            FINGERPRINT_COLUMN_NAME: generate_hash(diffuse_horizontal_irradiance_series),
+        } if fingerprint else {},
+    }
+
+    components = {}
+    for key, component in components_container.items():
+        components.update(component())
+
+    if verbose > DEBUG_AFTER_THIS_VERBOSITY_LEVEL:
+        debug(locals())
+
+    log_data_fingerprint(
+        data=diffuse_horizontal_irradiance_series,
+        log_level=log,
+        hash_after_this_verbosity_level=HASH_AFTER_THIS_VERBOSITY_LEVEL,
+    )
+
+    return Irradiance(
+            value=diffuse_horizontal_irradiance_series,
+            unit=IRRADIANCE_UNITS,
+            position_algorithm="",
+            timing_algorithm="",
+            elevation=None,
+            surface_orientation=None,
+            surface_tilt=None,
+            components=components,
+            )
+
 
 @log_function_call
 def calculate_diffuse_inclined_irradiance_time_series(
