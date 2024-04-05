848636437d93c18a2b176c7001a9b53742a143a7
Nikos Alexandris <nikosalexandris@duck.com>
2024-04-05 20:38:12 +0300

    Update generate_hash(), use hashlib.blake2b()

--- pvgisprototype/validation/hashing.py
+++ pvgisprototype/validation/hashing.py
@@ -1,6 +1,6 @@
 import numpy as np
 import hashlib
-import json
+# import json
 
 
 def ndarray_to_list(obj):
@@ -9,10 +9,21 @@ def ndarray_to_list(obj):
     raise TypeError(f"Object of type {type(obj).__name__} is not JSON serializable")
 
 
-def generate_hash(output):
-    output_str = json.dumps(output, default=ndarray_to_list, sort_keys=True)
-    # instead of _dumping_ : do read the file  directly without loading in-memory first !!!
-    hash_object = hashlib.sha256(output_str.encode())
-    hash_hex = hash_object.hexdigest()
+def generate_hash(output, person="PVGIS"):
+    hash = hashlib.blake2b(
+        digest_size=32,
+        # key=b'',
+        # salt=b'',
+        person=b"{person}",
+        # fanout=1,
+        # depth=1,
+        # leaf_size=0,
+        # node_offset=0,
+        # node_depth=0,
+        # inner_size=0,
+        # last_node=False,
+        usedforsecurity=False,
+    )
+    hash.update(output.tobytes())
 
-    return hash_hex
+    return hash.hexdigest()
