8950dd7f43dfdbcf6cb680b995b1ea35b4da9c5e
Nikos Alexandris <nikosalexandris@duck.com>
2024-04-07 17:31:43 +0300

    refactor: Make CLI timestamping eager, add naive timestamping callback functions!

--- pvgisprototype/cli/typer/timestamps.py
+++ pvgisprototype/cli/typer/timestamps.py
@@ -9,6 +9,7 @@ from pvgisprototype.api.utilities.timestamp import ctx_attach_requested_timezone
 from pvgisprototype.api.utilities.timestamp import now_utc_datetimezone
 from pvgisprototype.api.utilities.timestamp import parse_timestamp_series
 from pvgisprototype.api.utilities.timestamp import callback_generate_datetime_series
+from pvgisprototype.api.utilities.timestamp import callback_generate_naive_datetime_series
 from pvgisprototype.api.utilities.timestamp import ctx_convert_to_timezone
 from pvgisprototype.api.utilities.timestamp import now_local_datetimezone
 from pvgisprototype.cli.rich_help_panel_names import rich_help_panel_time_series
@@ -27,39 +28,70 @@ typer_argument_timestamp = typer.Argument(
 typer_argument_timestamps = typer.Argument(
     help=timestamps_typer_help,
     rich_help_panel=rich_help_panel_time_series,
+    is_eager=True,
     parser=parse_timestamp_series,
     callback=callback_generate_datetime_series,
+    # default_factory=now_utc_datetimezone,
+    show_default=False,
+)
+typer_argument_naive_timestamps = typer.Argument(
+    help=timestamps_typer_help,
+    rich_help_panel=rich_help_panel_time_series,
+    is_eager=True,
+    parser=parse_timestamp_series,
+    callback=callback_generate_naive_datetime_series,
     # default_factory=now_utc_datetimezone,
     show_default=False,
 )
 typer_option_timestamps = typer.Option(
     help='Timestamps',
     rich_help_panel=rich_help_panel_time_series,
+    is_eager=True,
     parser=parse_timestamp_series,
     callback=callback_generate_datetime_series,
 #     default_factory=now_utc_datetimezone_series,
 )
+
+from datetime import datetime
+def convert_datetime_to_Timestamp(
+        timestamp: datetime
+        ):
+    """
+    """
+    from pandas import Timestamp
+    if not timestamp:
+        return None
+    return Timestamp(timestamp)
+
+
 warning_overrides_timestamps = f'[yellow]Overrides the `timestamps` parameter![/yellow]'
 typer_option_start_time = typer.Option(
     help=f'Start timestamp of the period. {warning_overrides_timestamps}',
     rich_help_panel=rich_help_panel_time_series,
+    is_eager=True,
+    callback=convert_datetime_to_Timestamp,
 )
 typer_option_periods = typer.Option(
     help=f"Number of timestamps to generate",
     rich_help_panel=rich_help_panel_time_series,
+    is_eager=True,
 )
 typer_option_frequency = typer.Option(
     # https://pandas.pydata.org/pandas-docs/stable/user_guide/timeseries.html#timeseries-offset-aliases
     help=f"A common date/time frequency unit optionally with a multiples number, such as [code]H[/code](hourly), [code]min[/code](utely), [code]S[/code](econdly), [code]D[/code](aily), [code]W[/code](eekly), [code]M[/code](onth end), [code]Y[/code](early) or [code]30m[/code]. See Pandas time series offset aliases.",
     rich_help_panel=rich_help_panel_time_series,
+    is_eager=True,
 )
 typer_option_end_time = typer.Option(
     help=f'End timestamp of the period. {warning_overrides_timestamps}',
     rich_help_panel=rich_help_panel_time_series,
+    is_eager=True,
+    callback=convert_datetime_to_Timestamp,
 )
 typer_option_timezone = typer.Option(
     help="Timezone (e.g., 'Europe/Athens'). Use the system's time zone via the `--local` option.",
     rich_help_panel=rich_help_panel_time_series,
+    is_eager=True,
     callback=ctx_convert_to_timezone,
 )
 typer_option_local_time = typer.Option(
