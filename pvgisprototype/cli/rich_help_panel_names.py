849bb7852c588d1d0f3fc092b44c9cc55924a32a
Nikos Alexandris <nikosalexandris@duck.com>
2024-04-12 11:55:40 +0300

    refactor: Update rich help panel names

--- pvgisprototype/cli/rich_help_panel_names.py
+++ pvgisprototype/cli/rich_help_panel_names.py
@@ -4,9 +4,9 @@ Structural components of the command line interface
 
 # Generic
 
-rich_help_panel_output = 'Input > < Output'
-rich_help_panel_output_metadata = 'Output metadata'
-rich_help_panel_advanced_options = 'Advanced options'
+rich_help_panel_output = '> Input : < Output'
+rich_help_panel_output_metadata = 'Metadata'
+rich_help_panel_advanced_options = 'Advanced'
 rich_help_panel_plotting = 'Plotting'
 
 rich_help_panel_introduction = 'Introduction'
@@ -18,7 +18,7 @@ rich_help_panel_reference = 'Reference'
 
 rich_help_panel_surface_location = 'Location of solar surface'
 rich_help_panel_surface_geometry = 'Geometry of solar surface'
-rich_help_panel_time_series = 'Time series'  # timestamps
+rich_help_panel_time_series = 'Timestamps'  # timestamps
 
 # Solar position
 
@@ -30,21 +30,21 @@ rich_help_panel_solar_position = 'Solar position'
 
 # Solar irradiance
 
-rich_help_panel_irradiance_series = 'Solar irradiance time series'
+rich_help_panel_irradiance_series = 'External solar irradiance time series'
 
 # Ancillary data
 
-rich_help_panel_meteorological_series = 'Meteorological time series'
+rich_help_panel_meteorological_series = 'External meteorological time series'
 
 # Data selection options
 
 rich_help_panel_series = 'Series'
 rich_help_panel_time_series_data = 'External time series data'
-rich_help_panel_time_series_data_selection = 'Data selection options'
+rich_help_panel_time_series_data_selection = 'External time series selection'
 
 # Data processing options
 
-rich_help_panel_data_processing = 'Data processing options'
+rich_help_panel_data_processing = 'Data processing'
 
 # Statistics
 
@@ -52,6 +52,6 @@ rich_help_panel_statistics = 'Statistics'
 
 # Photovoltaic Performance
 
-rich_help_panel_efficiency = 'Efficiency'
+rich_help_panel_efficiency = 'Photovoltaic efficiency'
 rich_help_panel_performance = 'Performance'
 rich_help_panel_performance_toolbox = rich_help_panel_performance + ' toolbox'
