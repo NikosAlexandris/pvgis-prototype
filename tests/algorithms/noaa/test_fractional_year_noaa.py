9e2f9d33ae1ded591134554c57e6493b42d2804b
Alekos Falagas <alek.falagas@gmail.com>
2024-04-19 18:06:31 +0300

    test: Add tests and cases for algorithms.noaa.fractional_year.py
    Refers to #157

--- tests/algorithms/noaa/test_fractional_year_noaa.py
+++ tests/algorithms/noaa/test_fractional_year_noaa.py
@@ -1,9 +1,4 @@
-from datetime import datetime
-from typing import Union
-from numpy import isclose
-
 import pytest
-from pandas import Timestamp
 from pvgisprototype.algorithms.noaa.fractional_year import calculate_fractional_year_time_series_noaa
 
 from .cases.fractional_year_noaa import cases_fractional_year_noaa 
@@ -19,18 +14,13 @@ class TestCalculateFractionalYearNOAA(GenericCheckCustomObjects):
         return request.param
 
     @pytest.fixture
-    def in_(self, cases):
-        print(calculate_fractional_year_time_series_noaa(**cases[0]))
-        return calculate_fractional_year_time_series_noaa(**cases[0])
-    
-    @pytest.fixture
-    def expected(self, cases):
-        return cases[1]
+    def operation(self):
+        return calculate_fractional_year_time_series_noaa
     
     @pytest.fixture(params=cases_fractional_year_noaa_invalid)
     def in_invalid(self, request):
         return request.param
     
-    def test_invalid_input_datetime_string(self, in_invalid):
+    def test_invalid_input_datetime_string(self, in_invalid, operation):
         with pytest.raises(in_invalid[1]):  
-            _ = calculate_fractional_year_time_series_noaa(**in_invalid[0])
+            _ = operation(**in_invalid[0])
