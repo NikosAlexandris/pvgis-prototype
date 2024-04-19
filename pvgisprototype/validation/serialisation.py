6b277a4eb1e6713ab659e466d3aad12608415112
Nikos Alexandris <nikosalexandris@duck.com>
2024-04-20 00:45:08 +0300

    fix: Add missing validation/serialisation.py
    Add CustomEncoder and use in print_command_metadata().

---
+++ pvgisprototype/validation/serialisation.py
@@ -0,0 +1,24 @@
+import json
+from pandas import DatetimeIndex
+from datetime import datetime
+from zoneinfo import ZoneInfo
+from pvgisprototype import (
+        LinkeTurbidityFactor,
+        SpectralFactorSeries,
+        TemperatureSeries,
+        WindSpeedSeries,
+        )
+
+
+class CustomEncoder(json.JSONEncoder):
+    def default(self, object):
+        if isinstance(object, (DatetimeIndex,
+                               datetime,
+                               ZoneInfo,
+                               LinkeTurbidityFactor,
+                               SpectralFactorSeries,
+                               TemperatureSeries,
+                               WindSpeedSeries,
+                               )):
+            return str(object)  # convert non-serializable objects to string
+        return json.JSONEncoder.default(self, object)
