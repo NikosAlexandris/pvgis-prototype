804027535e630bb88d30dbb0d122d0bcad18205e
Nikos Alexandris <nikosalexandris@duck.com>
2024-04-22 01:30:14 +0300

    refactor: Update verbose and log parameters in some modules
    Use defaults from constants for the `verbose` and `log` input parameters in :
    - algorithms/noaa/ : equation_of_time.py, fractional_year.py, solar_time.py, time_offset.py
    - api/position/solar_time_series.py

--- pvgisprototype/api/position/solar_time_series.py
+++ pvgisprototype/api/position/solar_time_series.py
@@ -13,15 +13,20 @@ from pvgisprototype.constants import REFRACTED_SOLAR_ZENITH_ANGLE_DEFAULT
 from pvgisprototype.constants import PERIGEE_OFFSET
 from pvgisprototype.constants import ECCENTRICITY_CORRECTION_FACTOR
 from pvgisprototype.constants import RADIANS
+from pvgisprototype.constants import VERBOSE_LEVEL_DEFAULT
+from pvgisprototype.constants import LOG_LEVEL_DEFAULT
+from pvgisprototype.log import log_function_call
 
 
+@log_function_call
 @validate_with_pydantic(ModelSolarTimeTimeSeriesInputModel)
 def model_solar_time_time_series(
     longitude: Longitude,
     timestamps: Union[datetime, Sequence[datetime]],
     timezone: ZoneInfo = None,
     solar_time_model: SolarTimeModel = SolarTimeModel.noaa,
-    verbose: int = 0,
+    verbose: int = VERBOSE_LEVEL_DEFAULT,
+    log: int = LOG_LEVEL_DEFAULT,
 ):
     """Calculates the solar time using the requested _algorithm_.
 
