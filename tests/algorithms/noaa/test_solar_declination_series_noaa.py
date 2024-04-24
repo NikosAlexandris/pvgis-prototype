7ed286985f410db249eb4baeed0a9314ad1f8e74
Alekos Falagas <alek.falagas@gmail.com>
2024-04-24 13:50:41 +0300

    test: Update tests for solar_declination.py
    Refers to #157

--- tests/algorithms/noaa/test_solar_declination_series_noaa.py
+++ tests/algorithms/noaa/test_solar_declination_series_noaa.py
@@ -1,12 +1,6 @@
-from datetime import datetime
-from typing import Union
-from numpy import isclose
-
 import pytest
-from pandas import Timestamp
 
 from pvgisprototype.algorithms.noaa.solar_declination import calculate_solar_declination_time_series_noaa
-from pvgisprototype import SolarDeclination
 
 from .cases.solar_declination_angle_noaa import cases_solar_declination_angle_noaa
 from .cases.solar_declination_angle_noaa import cases_solar_declination_angle_noaa_ids
