0e675a232a291ab328752d614bf476c403ec700d
Alekos Falagas <alek.falagas@gmail.com>
2024-04-24 13:53:31 +0300

    test: Add tests for solar_time.py
    Refers to #157

---
+++ tests/algorithms/noaa/test_true_solar_time.py
@@ -0,0 +1,18 @@
+import pytest
+from pvgisprototype.algorithms.noaa.solar_time import calculate_true_solar_time_time_series_noaa
+
+from .cases.true_solar_time import cases_true_solar_time_noaa
+from .cases.true_solar_time import cases_true_solar_time_ids
+
+from .conftest import GenericCheckCustomObjects
+
+
+class TestCalculateTrueSolarTimeNOAA(GenericCheckCustomObjects):
+
+    @pytest.fixture(params=cases_true_solar_time_noaa, ids=cases_true_solar_time_ids)
+    def cases(self, request):
+        return request.param
+
+    @pytest.fixture
+    def operation(self):
+        return calculate_true_solar_time_time_series_noaa
