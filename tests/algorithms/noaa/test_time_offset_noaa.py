4127ebb376fe4302466cfac0fc96f580ef15b8ec
Alekos Falagas <alek.falagas@gmail.com>
2024-04-24 13:51:32 +0300

    test: Add tests for time_offset.py
    Refers to #157

---
+++ tests/algorithms/noaa/test_time_offset_noaa.py
@@ -0,0 +1,18 @@
+import pytest
+from pvgisprototype.algorithms.noaa.time_offset import calculate_time_offset_time_series_noaa
+
+from .cases.time_offset_noaa import cases_time_offset_noaa
+from .cases.time_offset_noaa import cases_time_offset_noaa_ids
+
+from .conftest import GenericCheckCustomObjects
+
+
+class TestCalculateTimeOffsetNOAA(GenericCheckCustomObjects):
+
+    @pytest.fixture(params=cases_time_offset_noaa, ids=cases_time_offset_noaa_ids)
+    def cases(self, request):
+        return request.param
+
+    @pytest.fixture
+    def operation(self):
+        return calculate_time_offset_time_series_noaa
