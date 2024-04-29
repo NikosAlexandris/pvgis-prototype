616d17e801f216f8df6de1cd5122bbaac1be9023
Nikos Alexandris <nikosalexandris@duck.com>
2024-04-29 14:24:41 +0300

    refactor!: Add calculate_solar_altitude_time_series_jenco()
    And update calculate_solar_azimuth_time_series_jenco()

---
+++ pvgisprototype/algorithms/jenco/solar_altitude.py
@@ -0,0 +1,124 @@
+import numpy
+from devtools import debug
+from datetime import datetime
+from zoneinfo import ZoneInfo
+from math import sin
+from math import cos
+from math import isfinite
+from pvgisprototype import SolarAltitude
+from pvgisprototype import Longitude
+from pvgisprototype import Latitude
+from pvgisprototype.constants import RADIANS
+from pvgisprototype.log import logger
+from pvgisprototype.log import log_function_call
+from pvgisprototype.log import log_data_fingerprint
+from cachetools import cached
+from pvgisprototype.caching import custom_hashkey
+from pandas import DatetimeIndex
+from pvgisprototype.constants import DATA_TYPE_DEFAULT
+from pvgisprototype.constants import ARRAY_BACKEND_DEFAULT
+from pvgisprototype.algorithms.noaa.solar_declination import calculate_solar_declination_time_series_noaa
+from pvgisprototype.algorithms.noaa.solar_hour_angle import calculate_solar_hour_angle_time_series_noaa
+from pvgisprototype.cli.messages import WARNING_OUT_OF_RANGE_VALUES
+from pvgisprototype.constants import HASH_AFTER_THIS_VERBOSITY_LEVEL
+from pvgisprototype.constants import DEBUG_AFTER_THIS_VERBOSITY_LEVEL
+
+@log_function_call
+@cached(cache={}, key=custom_hashkey)
+# @validate_with_pydantic(CalculateSolarAltitudeTimeSeriesJencoInput)
+def calculate_solar_altitude_time_series_jenco(
+    longitude: Longitude,   # radians
+    latitude: Latitude,     # radians
+    timestamps: DatetimeIndex,
+    timezone: ZoneInfo,
+    dtype: str = DATA_TYPE_DEFAULT,
+    array_backend: str = ARRAY_BACKEND_DEFAULT,
+    verbose: int = 0,
+    log: int = 0,
+) -> SolarAltitude:
+    """Calculate the solar altitude angle (θ) for a time series at a specific
+    geographic latitude and longitude.
+
+    Parameters
+    ----------
+    longitude : float
+        Longitude of the location in radians.
+    latitude : float
+        Latitude of the location in radians.
+    timestamps : Union[datetime, DatetimeIndex]
+        Times for which the solar azimuth will be calculated.
+    timezone : ZoneInfo
+        Timezone of the location.
+    dtype : str, optional
+        Data type for the calculations.
+    array_backend : str, optional
+        Backend array library to use.
+    verbose : int, optional
+        Verbosity level of the function.
+    log : int, optional
+        Log level for the function.
+
+    Returns
+    -------
+    SolarAltitude
+        A custom data class that hold a NumPy NDArray of calculated solar
+        azimuth angles in radians, a method to convert the angles to degrees
+        and other metadata.
+
+    Notes
+    -----
+
+    References
+    ----------
+
+    Examples
+    --------
+
+    """
+    solar_declination_series = calculate_solar_declination_time_series_noaa(
+        timestamps=timestamps,
+        dtype=dtype,
+        backend=array_backend,
+        verbose=verbose,
+        log=log,
+    )
+    solar_hour_angle_series = calculate_solar_hour_angle_time_series_noaa(
+        longitude=longitude,
+        timestamps=timestamps,
+        timezone=timezone,
+        dtype=dtype,
+        backend=array_backend,
+        verbose=verbose,
+        log=log,
+    )
+    C31 = cos(latitude.radians) * numpy.cos(solar_declination_series.radians)
+    C33 = sin(latitude.radians) * numpy.sin(solar_declination_series.radians)
+    sine_solar_altitude_series = C31 * numpy.cos(solar_hour_angle_series.value) + C33
+    solar_altitude_series = numpy.arcsin(sine_solar_altitude_series)
+
+    if (
+        (solar_altitude_series < SolarAltitude().min_radians)
+        | (solar_altitude_series > SolarAltitude().max_radians)
+    ).any():
+        out_of_range_values = solar_altitude_series[
+            (solar_altitude_series < SolarAltitude().min_radians)
+            | (solar_altitude_series > SolarAltitude().max_radians)
+        ]
+        # raise ValueError(# ?
+        logger.warning(
+            f"{WARNING_OUT_OF_RANGE_VALUES} "
+            f"[{SolarAltitude().min_radians}, {SolarAltitude().max_radians}] radians"
+            f" in [code]solar_altitude_series[/code] : {out_of_range_values}"
+        )
+    log_data_fingerprint(
+        data=solar_altitude_series,
+        log_level=log,
+        hash_after_this_verbosity_level=HASH_AFTER_THIS_VERBOSITY_LEVEL,
+    )
+
+    return SolarAltitude(
+        value=solar_altitude_series,
+        unit=RADIANS,
+        # positioning_algorithm=solar_declination_series.position_algorithm,  #
+        timing_algorithm=solar_hour_angle_series.timing_algorithm,  #
+    )
