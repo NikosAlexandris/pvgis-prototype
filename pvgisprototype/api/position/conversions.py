d82984faa22f93da539813613ec85905c296f217
Nikos Alexandris <nikosalexandris@duck.com>
2024-04-30 18:26:18 +0300

    refactor!: Add and use angle conversion from North to South based systems
    The surface orientation angle for the solar incidence angle by Iqbal (1983) is measured from south to the projection of the surface normal on the horizontal plane, positive or negative if oriented west or east from south, respectively.
    
    Relates to #168.

--- pvgisprototype/api/position/conversions.py
+++ pvgisprototype/api/position/conversions.py
@@ -25,6 +25,25 @@ def convert_north_to_east_radians_convention(
     # return north_based_angle.radians
 
 
+def convert_north_to_south_radians_convention(
+    north_based_angle: HasRadians,
+):
+    """Convert an azimuth from North-based to East-based.
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+
+    Notes
+    -----
+
+    """
+    return (north_based_angle.radians - pi) % (2 * pi)
+    # return north_based_angle.radians
+
+
 def convert_east_to_north_radians_convention(azimuth_east_radians):
     # return (pi/2 - east_based_angle + 360) % 360
     return (azimuth_east_radians + 3 * pi / 2) % (2 * pi)
