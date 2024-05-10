5a11ffeb6aa423e33c61562e195f314e237a062d
Nikos Alexandris <nikosalexandris@duck.com>
2024-05-10 14:35:56 +0300

    Add positioning time series functions for PVIS!
    Mostly to compare PVGIS with Jenco. Or something like that. To keep, finally, only relevant functions as presented in original peer reviewed published documents.

--- pvgisprototype/algorithms/pvis/solar_azimuth.py
+++ pvgisprototype/algorithms/pvis/solar_azimuth.py
@@ -1,11 +1,14 @@
 from devtools import debug
 from datetime import datetime
 from zoneinfo import ZoneInfo
-from math import pi
 from math import sin
 from math import cos
 from math import acos
 from math import isfinite
+import numpy
+from numpy.core import numeric
+from pvgisprototype.algorithms.pvis.solar_declination import calculate_solar_declination_time_series_pvis
+from pvgisprototype.algorithms.noaa.solar_hour_angle import calculate_solar_hour_angle_time_series_noaa
 from pvgisprototype.validation.functions import validate_with_pydantic
 from pvgisprototype.validation.functions import CalculateSolarAzimuthPVISInputModel
 from pvgisprototype import Longitude
@@ -15,11 +18,16 @@ from pvgisprototype import SolarAzimuth
 from pvgisprototype.api.position.declination import calculate_solar_declination_pvis
 from pvgisprototype.api.position.solar_time import model_solar_time
 from pvgisprototype.algorithms.pvis.solar_hour_angle import calculate_solar_hour_angle_pvis
-from pvgisprototype.constants import RADIANS
+from pvgisprototype.constants import ARRAY_BACKEND_DEFAULT, DATA_TYPE_DEFAULT, LOG_LEVEL_DEFAULT, RADIANS, VERBOSE_LEVEL_DEFAULT
+from pandas import DatetimeIndex
+from pvgisprototype.algorithms.noaa.solar_declination import calculate_solar_declination_time_series_noaa
+from pvgisprototype.algorithms.noaa.solar_hour_angle import calculate_solar_hour_angle_time_series_noaa
+from pvgisprototype.constants import PERIGEE_OFFSET
+from pvgisprototype.constants import ECCENTRICITY_CORRECTION_FACTOR
 
 
-def convert_east_to_north_radians_convention(azimuth_east_radians):
-    return (azimuth_east_radians + 3 * pi / 2) % (2 * pi)
+# def convert_east_to_north_radians_convention(azimuth_east_radians):
+#     return (azimuth_east_radians + 3 * pi / 2) % (2 * pi)
 
 
 @validate_with_pydantic(CalculateSolarAzimuthPVISInputModel)
@@ -36,14 +44,13 @@ def calculate_solar_azimuth_pvis(
     -------
     solar_azimuth: float
 
-
     Returns
     -------
     solar_azimuth: float
 
     Notes
     -----
-    According to Hofierka! solar azimuth is measured from East!
+    According to Hofierka (2002) solar azimuth is measured from East!
     Conflicht with Jenvco 1992?
     """
     solar_declination = calculate_solar_declination_pvis(
@@ -87,3 +94,103 @@ def calculate_solar_azimuth_pvis(
             [{solar_azimuth.min_degrees}, {solar_azimuth.max_degrees}] degrees"
         )
     return solar_azimuth
+
+
+def calculate_solar_azimuth_time_series_pvis(
+    longitude: Longitude,   # radians
+    latitude: Latitude,     # radians
+    timestamps: DatetimeIndex,
+    timezone: ZoneInfo,
+    perigee_offset: float = PERIGEE_OFFSET,
+    eccentricity_correction_factor: float = ECCENTRICITY_CORRECTION_FACTOR,
+    dtype: str = DATA_TYPE_DEFAULT,
+    array_backend: str = ARRAY_BACKEND_DEFAULT,
+    verbose: int = VERBOSE_LEVEL_DEFAULT,
+    log: int = LOG_LEVEL_DEFAULT,
+) -> SolarAzimuth:
+    """Calculate the solar azimuth angle
+
+    Returns
+    -------
+    solar_azimuth: float
+
+
+    Returns
+    -------
+    solar_azimuth: float
+
+    Notes
+    -----
+    According to Hofierka! solar azimuth is measured from East!
+    Conflicht with Jenvco 1992?
+    """
+    solar_declination_series = calculate_solar_declination_time_series_pvis(
+        timestamps=timestamps,
+        perigee_offset=perigee_offset,
+        eccentricity_correction_factor=eccentricity_correction_factor,
+        dtype=dtype,
+        array_backend=array_backend,
+        verbose=verbose,
+        log=log,
+    )
+    C11 = sin(latitude.radians) * numpy.cos(solar_declination_series.radians)
+    C13 = - cos(latitude.radians) * numpy.sin(solar_declination_series.radians)  # Attention to the - sign
+    C22 = numpy.cos(solar_declination_series.radians)
+    solar_hour_angle_series = calculate_solar_hour_angle_time_series_noaa(
+        longitude=longitude,
+        timestamps=timestamps,
+        timezone=timezone,
+        dtype=dtype,
+        array_backend=array_backend,
+        verbose=verbose,
+        log=log,
+    )
+    x_solar_vector_component = - C22 * numpy.sin(solar_hour_angle_series.radians)
+    y_solar_vector_component = C11 * numpy.cos(solar_hour_angle_series.radians) + C13
+
+    # numerator = y_solar_vector_component
+    denominator_a = numpy.power(x_solar_vector_component, 2)
+    denominator_b = numpy.power(y_solar_vector_component, 2)
+    event_hour_angle_inclined = numpy.power(denominator_a + denominator_b, 0.5)
+    cosine_solar_azimuth_series = numpy.clip(y_solar_vector_component / event_hour_angle_inclined, -1, 1)
+    solar_azimuth_series = numpy.arccos(cosine_solar_azimuth_series)
+    solar_azimuth_series = numpy.where(
+        - C22 * numpy.sin(solar_hour_angle_series.radians) < 0,
+        numpy.pi / 2 - solar_azimuth_series,
+        solar_azimuth_series
+    )
+
+    # from math import pi
+    # solar_azimuth_series = numpy.where(
+    #     solar_azimuth_series < pi / 2,
+    #     pi / 2 - solar_azimuth_series,
+    #     5 * pi / 2 - solar_azimuth_series
+    # )
+    # solar_azimuth_series = numpy.where(
+    #     solar_azimuth_series >= 2 * pi,
+    #     solar_azimuth_series - 2 * pi,
+    #     solar_azimuth_series
+    # )
+
+    # -------------------------- convert east to north zero degrees convention
+    # PVGIS' follows Hofierka (2002) who states : azimuth is measured from East
+    # solar_azimuth_series = convert_east_to_north_radians_convention(solar_azimuth_series)
+    # convert east to north zero degrees convention --------------------------
+    # if (
+    #     not isfinite(solar_azimuth.degrees)
+    #     or not solar_azimuth.min_degrees <= solar_azimuth.degrees <= solar_azimuth.max_degrees
+    # ):
+    #     raise ValueError(
+    #         f"The calculated solar azimuth angle {solar_azimuth.degrees} is out of the expected range\
+    #         [{solar_azimuth.min_degrees}, {solar_azimuth.max_degrees}] degrees"
+    #     )
+
+    return SolarAzimuth(
+        value=solar_azimuth_series,
+        unit=RADIANS,
+        position_algorithm='PVIS',
+        timing_algorithm='PVIS',
+        origin='East'
+        # definition=incidence_angle_definition,
+        # description=incidence_angle_description,
+    ) # zero_direction='East'
--- pvgisprototype/algorithms/pvis/solar_hour_angle.py
+++ pvgisprototype/algorithms/pvis/solar_hour_angle.py
@@ -1,15 +1,24 @@
 from devtools import debug
 from math import isfinite
 from datetime import datetime
+
+import numpy
 from pvgisprototype import SolarHourAngle
 from pvgisprototype import HourAngleSunrise
+from pvgisprototype import Longitude
 from pvgisprototype import Latitude
+from pandas import DatetimeIndex
+from zoneinfo import ZoneInfo
 from pvgisprototype.validation.functions import validate_with_pydantic
 from pvgisprototype.validation.functions import CalculateSolarHourAnglePVISInputModel
 from pvgisprototype.validation.functions import CalculateEventHourAnglePVISInputModel
 from pvgisprototype.api.utilities.timestamp import timestamp_to_minutes
 from math import pi
 from pvgisprototype.constants import RADIANS
+from pvgisprototype.constants import DATA_TYPE_DEFAULT
+from pvgisprototype.constants import ARRAY_BACKEND_DEFAULT
+from pvgisprototype.constants import VERBOSE_LEVEL_DEFAULT
+from pvgisprototype.constants import LOG_LEVEL_DEFAULT
 
 
 @validate_with_pydantic(CalculateSolarHourAnglePVISInputModel)
@@ -61,6 +70,70 @@ def calculate_solar_hour_angle_pvis(
             [{hour_angle.min_degrees}, {hour_angle.max_degrees}] degrees"
         )
     return hour_angle
+
+
+def calculate_solar_hour_angle_time_series_pvis(
+    longitude: Longitude,
+    timestamps: DatetimeIndex, 
+    timezone: ZoneInfo,
+    dtype: str = DATA_TYPE_DEFAULT,
+    array_backend: str = ARRAY_BACKEND_DEFAULT,
+    verbose: int = VERBOSE_LEVEL_DEFAULT,
+    log: int = LOG_LEVEL_DEFAULT,
+)-> SolarHourAngle:
+    """Calculate the hour angle ω'
+
+    ω = (ST / 3600 - 12) * 15 * pi / 180
+
+    Parameters
+    ----------
+
+    solar_time: float
+        The solar time (ST) is a calculation of the passage of time based on the
+        position of the Sun in the sky. It is expected to be decimal hours in a
+        24 hour format and measured internally in seconds. 
+
+    Returns
+    --------
+
+    hour_angle: float
+        The solar hour angle (ω) is the angle at any instant through which the
+        earth has to turn to bring the meridian of the observer directly in
+        line with the sun's rays measured in radian.
+
+    Notes
+    -----
+    If not mistaken, in PVGIS' C source code, the conversion function is:
+
+        hour_angle = (solar_time / 3600 - 12) * 15 * 0.0175
+
+        where the solar time was given in seconds.
+    """
+    from pvgisprototype.algorithms.noaa.solar_time import calculate_true_solar_time_time_series_noaa
+    true_solar_time_series = calculate_true_solar_time_time_series_noaa(
+        longitude=longitude,
+        timestamps=timestamps,
+        timezone=timezone,
+        dtype=dtype,
+        array_backend=array_backend,
+        verbose=verbose,
+        log=log,
+    )
+    solar_hour_angle_series = (true_solar_time_series.minutes - 720.) * (numpy.pi / 720.)
+    # if (
+    #     not isfinite(hour_angle.degrees)
+    #     or not hour_angle.min_degrees <= hour_angle.degrees <= hour_angle.max_degrees
+    # ):
+    #     raise ValueError(
+    #         f"The calculated solar hour angle {hour_angle.degrees} is out of the expected range\
+    #         [{hour_angle.min_degrees}, {hour_angle.max_degrees}] degrees"
+    #     )
+    return SolarHourAngle(
+        value=solar_hour_angle_series,
+        unit=RADIANS,
+        position_algorithm='PVIS',
+        timing_algorithm='PVIS',
+    )
 
 
 @validate_with_pydantic(CalculateEventHourAnglePVISInputModel)
