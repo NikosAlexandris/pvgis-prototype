141487ee4edccdac6c01f2f3c6583ca1f4b83619
Nikos Alexandris <nikosalexandris@duck.com>
2024-05-10 14:35:56 +0300

    Add positioning time series functions for PVIS!
    Mostly to compare PVGIS with Jenco. Or something like that. To keep, finally, only relevant functions as presented in original peer reviewed published documents.

--- pvgisprototype/algorithms/pvis/solar_altitude.py
+++ pvgisprototype/algorithms/pvis/solar_altitude.py
@@ -1,9 +1,16 @@
 from devtools import debug
+from pvgisprototype.log import log_function_call
+from pvgisprototype.log import log_data_fingerprint
+from cachetools import cached
+from pvgisprototype.caching import custom_hashkey
 from datetime import datetime
+from pandas import DatetimeIndex
 from zoneinfo import ZoneInfo
 from math import cos
 from math import sin
 from math import asin
+from pvgisprototype.algorithms.pvis.solar_declination import calculate_solar_declination_time_series_pvis
+from pvgisprototype.algorithms.noaa.solar_hour_angle import calculate_solar_hour_angle_time_series_noaa
 from pvgisprototype.validation.functions import validate_with_pydantic
 from pvgisprototype.validation.functions import CalculateSolarAltitudePVISInputModel
 from pvgisprototype import Latitude
@@ -15,6 +22,10 @@ from pvgisprototype.api.position.solar_time import model_solar_time
 from pvgisprototype.algorithms.pvis.solar_hour_angle import calculate_solar_hour_angle_pvis
 from pvgisprototype.constants import RADIANS
 from math import isfinite
+from pvgisprototype.constants import HASH_AFTER_THIS_VERBOSITY_LEVEL
+from pvgisprototype.constants import DEBUG_AFTER_THIS_VERBOSITY_LEVEL
+from pvgisprototype.constants import ARRAY_BACKEND_DEFAULT, DATA_TYPE_DEFAULT, LOG_LEVEL_DEFAULT, RADIANS, VERBOSE_LEVEL_DEFAULT
+import numpy
 
 
 @validate_with_pydantic(CalculateSolarAltitudePVISInputModel)
@@ -64,8 +75,6 @@ def calculate_solar_altitude_pvis(
     )
     C31 = cos(latitude.radians) * cos(solar_declination.radians)
     C33 = sin(latitude.radians) * sin(solar_declination.radians)
-    C31 = cos(latitude.radians) * cos(solar_declination.radians)
-    C33 = sin(latitude.radians) * sin(solar_declination.radians)
     solar_time = model_solar_time(
         longitude=longitude,
         latitude=latitude,
@@ -100,3 +109,123 @@ def calculate_solar_altitude_pvis(
         debug(locals())
 
     return solar_altitude
+
+
+@log_function_call
+@cached(cache={}, key=custom_hashkey)
+# @validate_with_pydantic(CalculateSolarAltitudePVISInputModel)
+def calculate_solar_altitude_time_series_pvis(
+    longitude: Longitude,
+    latitude: Latitude,
+    timestamps: DatetimeIndex,
+    timezone: ZoneInfo,
+    perigee_offset: float,
+    eccentricity_correction_factor: float,
+    # time_offset_global: int,
+    # solar_time_model: SolarTimeModel,
+    dtype: str = DATA_TYPE_DEFAULT,
+    array_backend: str = ARRAY_BACKEND_DEFAULT,
+    verbose: int = VERBOSE_LEVEL_DEFAULT,
+    log: int = LOG_LEVEL_DEFAULT,
+) -> SolarAltitude:
+    """Calculate the solar altitude angle.
+
+    Calculate the solar altitude angle based on the equation 
+
+    sine_solar_altitude = (
+        sin(latitude.radians)
+        * np.sin(solar_declination_series.radians)
+        + cos(latitude.radians)
+        * np.cos(solar_declination_series.radians)
+        * np.cos(solar_hour_angle_series.radians)
+
+    Parameters
+    ----------
+    longitude : float
+        The longitude in degrees. This value will be converted to radians. 
+        It should be in the range [-180, 180].
+
+    latitude : float
+        The latitude in degrees. This value will be converted to radians. 
+        It should be in the range [-90, 90].
+    
+    timestamp : datetime, optional
+        The timestamp for which to calculate the solar altitude. 
+        If not provided, the current UTC time will be used.
+    
+    timezone : str, optional
+        The timezone to use for the calculation. 
+        If not provided, the system's local timezone will be used.
+    
+    angle_output_units : str, default 'radians'
+        The units to use for the output solar geometry variables. 
+        This should be either 'degrees' or 'radians'.
+
+    Returns
+    -------
+    float
+        The calculated solar altitude.
+
+    Notes
+    -----
+
+    NOAA's equation is practically the same, though it targets the cosine
+    function of the solar zenith angle which it the complementary of the solar
+    altitude angle.
+
+    cosine_solar_zenith = (
+        sin(latitude.radians)
+        * np.sin(solar_declination_series.radians)
+        + cos(latitude.radians)
+        * np.cos(solar_declination_series.radians)
+        * np.cos(solar_hour_angle_series.radians)
+
+    """
+    solar_declination_series = calculate_solar_declination_time_series_pvis(
+        timestamps=timestamps,
+        perigee_offset=perigee_offset,
+        eccentricity_correction_factor=eccentricity_correction_factor,
+    )
+    C31 = cos(latitude.radians) * numpy.cos(solar_declination_series.radians)
+    C33 = sin(latitude.radians) * numpy.sin(solar_declination_series.radians)
+    solar_hour_angle_series = calculate_solar_hour_angle_time_series_noaa(
+        longitude=longitude,
+        timestamps=timestamps,
+        timezone=timezone,
+        dtype=dtype,
+        array_backend=array_backend,
+        verbose=verbose,
+        log=log,
+    )
+    solar_altitude_series = numpy.arcsin(
+        C31 * numpy.cos(solar_hour_angle_series.radians) + C33
+    )
+
+    # The hour angle of the time of sunrise/sunset over a horizontal surface
+    # Thr,s can be calculated then as:
+    # cos(event_hour_angle_horizontal) = -C33 / C31
+
+    # if (
+    #     not isfinite(solar_altitude.degrees)
+    #     or not solar_altitude.min_degrees <= solar_altitude.degrees <= solar_altitude.max_degrees
+    # ):
+    #     raise ValueError(
+    #         f"The calculated solar altitude angle {solar_altitude.degrees} is out of the expected range\
+    #         [{solar_altitude.min_degrees}, {solar_altitude.max_degrees}] radians"
+    #     )
+
+    if verbose > DEBUG_AFTER_THIS_VERBOSITY_LEVEL:
+        debug(locals())
+
+    log_data_fingerprint(
+            data=solar_altitude_series,
+            log_level=log,
+            hash_after_this_verbosity_level=HASH_AFTER_THIS_VERBOSITY_LEVEL,
+    )
+
+    return SolarAltitude(
+        value=solar_altitude_series,
+        unit=RADIANS,
+        position_algorithm='PVIS',
+        timing_algorithm=solar_hour_angle_series.timing_algorithm,
+    )
--- pvgisprototype/algorithms/pvis/solar_declination.py
+++ pvgisprototype/algorithms/pvis/solar_declination.py
@@ -1,14 +1,30 @@
+from devtools import debug
+from pvgisprototype.log import log_function_call
+from pvgisprototype.log import log_data_fingerprint
+from cachetools import cached
+from pvgisprototype.caching import custom_hashkey
+from pandas import DatetimeIndex
+from pvgisprototype.algorithms.noaa.fractional_year import calculate_fractional_year_time_series_noaa
 from pvgisprototype.validation.functions import validate_with_pydantic
 from pvgisprototype.validation.functions import CalculateSolarDeclinationPVISInputModel
 from datetime import datetime
 from pvgisprototype import SolarDeclination
 from pvgisprototype.algorithms.pvis.fractional_year import calculate_fractional_year_pvis
+from pvgisprototype.algorithms.pvis.fractional_year import calculate_fractional_year_series_pvis
 from math import sin
 from math import asin
 from math import isfinite
 from pvgisprototype.constants import PERIGEE_OFFSET
 from pvgisprototype.constants import ECCENTRICITY_CORRECTION_FACTOR
+from pvgisprototype.constants import DATA_TYPE_DEFAULT
+from pvgisprototype.constants import ARRAY_BACKEND_DEFAULT
 from pvgisprototype.constants import RADIANS
+from pvgisprototype.constants import VERBOSE_LEVEL_DEFAULT
+from pvgisprototype.constants import LOG_LEVEL_DEFAULT
+from pvgisprototype.cli.messages import WARNING_OUT_OF_RANGE_VALUES
+from pvgisprototype.constants import HASH_AFTER_THIS_VERBOSITY_LEVEL
+from pvgisprototype.constants import DEBUG_AFTER_THIS_VERBOSITY_LEVEL
+import numpy
 
 
 @validate_with_pydantic(CalculateSolarDeclinationPVISInputModel)
@@ -79,3 +95,91 @@ def calculate_solar_declination_pvis(
             [{solar_declination.min_degrees}, {solar_declination.max_degrees}] degrees"
         )
     return solar_declination
+
+
+# @validate_with_pydantic(CalculateSolarDeclinationPVISInputModel)
+@log_function_call
+@cached(cache={}, key=custom_hashkey)
+def calculate_solar_declination_time_series_pvis(
+    timestamps: DatetimeIndex,
+    perigee_offset: float = PERIGEE_OFFSET,
+    eccentricity_correction_factor: float = ECCENTRICITY_CORRECTION_FACTOR,
+    dtype: str = DATA_TYPE_DEFAULT,
+    array_backend: str = ARRAY_BACKEND_DEFAULT,
+    verbose: int = VERBOSE_LEVEL_DEFAULT,
+    log: int = LOG_LEVEL_DEFAULT,
+) -> SolarDeclination:
+    """Approximate the sun's declination for a given day of the year.
+
+    The solar declination is the angle between the Sun's rays and the
+    equatorial plane of Earth. It varies throughout the year due to the tilt of
+    the Earth's axis and is an important parameter in determining the seasons
+    and the amount of solar radiation received at different latitudes.
+
+    The function calculates the `proportion` of the way through the year (in
+    radians), which is given by `(2 * pi * day_of_year) / 365.25`.
+    The `0.3978`, `1.4`, and `0.0355` are constants in the approximation
+    formula, with the `0.0489` being an adjustment factor for the slight
+    eccentricity of Earth's orbit.
+  
+    Parameters
+    ----------
+    day_of_year: int
+        The day of the year (ranging from 1 to 365 or 366 in a leap year).
+
+    Returns
+    -------
+    solar_declination: float
+        The solar declination in radians for the given day of the year.
+
+    Notes
+    -----
+
+    The equation used here is a simple approximation and bases upon a direct
+    translation from PVGIS' rsun3 source code:
+
+      - from file: rsun_base.cpp
+      - function: com_declin(no_of_day)
+
+    For more accurate calculations of solar position, comprehensive models like
+    the Solar Position Algorithm (SPA) are typically used.
+    """
+    fractional_year_series = calculate_fractional_year_series_pvis(
+        timestamps=timestamps,
+        dtype=dtype,
+        array_backend=array_backend,
+        verbose=verbose,
+        log=log,
+    )
+    solar_declination_series = numpy.arcsin(
+        0.3978
+        * numpy.sin(
+            fractional_year_series.radians
+            - 1.4
+            + eccentricity_correction_factor
+            * numpy.sin(fractional_year_series.radians - perigee_offset)
+        )
+    )
+    # if (
+    #     not isfinite(solar_declination.degrees)
+    #     or not solar_declination.min_degrees <= solar_declination.degrees <= solar_declination.max_degrees
+    # ):
+    #     raise ValueError(
+    #         f"The calculated solar declination angle {solar_declination_series.degrees} is out of the expected range\
+    #         [{solar_declination_series.min_degrees}, {solar_declination_series.max_degrees}] degrees"
+    #     )
+    if verbose > DEBUG_AFTER_THIS_VERBOSITY_LEVEL:
+        debug(locals())
+
+    log_data_fingerprint(
+        data=solar_declination_series,
+        log_level=log,
+        hash_after_this_verbosity_level=HASH_AFTER_THIS_VERBOSITY_LEVEL,
+    )
+
+    return SolarDeclination(
+        value=solar_declination_series,
+        unit=RADIANS,
+        position_algorithm='PVIS',
+        timing_algorithm='PVIS',
+    )
