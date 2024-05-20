3ffe74743ec0f1cdcbb2f9ca4bdb2811d6ed7769
Nikos Alexandris <nikosalexandris@duck.com>
2024-05-20 16:37:05 +0300

    refactor!: Update uniplot_data_array_time_series()

--- pvgisprototype/api/plot.py
+++ pvgisprototype/api/plot.py
@@ -1,15 +1,17 @@
 from pathlib import Path
-from typing import Optional
+from typing import List, Optional
 from pandas import DatetimeIndex
 from pandas import Timestamp
 from pvgisprototype.api.series.models import MethodForInexactMatches
-from pvgisprototype.constants import UNITS_NAME
+from pvgisprototype.constants import AZIMUTH_ORIGIN_NAME, INCIDENCE_DEFINITION, NOT_AVAILABLE, UNITS_NAME, UNITLESS
 from pvgisprototype.constants import TERMINAL_WIDTH_FRACTION
 from pvgisprototype.constants import VERBOSE_LEVEL_DEFAULT
 from pvgisprototype.api.series.hardcodings import exclamation_mark
 from pvgisprototype.api.series.hardcodings import check_mark
 from pvgisprototype.api.series.hardcodings import x_mark
 from rich import print
+import numpy
+import xarray
 from numpy import ndarray
 from pvgisprototype.log import logger
 from pvgisprototype.log import log_function_call
@@ -21,8 +23,10 @@ def uniplot_data_array_time_series(
     list_extra_data_arrays,
     # longitude: float,
     # latitude: float,
+    orientation: List[float] | float = None,
+    tilt: List[float] | float = None,
     # time_series_2: Path = None,
-    # timestamps: DatetimeIndex = None,
+    timestamps: DatetimeIndex = None,
     # start_time: Timestamp = None,
     # end_time: Timestamp = None,
     resample_large_series: bool = False,
@@ -30,57 +34,65 @@ def uniplot_data_array_time_series(
     supertitle: str = None,
     title: str = None,
     label: str = None,
-    label_2: str = None,
+    extra_legend_labels: List[str] | None = None,
     unit: str = UNITS_NAME,
     terminal_width_fraction: float = TERMINAL_WIDTH_FRACTION,
     verbose: int = VERBOSE_LEVEL_DEFAULT,
 ):
     """Plot time series in the terminal"""
     import shutil
-    terminal_columns, _ = shutil.get_terminal_size()  # we don't need lines!
-    terminal_length = int(terminal_columns * terminal_width_fraction)
     from functools import partial
     from uniplot import plot as default_plot
+
+    terminal_columns, _ = shutil.get_terminal_size()  # we don't need lines!
+    terminal_length = int(terminal_columns * terminal_width_fraction)
     plot = partial(default_plot, width=terminal_length)
 
-    if resample_large_series:
-        data_array = data_array.resample(time='1M').mean()
+    def convert_and_resample(array, timestamps, freq='1ME'):
+        # Create xarray DataArray with time dimension
+        data_array = xarray.DataArray(array, coords=[timestamps], dims=["time"])
+        if resample_large_series:
+            return data_array.resample(time=freq).mean()
+        return data_array
 
-    if resample_large_series:
-        list_extra_data_arrays = [extra_array.resample(time='1M').mean() for extra_array in list_extra_data_arrays]
+    data_array = convert_and_resample(data_array, timestamps)
+    if list_extra_data_arrays:
+        list_extra_data_arrays = [convert_and_resample(extra_array, timestamps) for extra_array in list_extra_data_arrays]
 
     y_series = (
-        [data_array] + list_extra_data_arrays if list_extra_data_arrays else data_array
+        [data_array] + (list_extra_data_arrays if list_extra_data_arrays else [])
     )
 
     if isinstance(data_array, float):
         logger.error(f"{exclamation_mark} Aborting as I cannot plot the single float value {float}!", alt=f"{exclamation_mark} [red]Aborting[/red] as I [red]cannot[/red] plot the single float value {float}!")
         return
 
-    if isinstance(data_array, ndarray):
-        # supertitle = getattr(photovoltaic_power_output_series, 'long_name', 'Untitled')
-        # label = getattr(photovoltaic_power_output_series, 'name', None)
-        # label_2 = getattr(photovoltaic_power_output_series_2, 'name', None) if photovoltaic_power_output_series_2 is not None else None
-        # unit = getattr(photovoltaic_power_output_series, 'units', None)
-        supertitle = getattr(data_array, 'long_name', 'Untitled')
-        label = getattr(data_array, 'name', None)
-        list_extra_data_arrays = list_extra_data_arrays if list_extra_data_arrays is not None else []
-        label_2 = [getattr(extra_array, 'name', None) if extra_array is not None else None for extra_array in list_extra_data_arrays]
-        unit = getattr(data_array, 'units', None)
-        print(f'[reverse]Uniplot[/reverse]')
-        try:
-            plot(
-                # xs=timestamps,
-                ys=y_series,
-                legend_labels = [label, label_2] if label_2 else label,
-                lines=lines,
-                title=title if title else supertitle,
-                y_unit=' ' + str(unit),
-            )
-        except IOError as e:
-            raise IOError(
-                f"Could not _uniplot_ {data_array.value=}"
-            ) from e
+    # supertitle = getattr(photovoltaic_power_output_series, 'long_name', 'Untitled')
+    # label = getattr(photovoltaic_power_output_series, 'name', None)
+    # label_2 = getattr(photovoltaic_power_output_series_2, 'name', None) if photovoltaic_power_output_series_2 is not None else None
+    # unit = getattr(photovoltaic_power_output_series, 'units', None)
+    supertitle = getattr(data_array, 'long_name', 'Untitled')
+    label = label if label else getattr(data_array, 'name', None)
+    list_extra_data_arrays = list_extra_data_arrays if list_extra_data_arrays is not None else []
+    # legend_labels = [label] + [getattr(extra_array, 'name', None) for extra_array in list_extra_data_arrays]
+    legend_labels = [label] + (extra_legend_labels if extra_legend_labels else [])
+    unit = getattr(data_array, 'units', None) or unit
+    print(f'[reverse]Uniplot[/reverse]')
+    try:
+        plot(
+            # xs=timestamps,
+            ys=y_series,
+            legend_labels=legend_labels,
+            lines=lines,
+            title=title if title else supertitle,
+            y_unit=' ' + str(unit),
+        )
+    except IOError as e:
+        raise IOError(
+            f"Could not _uniplot_ {data_array.value=}"
+        ) from e
+
+
 def uniplot_solar_position_series(
     solar_position_series,
     # index: bool = False,
