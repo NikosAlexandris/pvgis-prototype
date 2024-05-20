9d62a921095c664fc740d615b5aa95f87ede6d71
Nikos Alexandris <nikosalexandris@duck.com>
2024-05-20 16:32:11 +0300

    refactor!: Update modules in algorithms/pvis
    Rename functions to _hofierka() instead of _pvis(). To Do : Rename directory to `hofierka`.
    Overall clean-up after removal of single-timestamp CLI + API functions.

--- pvgisprototype/algorithms/pvis/fractional_year.py
+++ pvgisprototype/algorithms/pvis/fractional_year.py
@@ -25,6 +25,7 @@ from pvgisprototype.constants import DEBUG_AFTER_THIS_VERBOSITY_LEVEL
 from pvgisprototype.validation.arrays import create_array
 from pvgisprototype.cli.messages import WARNING_OUT_OF_RANGE_VALUES
 import numpy
+from pvgisprototype.api.position.models import SolarPositionModel
 
 
 @validate_with_pydantic(CalculateFractionalYearPVISInputModel)
@@ -61,22 +62,51 @@ def calculate_fractional_year_pvis(
 # @validate_with_pydantic(CalculateFractionalYearPVISInputModel)
 @log_function_call
 @cached(cache={}, key=custom_hashkey)
-def calculate_fractional_year_series_pvis(
+def calculate_day_angle_series_hofierka(
     timestamps: DatetimeIndex,
     dtype: str = DATA_TYPE_DEFAULT,
     array_backend: str = ARRAY_BACKEND_DEFAULT,
     verbose: int = VERBOSE_LEVEL_DEFAULT,
     log: int = LOG_LEVEL_DEFAULT,
 ) -> FractionalYear:
-    """Calculate fractional year in radians
+    """Calculate the day angle for a time series.
+
+    Parameters
+    ----------
+    timestamps : DatetimeIndex
+        A Pandas DatetimeIndex representing the timestamps.
+
+    dtype : str, optional
+        The data type for the calculations (the default is 'float32').
+
+    array_backend : str, optional
+        The backend used for calculations (the default is 'NUMPY').
+
+    verbose: int
+        Verbosity level
+
+    log: int
+        Log level
+
+    Returns
+    -------
+    FractionalYear
+        A FractionalYear object containing the calculated fractional year
+        series.
 
     Notes
     -----
-    In PVGIS' C source code, this is called `day_angle`
+    The day angle [0]_ is the same quantity called Fractional Year in NOAA's
+    solar geometry equations, i.e. the function
+    calculate_fractional_year_time_series_noaa().
 
+    References
+    ----------
+    .. [0] Hofierka, 2002
+    
     NOAA's corresponding equation:
 
-        fractional_year = (
+        day_angle = (
             2
             * pi
             / 365
@@ -91,28 +121,39 @@ def calculate_fractional_year_series_pvis(
         "init_method": "zeros",
         "backend": array_backend,
     }  # Borrow shape from timestamps
-    fractional_year_series = create_array(**array_parameters)
-    fractional_year_series = numpy.array(
+    day_angle_series = create_array(**array_parameters)
+    day_angle_series = numpy.array(
         2 * pi * days_of_year / days_in_years,
         dtype=dtype,
     )
 
-    # if not ((isclose(FRACTIONAL_YEAR_MINIMUM, fractional_year) or FRACTIONAL_YEAR_MINIMUM < fractional_year) and 
-    #         (isclose(FRACTIONAL_YEAR_MAXIMUM, fractional_year) or fractional_year < FRACTIONAL_YEAR_MAXIMUM)):
-    #     raise ValueError(f'Calculated fractional year {fractional_year} is out of the expected range [{FRACTIONAL_YEAR_MINIMUM}, {FRACTIONAL_YEAR_MAXIMUM}] radians')
+    if not numpy.all(
+        (FractionalYear().min_radians <= day_angle_series)
+        & (day_angle_series <= FractionalYear().max_radians)
+    ):
+        index_of_out_of_range_values = numpy.where(
+            (day_angle_series < FractionalYear().min_radians)
+            | (day_angle_series > FractionalYear().max_radians)
+        )
+        out_of_range_values = day_angle_series[index_of_out_of_range_values]
+        # Report values in "human readable" degrees
+        raise ValueError(
+            f"{WARNING_OUT_OF_RANGE_VALUES} "
+            f"[{FractionalYear().min_radians}, {FractionalYear().max_radians}] radians"
+            f" in [code]day_angle_series[/code] : {out_of_range_values}"
+        )
 
     if verbose > DEBUG_AFTER_THIS_VERBOSITY_LEVEL:
         debug(locals())
 
     log_data_fingerprint(
-            data=fractional_year_series,
+            data=day_angle_series,
             log_level=log,
             hash_after_this_verbosity_level=HASH_AFTER_THIS_VERBOSITY_LEVEL,
     )
             
     return FractionalYear(
-        value=fractional_year_series,
+        value=day_angle_series,
         unit=RADIANS,
-        position_algorithm='PVIS',
-        timing_algorithm='PVIS',
+        position_algorithm=SolarPositionModel.hofierka,
     )
