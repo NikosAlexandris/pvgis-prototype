8c3735ca0c18b70844027ea31b399e4a18a71dc7
Nikos Alexandris <nikosalexandris@duck.com>
2024-05-20 16:32:57 +0300

    refactor!: Update modules in algorithms/pvlib
    Add support for time series!

--- pvgisprototype/algorithms/pvlib/solar_hour_angle.py
+++ pvgisprototype/algorithms/pvlib/solar_hour_angle.py
@@ -8,6 +8,9 @@ from pvgisprototype import SolarHourAngle
 import pandas
 import pvlib
 from pvgisprototype.constants import DEGREES
+from pandas import DatetimeIndex
+from pvgisprototype.cli.messages import WARNING_OUT_OF_RANGE_VALUES
+import numpy
 
 
 @validate_with_pydantic(SolarHourAnglePVLIBInput)
@@ -39,3 +42,39 @@ def calculate_solar_hour_angle_pvlib(
             [{solar_hour_angle.min_degrees}, {solar_hour_angle.max_degrees}] degrees"
         )
     return solar_hour_angle
+
+
+# @validate_with_pydantic(SolarHourAnglePVLIBInput)
+def calculate_solar_hour_angle_series_pvlib(
+    longitude: Longitude,
+    timestamps: DatetimeIndex,
+) -> SolarHourAngle:
+    """Calculate the solar hour angle in radians.
+    """
+    equation_of_time_series = pvlib.solarposition.equation_of_time_spencer71(timestamps.dayofyear)
+    solar_hour_angle_series = pvlib.solarposition.hour_angle(
+        timestamps,
+        longitude.degrees,
+        equation_of_time=equation_of_time_series
+        )
+    if not numpy.all(
+        (SolarHourAngle().min_degrees <= solar_hour_angle_series)
+        & (solar_hour_angle_series <= SolarHourAngle().max_degrees)
+    ):
+        out_of_range_values = solar_hour_angle_series[
+            ~(
+                (-SolarHourAngle().min_degrees <= solar_hour_angle_series)
+                & (solar_hour_angle_series <= SolarHourAngle().max_degrees)
+            )
+        ]
+        raise ValueError(
+            f"{WARNING_OUT_OF_RANGE_VALUES} "
+            f"[{SolarHourAngle().min_degrees}, {SolarHourAngle().max_degrees}] degrees"
+            f" in [code]solar_hour_angle_series[/code] : {numpy.degrees(out_of_range_values)}"
+        )
+    return SolarHourAngle(
+        value=solar_hour_angle_series,
+        unit=DEGREES,
+        position_algorithm='pvlib (Spencer 1971)',
+        timing_algorithm='pvlib',
+    )
