f6ab16f6b61dc3cd01c3fb34c0aadbacb729002b
Nikos Alexandris <nikosalexandris@duck.com>
2024-05-20 16:41:32 +0300

    refactor!: Update CLI functions after updates for --uniplot

--- pvgisprototype/cli/irradiance/diffuse/horizontal.py
+++ pvgisprototype/cli/irradiance/diffuse/horizontal.py
@@ -121,6 +121,7 @@ def get_diffuse_horizontal_irradiance_time_series(
     groupby: Annotated[Optional[str], typer_option_groupby] = GROUPBY_DEFAULT,
     csv: Annotated[Path, typer_option_csv] = CSV_PATH_DEFAULT,
     uniplot: Annotated[bool, typer_option_uniplot] = UNIPLOT_FLAG_DEFAULT,
+    resample_large_series: Annotated[bool, 'Resample large time series?'] = False,
     terminal_width_fraction: Annotated[float, typer_option_uniplot_terminal_width] = TERMINAL_WIDTH_FRACTION,
     verbose: Annotated[int, typer_option_verbose] = VERBOSE_LEVEL_DEFAULT,
     index: Annotated[bool, typer_option_index] = INDEX_IN_TABLE_OUTPUT_FLAG_DEFAULT,
@@ -199,12 +200,15 @@ def get_diffuse_horizontal_irradiance_time_series(
         uniplot_data_array_time_series(
             data_array=diffuse_horizontal_irradiance_time_series.value,
             list_extra_data_arrays=None,
+            timestamps=timestamps,
+            resample_large_series=resample_large_series,
             lines=True,
             supertitle = 'Diffuse Horizontal Irradiance Series',
             title = 'Diffuse Horizontal Irradiance Series',
             label = 'Diffuse Horizontal Irradiance',
-            label_2 = None,
+            extra_legend_labels=None,
             unit = IRRADIANCE_UNITS,
+            terminal_width_fraction=terminal_width_fraction,
         )
     if fingerprint:
         from pvgisprototype.cli.print import print_finger_hash
