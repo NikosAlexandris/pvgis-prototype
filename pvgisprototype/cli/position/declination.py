148293535934e601c226cdb7d58967a00bc5688c
Nikos Alexandris <nikosalexandris@duck.com>
2024-05-22 03:27:36 +0300

    refactor!: Update API position/declination.py, add calculate_solar_declination_series()
    Refers to #180.

--- pvgisprototype/cli/position/declination.py
+++ pvgisprototype/cli/position/declination.py
@@ -2,22 +2,22 @@
 CLI module to calculate the solar declination angle for a location and moment in time.
 """
 
+from pandas import DatetimeIndex
 from rich import print
 from typing import Annotated
 from typing import Optional
 from typing import List
 from datetime import datetime
 from zoneinfo import ZoneInfo
+from pathlib import Path
 
 from pvgisprototype.api.position.models import SolarDeclinationModel
 from pvgisprototype.api.position.models import select_models
 
-from pvgisprototype.cli.typer.timestamps import typer_argument_timestamp
+from pvgisprototype.api.utilities.conversions import convert_float_to_degrees_if_requested
 from pvgisprototype.cli.typer.timestamps import typer_option_timezone
 from pvgisprototype.cli.typer.timestamps import typer_option_local_time
-from pvgisprototype.cli.typer.timestamps import typer_option_random_time
 from pvgisprototype.cli.typer.position import typer_option_solar_position_model
-from pvgisprototype.cli.typer.refraction import typer_option_apply_atmospheric_refraction
 from pvgisprototype.cli.typer.timing import typer_option_solar_time_model
 from pvgisprototype.cli.typer.earth_orbit import typer_option_perigee_offset
 from pvgisprototype.cli.typer.earth_orbit import typer_option_eccentricity_correction_factor
@@ -25,27 +25,58 @@ from pvgisprototype.cli.typer.output import typer_option_angle_output_units
 from pvgisprototype.cli.typer.output import typer_option_rounding_places
 from pvgisprototype.cli.typer.verbosity import typer_option_verbose
 
-from pvgisprototype.api.utilities.timestamp import random_datetimezone
 from pvgisprototype.api.position.declination import calculate_solar_declination_series
-from pvgisprototype.cli.print import print_solar_position_table
 
-from pvgisprototype.constants import RADIANS
+from pvgisprototype.constants import ARRAY_BACKEND_DEFAULT, CSV_PATH_DEFAULT, DATA_TYPE_DEFAULT, ECCENTRICITY_CORRECTION_FACTOR, INDEX_IN_TABLE_OUTPUT_FLAG_DEFAULT, PERIGEE_OFFSET, QUIET_FLAG_DEFAULT, RADIANS, RANDOM_TIMESTAMPS_FLAG_DEFAULT, TERMINAL_WIDTH_FRACTION, UNIPLOT_FLAG_DEFAULT
 from pvgisprototype.constants import ROUNDING_PLACES_DEFAULT
 from pvgisprototype.constants import VERBOSE_LEVEL_DEFAULT
+from pvgisprototype.cli.typer.timestamps import typer_argument_timestamps
+from pvgisprototype.cli.typer.timestamps import typer_option_random_timestamps
+from pvgisprototype.cli.typer.timestamps import typer_option_start_time
+from pvgisprototype.cli.typer.timestamps import typer_option_periods
+from pvgisprototype.cli.typer.timestamps import typer_option_frequency
+from pvgisprototype.cli.typer.timestamps import typer_option_end_time
+from pvgisprototype.cli.typer.timestamps import typer_option_timezone
+from pvgisprototype.api.utilities.timestamp import now_utc_datetimezone
+from pvgisprototype.cli.typer.data_processing import typer_option_dtype
+from pvgisprototype.cli.typer.data_processing import typer_option_array_backend
+from pvgisprototype.cli.typer.plot import typer_option_uniplot
+from pvgisprototype.cli.typer.plot import typer_option_uniplot_terminal_width
+from pvgisprototype.cli.typer.output import typer_option_csv
+from pvgisprototype.cli.typer.verbosity import typer_option_quiet
+from pvgisprototype.cli.typer.output import typer_option_index
+from pvgisprototype.log import logger
+from pvgisprototype.log import log_function_call
+from pvgisprototype.cli.typer.output import typer_option_panels_output
 
 
+@log_function_call
 def declination(
-    timestamp: Annotated[Optional[datetime], typer_argument_timestamp],
+    timestamps: Annotated[DatetimeIndex, typer_argument_timestamps] = str(now_utc_datetimezone()),
+    start_time: Annotated[Optional[datetime], typer_option_start_time] = None,  # Used by a callback function
+    periods: Annotated[Optional[int], typer_option_periods] = None,  # Used by a callback function
+    frequency: Annotated[Optional[str], typer_option_frequency] = None,  # Used by a callback function
+    end_time: Annotated[Optional[datetime], typer_option_end_time] = None,  # Used by a callback function
     timezone: Annotated[Optional[str], typer_option_timezone] = None,
     local_time: Annotated[bool, typer_option_local_time] = False,
-    random_time: Annotated[bool, typer_option_random_time] = False,
+    random_timestamps: Annotated[bool, typer_option_random_timestamps] = RANDOM_TIMESTAMPS_FLAG_DEFAULT,  # Used by a callback function
     model: Annotated[List[SolarDeclinationModel], typer_option_solar_position_model] = [SolarDeclinationModel.pvis],
-    perigee_offset: Annotated[float, typer_option_perigee_offset] = 0.048869,
-    eccentricity_correction_factor: Annotated[float, typer_option_eccentricity_correction_factor] = 0.03344,
+    perigee_offset: Annotated[float, typer_option_perigee_offset] = PERIGEE_OFFSET,
+    eccentricity_correction_factor: Annotated[float, typer_option_eccentricity_correction_factor] = ECCENTRICITY_CORRECTION_FACTOR,
     angle_output_units: Annotated[str, typer_option_angle_output_units] = RADIANS,
     rounding_places: Annotated[Optional[int], typer_option_rounding_places] = ROUNDING_PLACES_DEFAULT,
+    group_models: Annotated[Optional[bool], 'Visually cluster time series results per model'] = False,
+    csv: Annotated[Path, typer_option_csv] = CSV_PATH_DEFAULT,
+    dtype: Annotated[str, typer_option_dtype] = DATA_TYPE_DEFAULT,
+    array_backend: Annotated[str, typer_option_array_backend] = ARRAY_BACKEND_DEFAULT,
+    uniplot: Annotated[bool, typer_option_uniplot] = UNIPLOT_FLAG_DEFAULT,
+    resample_large_series: Annotated[bool, 'Resample large time series?'] = False,
+    terminal_width_fraction: Annotated[float, typer_option_uniplot_terminal_width] = TERMINAL_WIDTH_FRACTION,
     verbose: Annotated[int, typer_option_verbose] = VERBOSE_LEVEL_DEFAULT,
-) -> float:
+    panels: Annotated[bool, typer_option_panels_output] = False,
+    index: Annotated[bool, typer_option_index] = INDEX_IN_TABLE_OUTPUT_FLAG_DEFAULT,
+    quiet: Annotated[bool, typer_option_quiet] = QUIET_FLAG_DEFAULT,
+) -> None:
     """Calculate the solar declination angle 
 
     The solar declination (delta) is the angle between the line from the Earth
@@ -60,43 +91,141 @@ def declination(
     solar_declination: float
     """
     # Initialize with None ---------------------------------------------------
-    user_requested_timestamp = None
+    user_requested_timestamps = None
     user_requested_timezone = None
     # -------------------------------------------- Smarter way to do this? ---
-
-    # Possible to move to a callback? ----------------------------------------
-    if random_time:
-        timestamp, timezone = random_datetimezone()
-    # ------------------------------------------------------------------------
-
-    # Convert the input timestamp to UTC, for _all_ internal calculations
+    
     utc_zoneinfo = ZoneInfo("UTC")
-    if timestamp.tzinfo != utc_zoneinfo:
+    if timestamps.tz != utc_zoneinfo:
 
         # Note the input timestamp and timezone
-        user_requested_timestamp = timestamp
+        user_requested_timestamps = timestamps
         user_requested_timezone = timezone
 
-        timestamp = timestamp.astimezone(utc_zoneinfo)
-        print(f'The requested timestamp - zone {user_requested_timestamp} {user_requested_timezone} has been converted to {timestamp} for all internal calculations!')
+        timestamps = timestamps.tz_localize(utc_zoneinfo)
+        timezone = utc_zoneinfo
+        logger.info(f'Input timestamps & zone ({user_requested_timestamps} & {user_requested_timezone}) converted to {timestamps} for all internal calculations!')
 
     solar_declination_models = select_models(SolarDeclinationModel, model)  # Using a callback fails!
-    solar_declination = calculate_solar_declination_series(
-        # timestamps=timestamp,
-        # timezone=timezone,
-        # declination_models=solar_declination_models,
-        # eccentricity_correction_factor=eccentricity_correction_factor,
-        # perigee_offset=perigee_offset,
-        # angle_output_units=angle_output_units,
-    )
-    print_solar_position_table(
-        longitude=None,
-        latitude=None,
-        timestamp=timestamp,
+    solar_declination_series = calculate_solar_declination_series(
+        timestamps=timestamps,
         timezone=timezone,
-        table=solar_declination,
-        rounding_places=rounding_places,
-        declination=True,
-        user_requested_timestamp=user_requested_timestamp, 
-        user_requested_timezone=user_requested_timezone,
+        solar_declination_models=solar_declination_models,
+        eccentricity_correction_factor=eccentricity_correction_factor,
+        perigee_offset=perigee_offset,
+        angle_output_units=angle_output_units,
+        array_backend=array_backend,
+        dtype=dtype,
+        verbose=verbose,
     )
+    from pvgisprototype.cli.print import print_solar_position_series_table
+    if not quiet:
+        if timestamps.size == 1:
+            if not panels:
+                from pvgisprototype.cli.print import print_solar_position_table
+                print_solar_position_table(
+                    longitude=None,
+                    latitude=None,
+                    timestamp=timestamps,
+                    timezone=timezone,
+                    table=solar_declination_series,
+                    rounding_places=rounding_places,
+                    timing=True,
+                    declination=True,
+                    hour_angle=True,
+                    zenith=True,
+                    altitude=True,
+                    azimuth=True,
+                    surface_orientation=True,
+                    surface_tilt=True,
+                    incidence=True,  # Add Me ?
+                    user_requested_timestamp=user_requested_timestamps, 
+                    user_requested_timezone=user_requested_timezone
+                )
+            else:
+                from pvgisprototype.cli.print import print_solar_position_table_panels
+                print_solar_position_table_panels(
+                    longitude=None,
+                    latitude=None,
+                    timestamp=timestamps,
+                    timezone=timezone,
+                    table=solar_declination_series,
+                    rounding_places=rounding_places,
+                    timing=True,
+                    declination=True,
+                    hour_angle=True,
+                    zenith=True,
+                    altitude=True,
+                    azimuth=True,
+                    incidence=False,  # Add Me ?
+                    user_requested_timestamp=user_requested_timestamps, 
+                    user_requested_timezone=user_requested_timezone
+                )
+        else:
+            print_solar_position_series_table(
+                longitude=None,
+                latitude=None,
+                timestamps=timestamps,
+                timezone=timezone,
+                table=solar_declination_series,
+                title='Solar Declination Angle',
+                index=index,
+                timing=True,
+                declination=True,
+                hour_angle=False,
+                zenith=False,
+                altitude=False,
+                azimuth=False,
+                surface_orientation=False,
+                surface_tilt=False,
+                incidence=False,
+                user_requested_timestamps=user_requested_timestamps, 
+                user_requested_timezone=user_requested_timezone,
+                rounding_places=rounding_places,
+                group_models=group_models,
+            )
+    if csv:
+        from pvgisprototype.cli.write import write_solar_position_series_csv
+        write_solar_position_series_csv(
+            longitude=None,
+            latitude=None,
+            timestamps=timestamps,
+            timezone=timezone,
+            table=solar_declination_series,
+            timing=True,
+            declination=True,
+            hour_angle=False,
+            zenith=False,
+            altitude=False,
+            azimuth=False,
+            surface_orientation=False,
+            surface_tilt=False,
+            incidence=False,
+            user_requested_timestamps=user_requested_timestamps, 
+            user_requested_timezone=user_requested_timezone,
+            # rounding_places=rounding_places,
+            # group_models=group_models,
+            filename=csv,
+        )
+    if uniplot:
+        from pvgisprototype.api.plot import uniplot_solar_position_series
+        uniplot_solar_position_series(
+            solar_position_series=solar_declination_series,
+            timing=True,
+            timestamps=timestamps,
+            declination=True,
+            # hour_angle=True,
+            # zenith=True,
+            # altitude=True,
+            # azimuth=True,
+            # surface_orientation=True,
+            # surface_tilt=True,
+            # incidence=True,
+            resample_large_series=resample_large_series,
+            lines=True,
+            supertitle='Solar Declination Series',
+            title="Solar Declination",
+            label='Declination',
+            legend_labels=None,
+            terminal_width_fraction=terminal_width_fraction,
+        )
