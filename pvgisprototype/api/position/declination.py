d89533a46057a81b87e909bb98342481ac689a63
Nikos Alexandris <nikosalexandris@duck.com>
2024-05-22 03:27:36 +0300

    refactor!: Update API position/declination.py, add calculate_solar_declination_series()
    Refers to #180.

--- pvgisprototype/api/position/declination.py
+++ pvgisprototype/api/position/declination.py
@@ -1,12 +1,15 @@
+from typing import Dict, List
 from devtools import debug
 from zoneinfo import ZoneInfo
 
 from pandas import DatetimeIndex
 from pvgisprototype import SolarDeclination
+from pvgisprototype.algorithms.pvis.solar_declination import calculate_solar_declination_series_hofierka
+from pvgisprototype.algorithms.pvlib.solar_declination import calculate_solar_declination_series_pvlib
 from pvgisprototype.api.position.models import SolarDeclinationModel
 from pvgisprototype.api.utilities.conversions import convert_series_to_degrees_if_requested
-from pvgisprototype.algorithms.noaa.solar_declination import calculate_solar_declination_time_series_noaa
-from pvgisprototype.constants import HASH_AFTER_THIS_VERBOSITY_LEVEL
+from pvgisprototype.algorithms.noaa.solar_declination import calculate_solar_declination_series_noaa
+from pvgisprototype.constants import ARRAY_BACKEND_DEFAULT, DATA_TYPE_DEFAULT, DEBUG_AFTER_THIS_VERBOSITY_LEVEL, DECLINATION_NAME, HASH_AFTER_THIS_VERBOSITY_LEVEL, LOG_LEVEL_DEFAULT, NOT_AVAILABLE, POSITION_ALGORITHM_NAME, UNITS_NAME, VERBOSE_LEVEL_DEFAULT
 from pvgisprototype.constants import PERIGEE_OFFSET
 from pvgisprototype.constants import ECCENTRICITY_CORRECTION_FACTOR
 from pvgisprototype.constants import RADIANS
@@ -19,37 +22,61 @@ from pvgisprototype.caching import custom_hashkey
 
 @log_function_call
 @cached(cache={}, key=custom_hashkey)
-def model_solar_declination_time_series(
+def model_solar_declination_series(
     timestamps: DatetimeIndex,
     timezone: ZoneInfo = ZoneInfo('UTC'),
-    model: SolarDeclinationModel = SolarDeclinationModel.pvis,
+    solar_declination_model: SolarDeclinationModel = SolarDeclinationModel.pvis,
     perigee_offset: float = PERIGEE_OFFSET,
     eccentricity_correction_factor: float = ECCENTRICITY_CORRECTION_FACTOR,
     angle_output_units: str = RADIANS,
-    verbose: int = 0,
-    log: int = 0,
+    dtype: str = DATA_TYPE_DEFAULT,
+    array_backend: str = ARRAY_BACKEND_DEFAULT,
+    verbose: int = VERBOSE_LEVEL_DEFAULT,
+    log: int = VERBOSE_LEVEL_DEFAULT,
 ) -> SolarDeclination:
     """ """
     solar_declination_series = None
 
-    if model.value == SolarDeclinationModel.noaa:
+    if solar_declination_model.value == SolarDeclinationModel.noaa:
 
-        solar_declination_series = calculate_solar_declination_time_series_noaa(
+        solar_declination_series = calculate_solar_declination_series_noaa(
             timestamps=timestamps,
+            dtype=dtype,
+            array_backend=array_backend,
+            verbose=verbose,
+            log=log,
         )
         solar_declination_series = convert_series_to_degrees_if_requested(
             solar_declination_series,
             angle_output_units,
         )
 
-    if model.value  == SolarDeclinationModel.pvis:
+    if solar_declination_model.value  == SolarDeclinationModel.pvis:
+    # if solar_position_model.value == SolarPositionModel.hofierka:
+
+        solar_declination_series = calculate_solar_declination_series_hofierka(
+            timestamps=timestamps,
+            dtype=dtype,
+            array_backend=array_backend,
+            verbose=verbose,
+            log=log,
+        )
+
+    if solar_declination_model.value  == SolarDeclinationModel.hargreaves:
         pass
 
-    if model.value  == SolarDeclinationModel.hargreaves:
-        pass
+    if solar_declination_model.value  == SolarDeclinationModel.pvlib:
 
-    if model.value  == SolarDeclinationModel.pvlib:
-        pass
+        solar_declination_series = calculate_solar_declination_series_pvlib(
+            timestamps=timestamps,
+            # dtype=dtype,
+            # array_backend=array_backend,
+            # verbose=verbose,
+            # log=log,
+        )
+
+    if verbose > DEBUG_AFTER_THIS_VERBOSITY_LEVEL:
+        debug(locals())
 
     log_data_fingerprint(
         data=solar_declination_series.value,
@@ -60,39 +87,47 @@ def model_solar_declination_time_series(
     return solar_declination_series
 
 
-def calculate_solar_declination_series():
-    pass
+def calculate_solar_declination_series(
+    timestamps: DatetimeIndex,
+    timezone: ZoneInfo,
+    solar_declination_models: List[SolarDeclinationModel] = [SolarDeclinationModel.pvis],
+    perigee_offset: float = PERIGEE_OFFSET,
+    eccentricity_correction_factor: float = ECCENTRICITY_CORRECTION_FACTOR,
+    angle_output_units: str = RADIANS,
+    dtype: str = DATA_TYPE_DEFAULT,
+    array_backend: str = ARRAY_BACKEND_DEFAULT,
+    verbose: int = VERBOSE_LEVEL_DEFAULT,
+    log: int = LOG_LEVEL_DEFAULT,
+) -> Dict:
+    """Calculate the solar declination angle
 
-# def calculate_solar_declination(
-#     timestamp: datetime,
-#     timezone: ZoneInfo = None,
-#     declination_models: List[SolarDeclinationModel] = [SolarDeclinationModel.pvis],
-#     perigee_offset: Annotated[float, typer_option_perigee_offset] = PERIGEE_OFFSET,
-#     eccentricity_correction_factor: Annotated[float, typer_option_eccentricity_correction_factor] = ECCENTRICITY_CORRECTION_FACTOR,
-#     angle_output_units: str = RADIANS,
-#     verbose: Annotated[int, typer_option_verbose] = VERBOSE_LEVEL_DEFAULT,
-# ) -> List:
-#     """Calculate the solar declination angle
+    The solar declination is the angle between the rays of the sun and the
+    equator of the earth. It is used to calculate the solar elevation and
+    azimuth angles.
+    """
+    results = {}
+    for solar_declination_model in solar_declination_models:
+        if solar_declination_model != SolarDeclinationModel.all:  # ignore 'all' in the enumeration
+            solar_declination_series = model_solar_declination_series(
+                timestamps=timestamps,
+                timezone=timezone,
+                solar_declination_model=solar_declination_model,
+                perigee_offset=perigee_offset,
+                eccentricity_correction_factor=eccentricity_correction_factor,
+                array_backend=array_backend,
+                dtype=dtype,
+                verbose=verbose,
+                log=log,
+            )
+            solar_declination_model_series = {  # A less confusing name maybe ?
+                solar_declination_model.name: {
+                    POSITION_ALGORITHM_NAME: solar_declination_model.value,
+                    DECLINATION_NAME: getattr(
+                        solar_declination_series, angle_output_units, NOT_AVAILABLE
+                    ),
+                    UNITS_NAME: angle_output_units,
+                }
+            }
+            results = results | solar_declination_model_series
 
-#     The solar declination is the angle between the rays of the sun and the
-#     equator of the earth. It is used to calculate the solar elevation and
-#     azimuth angles.
-#     """
-#     results = []
-#     for declination_model in declination_models:
-#         if declination_model != SolarDeclinationModel.all:  # ignore 'all' in the enumeration
-#             solar_declination = model_solar_declination(
-#                 timestamp=timestamp,
-#                 timezone=timezone,
-#                 declination_model=declination_model,
-#                 perigee_offset=perigee_offset,
-#                 eccentricity_correction_factor=eccentricity_correction_factor,
-#                 verbose=verbose,
-#             )
-#             results.append({
-#                 POSITION_ALGORITHM_NAME: declination_model.value,
-#                 DECLINATION_NAME if solar_declination else None: getattr(solar_declination, angle_output_units) if solar_declination else None,
-#                 UNITS_NAME: angle_output_units,
-#             })
-
-#     return results
+    return results
