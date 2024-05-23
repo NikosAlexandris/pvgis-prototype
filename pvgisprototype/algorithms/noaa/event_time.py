d4b79bc83678170b437a12e75d2e1b939692b102
Alekos Falagas <alek.falagas@gmail.com>
2024-05-23 15:23:49 +0300

    test: Add tests for event_time.py
    - Update data classes for new custom object EventTime
    - Refactor the function to work properly
    - Add tests and cases for the module
    
    Refers to #157

--- pvgisprototype/algorithms/noaa/event_time.py
+++ pvgisprototype/algorithms/noaa/event_time.py
@@ -1,6 +1,11 @@
 from datetime import time
 from datetime import timedelta
 from typing import Optional, List
+from pandas import DatetimeIndex
+from pandas import NaT
+from zoneinfo import ZoneInfo
+import numpy as np
+
 from pvgisprototype.validation.functions import validate_with_pydantic
 from pvgisprototype.algorithms.noaa.function_models import CalculateEventTimeNOAAInput
 from pvgisprototype.algorithms.noaa.function_models import CalculateEventTimeTimeSeriesNOAAInput
@@ -8,6 +13,11 @@ from pvgisprototype import Longitude
 from pvgisprototype import Latitude
 from datetime import datetime
 from pvgisprototype import RefractedSolarZenith
+from pvgisprototype import EventTime
+from pvgisprototype.constants import REFRACTED_SOLAR_ZENITH_ANGLE_DEFAULT
+from pvgisprototype.constants import ARRAY_BACKEND_DEFAULT
+from pvgisprototype.constants import DATA_TYPE_DEFAULT
+from pvgisprototype import EventTime
 from pvgisprototype.algorithms.noaa.event_hour_angle import calculate_event_hour_angle_noaa
 from pvgisprototype.algorithms.noaa.event_hour_angle import calculate_event_hour_angle_time_series_noaa
 from pvgisprototype.algorithms.noaa.equation_of_time import calculate_equation_of_time_noaa
@@ -84,12 +94,15 @@ def calculate_event_time_noaa(
 def calculate_event_time_time_series_noaa(
     longitude: Longitude,
     latitude: Latitude,
-    timestamps: datetime,
-    # timezone: str,
+    timestamps: DatetimeIndex,
     event: str,
-    refracted_solar_zenith: RefractedSolarZenith,
-    apply_atmospheric_refraction: bool = False,
-)-> List[datetime]:
+    timezone: ZoneInfo,
+    refracted_solar_zenith: RefractedSolarZenith = REFRACTED_SOLAR_ZENITH_ANGLE_DEFAULT,
+    dtype: str = DATA_TYPE_DEFAULT,
+    array_backend: str = ARRAY_BACKEND_DEFAULT,
+    verbose: int = 0,
+    log: int = 0,
+)-> EventTime:
     event_hour_angle = calculate_event_hour_angle_time_series_noaa(
         latitude=latitude,
         timestamps=timestamps,
@@ -98,21 +111,16 @@ def calculate_event_time_time_series_noaa(
     equation_of_time = calculate_equation_of_time_time_series_noaa(
         timestamps=timestamps,
     )
-    # 2 * pi radians equals a circle, 360 degrees or 24 hours
-    # 60 minutes * 24 hours = 1440 minutes (in 24 hours or a day)
-    # The calculation `(1440 / (2 * pi)) * value_in_radians`
-    #   maps a  'value in radians'
-    #   from a range of [0, 2 * pi] which is a full circle
-    #   to a range of [0, 1440] which is a full day in minutes
+
+    timezone_offset_timedelta = timezone.utcoffset(None)
+    timezone_offset_hours_utc = timezone_offset_timedelta.total_seconds() / 3600
+
     event_calculations = {
-        'sunrise': 720 - (longitude.as_minutes + event_hour_angle.as_minutes) - equation_of_time.minutes,
-        'noon': 720 - longitude.as_minutes - equation_of_time.minutes,
-        'sunset': 720 - (longitude.as_minutes - event_hour_angle.as_minutes) - equation_of_time.minutes,
+        'sunrise': 720 - (longitude.as_minutes + event_hour_angle.as_minutes) - equation_of_time.minutes + timezone_offset_hours_utc * 60,
+        'noon': (720 - longitude.as_minutes - equation_of_time.minutes + timezone_offset_hours_utc * 60),
+        'sunset': 720 - (longitude.as_minutes - event_hour_angle.as_minutes) - equation_of_time.minutes + timezone_offset_hours_utc * 60,
     }
     event_time = event_calculations.get(event.lower())
-    # event_datetime = datetime.combine(timestamps.date(), time(0)) + timedelta(minutes=event_time)
-    event_datetimes = [datetime.combine(ts.date(), time(0)) + timedelta(minutes=et) for ts, et in zip(timestamps, event_time)]
+    event_datetimes = [datetime.combine(ts.date(), time(0)) + timedelta(minutes=et) for ts, et in zip(timestamps, event_time.tolist())]
 
-    event_datetime_utc = [attach_requested_timezone(ed) for ed in event_datetimes]  # assign UTC
-
-    return event_datetime_utc
+    return EventTime(value = DatetimeIndex(event_datetimes), event = event)
