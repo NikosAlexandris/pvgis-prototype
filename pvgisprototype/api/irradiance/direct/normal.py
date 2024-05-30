f46547327fe2d03a0a17d4025fcb7380b70695a4
Nikos Alexandris <nikosalexandris@duck.com>
2024-05-30 10:22:02 +0300

    feature: Add calculate_direct_normal_from_horizontal_irradiance_series()
    In API irradiance direct normal and get_direct_normal_from_horizontal_irradiance_series() in the respective CLI module too.

--- pvgisprototype/api/irradiance/direct/normal.py
+++ pvgisprototype/api/irradiance/direct/normal.py
@@ -10,6 +10,8 @@ During a cloudy day the sunlight will be partially absorbed and scattered by
 different air molecules. The latter part is defined as the _diffuse_
 irradiance. The remaining part is the _direct_ irradiance.
 """
+
+from pathlib import Path
 from pvgisprototype.log import logger
 from pvgisprototype.log import log_function_call
 from pvgisprototype.log import log_data_fingerprint
@@ -22,6 +24,8 @@ from pvgisprototype import RayleighThickness
 from pvgisprototype import LinkeTurbidityFactor
 from pvgisprototype import Elevation
 from pvgisprototype import Irradiance
+from pvgisprototype import Longitude
+from pvgisprototype import Latitude
 from pvgisprototype.validation.functions import validate_with_pydantic
 from pvgisprototype.validation.functions import AdjustElevationInputModel
 from pvgisprototype.validation.functions import CalculateOpticalAirMassTimeSeriesInputModel
@@ -32,13 +36,14 @@ from pvgisprototype.api.position.incidence import model_solar_incidence_series
 from pvgisprototype.api.irradiance.models import DirectIrradianceComponents
 from pvgisprototype.api.irradiance.models import MethodForInexactMatches
 from pvgisprototype.api.irradiance.shade import is_surface_in_shade_series
+from pvgisprototype.api.utilities.conversions import convert_float_to_radians_if_requested
 from pvgisprototype.api.irradiance.direct.linke_turbidity_factor import correct_linke_turbidity_factor_series
 from pvgisprototype.api.irradiance.direct.rayleigh_optical_thickness import calculate_rayleigh_optical_thickness_series
 from pvgisprototype.api.irradiance.extraterrestrial import calculate_extraterrestrial_normal_irradiance_series
 from pvgisprototype.api.irradiance.limits import LOWER_PHYSICALLY_POSSIBLE_LIMIT
 from pvgisprototype.api.irradiance.limits import UPPER_PHYSICALLY_POSSIBLE_LIMIT
 from pvgisprototype.cli.messages import WARNING_OUT_OF_RANGE_VALUES
-from pvgisprototype.constants import FINGERPRINT_COLUMN_NAME
+from pvgisprototype.constants import FINGERPRINT_COLUMN_NAME, IN_MEMORY_FLAG_DEFAULT, MASK_AND_SCALE_FLAG_DEFAULT, NEIGHBOR_LOOKUP_DEFAULT
 from pvgisprototype.constants import TITLE_KEY_NAME
 from pvgisprototype.constants import DATA_TYPE_DEFAULT
 from pvgisprototype.constants import ARRAY_BACKEND_DEFAULT
@@ -80,6 +85,17 @@ from pvgisprototype.constants import RANDOM_TIMESTAMPS_FLAG_DEFAULT
 from pvgisprototype.constants import ATMOSPHERIC_REFRACTION_FLAG_DEFAULT
 from pvgisprototype.constants import LOG_LEVEL_DEFAULT
 from pvgisprototype.constants import INDEX_IN_TABLE_OUTPUT_FLAG_DEFAULT
+from pvgisprototype.constants import DIRECT_HORIZONTAL_IRRADIANCE_COLUMN_NAME
+from pvgisprototype.constants import ALTITUDE_COLUMN_NAME
+from pvgisprototype.constants import POSITION_ALGORITHM_COLUMN_NAME
+from pvgisprototype.constants import TIME_ALGORITHM_COLUMN_NAME
+from pvgisprototype.api.position.models import SOLAR_TIME_ALGORITHM_DEFAULT
+from pvgisprototype.api.position.models import SOLAR_POSITION_ALGORITHM_DEFAULT
+from pvgisprototype.constants import REFRACTED_SOLAR_ZENITH_ANGLE_DEFAULT
+from pvgisprototype.api.position.altitude import model_solar_altitude_series
+from typing import Optional
+from zoneinfo import ZoneInfo
+from pvgisprototype.api.series.select import select_time_series
 
 
 @log_function_call
@@ -96,7 +112,7 @@ def calculate_direct_normal_irradiance_series(
     verbose: int = VERBOSE_LEVEL_DEFAULT,
     log: int = 0,
     fingerprint: bool = False,
-) -> np.array:
+) -> Irradiance:
     """Calculate the direct normal irradiance.
 
     The direct normal irradiance represents the amount of solar radiation
@@ -195,6 +211,167 @@ def calculate_direct_normal_irradiance_series(
             SOLAR_CONSTANT_COLUMN_NAME: solar_constant,
             PERIGEE_OFFSET_COLUMN_NAME: perigee_offset,
             ECCENTRICITY_CORRECTION_FACTOR_COLUMN_NAME: eccentricity_correction_factor,
+        } if verbose > 3 else {},
+
+        'fingerprint': lambda: {
+            FINGERPRINT_COLUMN_NAME: generate_hash(direct_normal_irradiance_series),
+        } if fingerprint else {},
+    }
+
+    components = {}
+    for key, component in components_container.items():
+        components.update(component())
+
+    if verbose > DEBUG_AFTER_THIS_VERBOSITY_LEVEL:
+        debug(locals())
+
+    log_data_fingerprint(
+        data=direct_normal_irradiance_series,
+        log_level=log,
+        hash_after_this_verbosity_level=HASH_AFTER_THIS_VERBOSITY_LEVEL,
+    )
+
+    return Irradiance(
+            value=direct_normal_irradiance_series,
+            unit=IRRADIANCE_UNITS,
+            position_algorithm="",
+            timing_algorithm="",
+            elevation=None,
+            surface_orientation=None,
+            surface_tilt=None,
+            components=components,
+            )
+
+
+@log_function_call
+@cached(cache={}, key=custom_hashkey)
+def calculate_direct_normal_from_horizontal_irradiance_series(
+    direct: Path,
+    longitude: Longitude,
+    latitude: Latitude,
+    timestamps: DatetimeIndex = None,
+    timezone: Optional[ZoneInfo] = None,
+    neighbor_lookup: MethodForInexactMatches = NEIGHBOR_LOOKUP_DEFAULT,
+    tolerance: Optional[float] = TOLERANCE_DEFAULT,
+    mask_and_scale: bool = MASK_AND_SCALE_FLAG_DEFAULT,
+    in_memory: bool = IN_MEMORY_FLAG_DEFAULT,
+    solar_time_model: SolarTimeModel = SOLAR_TIME_ALGORITHM_DEFAULT,
+    solar_position_model: SolarPositionModel = SOLAR_POSITION_ALGORITHM_DEFAULT,
+    apply_atmospheric_refraction: Optional[bool] = ATMOSPHERIC_REFRACTION_FLAG_DEFAULT,
+    # refracted_solar_zenith: Optional[float] = REFRACTED_SOLAR_ZENITH_ANGLE_DEFAULT,
+    perigee_offset: float = PERIGEE_OFFSET,
+    eccentricity_correction_factor: float = ECCENTRICITY_CORRECTION_FACTOR,
+    angle_output_units: str = RADIANS,
+    dtype: str = DATA_TYPE_DEFAULT,
+    array_backend: str = ARRAY_BACKEND_DEFAULT,
+    verbose: int = VERBOSE_LEVEL_DEFAULT,
+    log: int = 0,
+    fingerprint: bool = False,
+) -> Irradiance:
+    """Calculate the direct normal from the horizontal irradiance.
+
+    The direct normal irradiance represents the amount of solar radiation
+    received per unit area by a surface that is perpendicular (normal) to the
+    rays that come in a straight line from the direction of the sun at its
+    current position in the sky.
+
+    This function calculates the normal irradiance from the given horizontal
+    irradiance component.
+
+    Notes
+    -----
+    Known also as : SID, units : W*m-2
+
+    References
+    ----------
+    .. [1] Hofierka, J. (2002). Some title of the paper. Journal Name, vol(issue), pages.
+
+    """
+    direct_horizontal_irradiance_series = select_time_series(
+        time_series=direct,
+        longitude=longitude,
+        latitude=latitude,
+        timestamps=timestamps,
+        neighbor_lookup=neighbor_lookup,
+        tolerance=tolerance,
+        mask_and_scale=mask_and_scale,
+        in_memory=in_memory,
+        log=log,
+    ).to_numpy().astype(dtype=dtype)
+    solar_altitude_series = model_solar_altitude_series(
+        longitude=convert_float_to_radians_if_requested(longitude, RADIANS),
+        latitude=convert_float_to_radians_if_requested(latitude, RADIANS),
+        timestamps=timestamps,
+        timezone=timezone,
+        solar_position_model=solar_position_model,
+        apply_atmospheric_refraction=apply_atmospheric_refraction,
+        # refracted_solar_zenith=refracted_solar_zenith,
+        # solar_time_model=solar_time_model,
+        perigee_offset=perigee_offset,
+        eccentricity_correction_factor=eccentricity_correction_factor,
+        dtype=dtype,
+        array_backend=array_backend,
+        verbose=verbose,
+    )
+    mask_solar_altitude_positive = solar_altitude_series.radians > 0
+    mask_not_in_shade = np.full_like(solar_altitude_series.radians, True)  # Stub, replace with actual condition
+    mask = np.logical_and.reduce((mask_solar_altitude_positive, mask_not_in_shade))
+
+    direct_normal_irradiance_series = np.zeros_like(solar_altitude_series.radians)
+    if np.any(mask):
+        direct_normal_irradiance_series[mask] = (
+            direct_horizontal_irradiance_series / np.sin(solar_altitude_series.radians)
+        )[mask]
+
+    # Warning
+    if (
+        (direct_normal_irradiance_series < LOWER_PHYSICALLY_POSSIBLE_LIMIT)
+        | (direct_normal_irradiance_series > UPPER_PHYSICALLY_POSSIBLE_LIMIT)
+    ).any():
+        out_of_range_values = direct_normal_irradiance_series[
+            (direct_normal_irradiance_series < LOWER_PHYSICALLY_POSSIBLE_LIMIT)
+            | (direct_normal_irradiance_series > UPPER_PHYSICALLY_POSSIBLE_LIMIT)
+        ]
+        warning_unstyled = (
+                f"\n"
+                f"{WARNING_OUT_OF_RANGE_VALUES} "
+                f"[{LOWER_PHYSICALLY_POSSIBLE_LIMIT}, {UPPER_PHYSICALLY_POSSIBLE_LIMIT}]"
+                f" in direct_normal_irradiance_series : "
+                f"{out_of_range_values}"
+                f"\n"
+                )
+        warning = (
+                f"\n"
+                f"{WARNING_OUT_OF_RANGE_VALUES} "
+                f"[{LOWER_PHYSICALLY_POSSIBLE_LIMIT}, {UPPER_PHYSICALLY_POSSIBLE_LIMIT}]"
+                f" in [code]direct_normal_irradiance_series[/code] : "
+                f"{out_of_range_values}"
+                f"\n"
+                )
+        logger.warning(warning_unstyled, alt=warning)
+
+    # Building the output dictionary=========================================
+
+    components_container = {
+        'main': lambda: {
+            TITLE_KEY_NAME: DIRECT_NORMAL_IRRADIANCE,
+            DIRECT_NORMAL_IRRADIANCE_COLUMN_NAME: direct_normal_irradiance_series,
+            RADIATION_MODEL_COLUMN_NAME: HOFIERKA_2002,
+        },
+
+        'extended': lambda: {
+            TITLE_KEY_NAME: DIRECT_NORMAL_IRRADIANCE + ' & horizontal component',
+            DIRECT_HORIZONTAL_IRRADIANCE_COLUMN_NAME: direct_horizontal_irradiance_series,
+        } if verbose > 1 else {},
+
+        'more_extended': lambda: {
+            ALTITUDE_COLUMN_NAME: getattr(solar_altitude_series, angle_output_units),
+            ANGLE_UNITS_COLUMN_NAME: angle_output_units,
+        } if verbose > 2 else {},
+
+        'even_more_extended': lambda: {
+            POSITION_ALGORITHM_COLUMN_NAME: solar_position_model.value,
+            TIME_ALGORITHM_COLUMN_NAME: solar_time_model.value,
         } if verbose > 3 else {},
 
         'fingerprint': lambda: {
