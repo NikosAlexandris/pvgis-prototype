97a097db53e0b71ae886b62bc094899198e04ae2
Nikos Alexandris <nikosalexandris@duck.com>
2024-06-07 12:35:30 +0300

    refactor!: Enable performance analysis via the power/broadband CLI
    Add analysis (and nomenclature, yet to use!) and restructure the output section.
    This implies the definition of Typer relevant bits for analysis and nomenclature.
    Important is the use of a callback function for the verbose option : As it stands, the analysis of photovoltaic performance is only possible along with verbose = 7 ! Hence, the new callback function callback_analysis_of_performance() does this : if analysis is requested, it sets verbose to 7.
    
    # Conflicts:
    #	pvgisprototype/cli/power/broadband.py

--- pvgisprototype/cli/typer/statistics.py
+++ pvgisprototype/cli/typer/statistics.py
@@ -2,6 +2,7 @@
 Statistics related options
 """
 
+from re import VERBOSE
 import typer
 from pvgisprototype.cli.rich_help_panel_names import rich_help_panel_statistics
 
@@ -12,5 +13,13 @@ typer_option_statistics = typer.Option(
 )
 typer_option_groupby = typer.Option(
     help=f"Group statistics, ex. M or 3H. A number and date/time unit : (Y)ear, (M)onth, (D)ay, (W)eek, (S)eason. See Xarray\'s group-by operations.",
+    rich_help_panel=rich_help_panel_statistics,
+)
+typer_option_analysis = typer.Option(
+    help='Analysis of performance',
+    rich_help_panel=rich_help_panel_statistics,
+)
+typer_option_nomenclature = typer.Option(
+    help='Nomenclature',
     rich_help_panel=rich_help_panel_statistics,
 )
