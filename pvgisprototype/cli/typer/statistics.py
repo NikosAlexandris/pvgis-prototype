2d2e05c729f12795b480cd1731d0bd98650c58d5
Nikos Alexandris <nikosalexandris@duck.com>
2024-06-12 08:09:45 +0300

    refactor!: Set angle output units to degrees if QR or analysis requested
    See callback_angle_output_units().
    Also :
    - update help text for the analysis option.
    - use h instead of H for the hour/s unit in print_change_percentages_panel() and update help text for the groupby option.

--- pvgisprototype/cli/typer/statistics.py
+++ pvgisprototype/cli/typer/statistics.py
@@ -2,7 +2,6 @@
 Statistics related options
 """
 
-from re import VERBOSE
 import typer
 from pvgisprototype.cli.rich_help_panel_names import rich_help_panel_statistics
 
@@ -12,11 +11,11 @@ typer_option_statistics = typer.Option(
     rich_help_panel=rich_help_panel_statistics,
 )
 typer_option_groupby = typer.Option(
-    help=f"Group statistics, ex. M or 3H. A number and date/time unit : (Y)ear, (M)onth, (D)ay, (W)eek, (S)eason. See Xarray\'s group-by operations.",
+    help=f"Group statistics, ex. M or 3h. A number and date/time unit : (Y)ear, (S)eason, (M)onth, (D)ay, (W)eek, (h)our. See Xarray\'s group-by operations.",
     rich_help_panel=rich_help_panel_statistics,
 )
 typer_option_analysis = typer.Option(
-    help='Analysis of performance',
+        help='Analysis of performance. Will force verbose=9 (for detailed calculations) and quiet=True.',
     rich_help_panel=rich_help_panel_statistics,
 )
 typer_option_nomenclature = typer.Option(
