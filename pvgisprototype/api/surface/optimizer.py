34cbb4832efc045be7407188fd02fa8e76db7b19
Nikos Alexandris <nikosalexandris@duck.com>
2024-06-26 15:08:00 +0300

    refactor!: Read external time series for the surface position optimizer
    Refers to #97.
    
    Kudos to Alba Santos Rodriguez :-)

--- pvgisprototype/api/surface/optimizer.py
+++ pvgisprototype/api/surface/optimizer.py
@@ -1,15 +1,37 @@
-from pvgisprototype import SurfaceTilt
+from pathlib import Path
+from pvgisprototype import (
+    TemperatureSeries,
+    WindSpeedSeries,
+    SpectralFactorSeries,
+    LinkeTurbidityFactor,
+    SurfaceTilt,
+)
 from scipy import optimize
+from pvgisprototype.api.power.photovoltaic_module import PhotovoltaicModuleModel
+from pvgisprototype.api.series.models import MethodForInexactMatches
 from pvgisprototype.api.surface.parameter_models import (
     SurfacePositionOptimizerMethod,
     SurfacePositionOptimizerMode,
 )
-from typing import Callable
+from typing import Callable, Optional
+
+from pvgisprototype.constants import IN_MEMORY_FLAG_DEFAULT, LINKE_TURBIDITY_TIME_SERIES_DEFAULT, MASK_AND_SCALE_FLAG_DEFAULT, NEIGHBOR_LOOKUP_DEFAULT, SPECTRAL_FACTOR_DEFAULT, TEMPERATURE_DEFAULT, TOLERANCE_DEFAULT, WIND_SPEED_DEFAULT
 
 
 def optimizer(
     location_parameters: dict,
     func: Callable,
+    global_horizontal_irradiance: Optional[Path] = None,
+    direct_horizontal_irradiance: Optional[Path] = None,
+    spectral_factor_series: SpectralFactorSeries = SpectralFactorSeries(value=SPECTRAL_FACTOR_DEFAULT),
+    temperature_series: TemperatureSeries = TemperatureSeries(value=TEMPERATURE_DEFAULT),
+    wind_speed_series: WindSpeedSeries = WindSpeedSeries(value=WIND_SPEED_DEFAULT),
+    neighbor_lookup: MethodForInexactMatches = MethodForInexactMatches.nearest,
+    tolerance: Optional[float] = TOLERANCE_DEFAULT,
+    mask_and_scale: bool = MASK_AND_SCALE_FLAG_DEFAULT,
+    in_memory: bool = IN_MEMORY_FLAG_DEFAULT,
+    photovoltaic_module: PhotovoltaicModuleModel = PhotovoltaicModuleModel.CSI_FREE_STANDING, 
+    linke_turbidity_factor_series: LinkeTurbidityFactor = LinkeTurbidityFactor(value = LINKE_TURBIDITY_TIME_SERIES_DEFAULT),
     method: SurfacePositionOptimizerMethod = SurfacePositionOptimizerMethod.shgo,
     iterations: int = 100,
     mode: SurfacePositionOptimizerMode = SurfacePositionOptimizerMode.Tilt,
@@ -24,7 +46,21 @@ def optimizer(
             func=func,
             bounds=bounds,
             n=iterations,
-            args=(location_parameters, mode),
+            args=(
+                location_parameters,
+                global_horizontal_irradiance,
+                direct_horizontal_irradiance,
+                spectral_factor_series,
+                temperature_series,
+                wind_speed_series,
+                neighbor_lookup,
+                tolerance,
+                mask_and_scale,
+                in_memory,
+                linke_turbidity_factor_series,
+                photovoltaic_module,
+                mode,
+                ),
             sampling_method=sampling_method_shgo,
             workers=workers,
             options={"disp": True},
@@ -34,7 +70,21 @@ def optimizer(
         result = optimize.brute(
             func=func,
             ranges=bounds,
-            args=(location_parameters, mode),
+            args=(
+                location_parameters,
+                global_horizontal_irradiance,
+                direct_horizontal_irradiance,
+                spectral_factor_series,
+                temperature_series,
+                wind_speed_series,
+                neighbor_lookup,
+                tolerance,
+                mask_and_scale,
+                in_memory,
+                linke_turbidity_factor_series,
+                photovoltaic_module,
+                mode,
+                ),
             finish=None,
             workers=workers,
         )
