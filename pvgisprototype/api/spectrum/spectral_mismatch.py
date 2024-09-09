9f5ddb429aa63cf6642c06fd61cb6aaa0cf08b13
Nikos Alexandris <nikosalexandris@duck.com>
2024-09-09 09:30:54 +0300

    feature(api): Add API function for spectral mismatch factor
    Add :
    - model_spectral_mismatch()
    - calculate_spectral_mismatch()
    
    Remember to unsin : convert fully to NumPy/Xarray.-
    
    Refers to #213.

---
+++ pvgisprototype/api/spectrum/spectral_mismatch.py
@@ -0,0 +1,217 @@
+from pvgisprototype.algorithms.pelland.spectral_mismatch import calculate_spectral_mismatch_pelland
+from pvgisprototype.log import log_function_call, log_data_fingerprint
+from devtools import debug
+from typing import Dict, List
+from zoneinfo import ZoneInfo
+from pandas import DataFrame, DatetimeIndex, Series
+from pvlib.spectrum import calc_spectral_mismatch_field
+
+from pvgisprototype import SpectralFactorSeries
+from pvgisprototype.algorithms.pvis.spectral_responsivity import SPECTRAL_RESPONSIVITY_DEFAULT
+from pvgisprototype.api.position.models import select_models
+from pvgisprototype.api.spectrum.models import PhotovoltaicModuleSpectralResponsivityModel, SpectralMismatchModel
+from pvgisprototype.api.spectrum.constants import (
+    MAX_WAVELENGTH,
+    MIN_WAVELENGTH,
+)
+from pvgisprototype.constants import (
+        UNIT_NAME,
+    DEBUG_AFTER_THIS_VERBOSITY_LEVEL,
+    IRRADIANCE_COLUMN_NAME,
+    IRRADIANCE_SOURCE_COLUMN_NAME,
+    LOG_LEVEL_DEFAULT,
+    VERBOSE_LEVEL_DEFAULT,
+    UNITLESS,
+    TITLE_KEY_NAME,
+    SPECTRAL_FACTOR_NAME,
+    SPECTRAL_FACTOR_COLUMN_NAME,
+    TECHNOLOGY_NAME,
+    SPECTRAL_MISMATCH_MODEL_COLUMN_NAME,
+    NOT_AVAILABLE,
+    FINGERPRINT_FLAG_DEFAULT,
+    FINGERPRINT_COLUMN_NAME,
+    REFERENCE_SPECTRUM_COLUMN_NAME,
+    RESPONSIVITY_COLUMN_NAME,
+    HASH_AFTER_THIS_VERBOSITY_LEVEL,
+)
+from pvgisprototype.validation.hashing import generate_hash
+
+
+@log_function_call
+def model_spectral_mismatch(
+    # longitude: Longitude,
+    # latitude: Latitude,
+    # elevation: Elevation,
+    timestamps: DatetimeIndex,
+    timezone: ZoneInfo,
+    responsivity: Series,
+    irradiance: DataFrame,
+    # neighbor_lookup: Annotated[
+    #     MethodForInexactMatches, typer_option_nearest_neighbor_lookup
+    # ] = NEIGHBOR_LOOKUP_DEFAULT,
+    # tolerance: Annotated[Optional[float], typer_option_tolerance] = TOLERANCE_DEFAULT,
+    # mask_and_scale: Annotated[
+    #     bool, typer_option_mask_and_scale
+    # ] = MASK_AND_SCALE_FLAG_DEFAULT,
+    # in_memory: Annotated[bool, typer_option_in_memory] = IN_MEMORY_FLAG_DEFAULT,
+    min_wavelength: float = MIN_WAVELENGTH,
+    max_wavelength: float = MAX_WAVELENGTH,
+    reference_spectrum: None | Series = None,  # AM15G_IEC60904_3_ED4,
+    integrate_reference_spectrum: bool = False,
+    spectral_mismatch_model: SpectralMismatchModel = SpectralMismatchModel.pvlib,
+    # dtype: Annotated[str, typer_option_dtype] = DATA_TYPE_DEFAULT,
+    # array_backend: Annotated[str, typer_option_array_backend] = ARRAY_BACKEND_DEFAULT,
+    # multi_thread: Annotated[
+    #     bool, typer_option_multi_thread
+    # ] = MULTI_THREAD_FLAG_DEFAULT,
+    resample_large_series: bool = False,
+    # dtype: str = DATA_TYPE_DEFAULT,
+    # array_backend: str = ARRAY_BACKEND_DEFAULT,
+    # multi_thread=multi_thread,
+    verbose: int = VERBOSE_LEVEL_DEFAULT,
+    log: int = LOG_LEVEL_DEFAULT,
+    fingerprint: bool = FINGERPRINT_FLAG_DEFAULT,
+):
+    """
+    """
+    spectral_mismatch = None
+
+    if spectral_mismatch_model.value == SpectralMismatchModel.pvlib:
+
+        spectral_mismatch = calc_spectral_mismatch_field(
+            sr=responsivity.T,  # for one PV module technology
+            e_sun=irradiance,
+            e_ref=reference_spectrum,
+        ).to_numpy()  # Very Important !
+
+    if spectral_mismatch_model.value == SpectralMismatchModel.pelland:
+
+        spectral_mismatch = calculate_spectral_mismatch_pelland(
+            irradiance=irradiance,
+            responsivity=responsivity.T,
+            reference_spectrum=reference_spectrum,
+        )
+
+    if spectral_mismatch_model.value == SpectralMismatchModel.mihaylov:
+
+        pass
+
+    return spectral_mismatch
+
+
+def calculate_spectral_mismatch(
+    # longitude: Longitude,
+    # latitude: Latitude,
+    # elevation: Elevation,
+    timestamps: DatetimeIndex,
+    timezone: ZoneInfo,
+    # responsivity: Series,
+    responsivity: Dict[str, Series],  # Dictionary to hold responsivity for each type
+    photovoltaic_module_type: List[PhotovoltaicModuleSpectralResponsivityModel],
+    irradiance: DataFrame,
+    # neighbor_lookup: Annotated[
+    #     MethodForInexactMatches, typer_option_nearest_neighbor_lookup
+    # ] = NEIGHBOR_LOOKUP_DEFAULT,
+    # tolerance: Annotated[Optional[float], typer_option_tolerance] = TOLERANCE_DEFAULT,
+    # mask_and_scale: Annotated[
+    #     bool, typer_option_mask_and_scale
+    # ] = MASK_AND_SCALE_FLAG_DEFAULT,
+    # in_memory: Annotated[bool, typer_option_in_memory] = IN_MEMORY_FLAG_DEFAULT,
+    reference_spectrum: Series,  # AM15G_IEC60904_3_ED4,
+    integrate_reference_spectrum: bool = False,
+    spectral_mismatch_models: List[SpectralMismatchModel] = [SpectralMismatchModel.pvlib],
+    min_wavelength: float = MIN_WAVELENGTH,
+    max_wavelength: float = MAX_WAVELENGTH,
+    # dtype: Annotated[str, typer_option_dtype] = DATA_TYPE_DEFAULT,
+    # array_backend: Annotated[str, typer_option_array_backend] = ARRAY_BACKEND_DEFAULT,
+    # multi_thread: Annotated[
+    #     bool, typer_option_multi_thread
+    # ] = MULTI_THREAD_FLAG_DEFAULT,
+    resample_large_series: bool = False,
+    # dtype: str = DATA_TYPE_DEFAULT,
+    # array_backend: str = ARRAY_BACKEND_DEFAULT,
+    # multi_thread=multi_thread,
+    verbose: int = VERBOSE_LEVEL_DEFAULT,
+    log: int = LOG_LEVEL_DEFAULT,
+    fingerprint: bool = FINGERPRINT_FLAG_DEFAULT,
+) -> Dict:
+    """Calculate an overview of solar position parameters for a time series.
+    """
+    results = {}
+    # ctx.params.get() does not return a list !
+    photovoltaic_module_types = select_models(
+        PhotovoltaicModuleSpectralResponsivityModel, photovoltaic_module_type
+        )# Using a callback fails!
+    # print('Here I Am!')
+    for spectral_mismatch_model in spectral_mismatch_models:
+        if (
+            spectral_mismatch_model != SpectralMismatchModel.all
+        ):  # ignore 'all' in the enumeration
+
+            model_results = {}  # To store results for the current mismatch model
+
+            for module_type in photovoltaic_module_types:
+                spectral_mismatch_series = model_spectral_mismatch(
+                    # longitude=longitude,
+                    # latitude=latitude,
+                    timestamps=timestamps,
+                    timezone=timezone,
+                    spectral_mismatch_model=spectral_mismatch_model,
+                    responsivity=responsivity[module_type.value],
+                    # responsivity=responsivity,
+                    irradiance=irradiance,
+                    reference_spectrum=reference_spectrum,
+                    # dtype=dtype,
+                    # array_backend=array_backend,
+                    verbose=verbose,
+                    log=log,
+                )
+                components_container = {
+                    "Metadata": lambda: {
+                        RESPONSIVITY_COLUMN_NAME: responsivity[module_type.value],
+                        IRRADIANCE_COLUMN_NAME: irradiance,
+                        IRRADIANCE_SOURCE_COLUMN_NAME: 'UpdateMe',
+                        REFERENCE_SPECTRUM_COLUMN_NAME: reference_spectrum,
+                    }
+                    if verbose > 2
+                    else {},
+                    "Mismatch": lambda: {
+                        TITLE_KEY_NAME: SPECTRAL_FACTOR_NAME,
+                        SPECTRAL_FACTOR_COLUMN_NAME: spectral_mismatch_series,
+                        TECHNOLOGY_NAME: module_type.name,
+                        SPECTRAL_MISMATCH_MODEL_COLUMN_NAME: (
+                            spectral_mismatch_model
+                            if spectral_mismatch_model
+                            else NOT_AVAILABLE
+                            ),
+                        UNIT_NAME: UNITLESS,
+                    },  # if verbose > 0 else {},
+                    "fingerprint": lambda: {
+                        FINGERPRINT_COLUMN_NAME: generate_hash(spectral_mismatch_series),
+                    }
+                    if fingerprint
+                    else {},
+                }
+
+                components = {}
+                for key, component in components_container.items():
+                    components.update(component())
+
+                model_results[module_type] = components
+
+                # Add the results for the current mismatch model to the final results
+                results[spectral_mismatch_model] = model_results
+                # results = results | spectral_mismatch_model_overview
+    
+    if verbose > DEBUG_AFTER_THIS_VERBOSITY_LEVEL:
+        debug(locals())
+
+    log_data_fingerprint(
+        data=spectral_mismatch_series,
+        log_level=log,
+        hash_after_this_verbosity_level=HASH_AFTER_THIS_VERBOSITY_LEVEL,
+    )
+    return SpectralFactorSeries(
+            value=spectral_mismatch_series,
+            components=results,
+            )
