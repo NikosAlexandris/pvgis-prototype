d933638af5c859296fe34443958ee034b8fa0891
falanal <Alexandros.FALANGAS@ext.ec.europa.eu>
2024-09-12 17:23:32 +0200

    perf: Add middleware for timing endpoints
    - New middleware function for timing the endpoints. Prints the results using the logger of uvicorn
    
    Refers to #72

--- pvgisprototype/webapi.py
+++ pvgisprototype/webapi.py
@@ -20,6 +20,7 @@ from pvgisprototype.web_api.power.broadband import (
     get_photovoltaic_power_series_advanced,
 )
 from pvgisprototype.web_api.surface.optimise import get_optimised_surface_position
+from pvgisprototype.web_api.utilities import response_time
 
 current_file = Path(__file__).resolve()
 assets_directory = current_file.parent / "web_api/assets"
@@ -288,11 +289,13 @@ app.get("/calculate/power/broadband-advanced", tags=["Power"])(
 app.get("/calculate/power/broadband-multi", tags=["Power"])(
     get_photovoltaic_power_output_series_multi
 )
-
-
 app.get("/surface/optimise-surface-position", tags=["Power"])(
     get_optimised_surface_position
 )
+app.middleware("http")(
+    response_time
+)
+
 app.openapi = customise_openapi(app) # type: ignore
 
 
