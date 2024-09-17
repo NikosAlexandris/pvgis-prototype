5dd98c7d0c129e3c704ec52eb78a810f622decad
Nikos Alexandris <nikosalexandris@duck.com>
2024-09-17 19:12:27 +0300

    refactor(cli): Refactor timezone relevant bits in overview.py, print.py
    Refers to #163

--- pvgisprototype/cli/print.py
+++ pvgisprototype/cli/print.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from typing import List, Sequence
+from zoneinfo import ZoneInfo, ZoneInfoNotFoundError
 
 import numpy as np
 import pandas
@@ -171,8 +172,8 @@ def build_caption(
         # f"[underline]Definitions[/underline]  "
         # f"Azimuth origin: {rounded_table[first_model].get(AZIMUTH_ORIGIN_NAME, NOT_AVAILABLE)}, "
         # f"Incidence angle: {rounded_table[first_model].get(INCIDENCE_DEFINITION, NOT_AVAILABLE)}\n"
-    
-    if user_requested_timezone != timezone and user_requested_timezone is not None:
+
+    if user_requested_timezone != ZoneInfo('UTC'):
         caption += f"Local Zone : {user_requested_timezone}, "
     else:
         caption += f"Zone : {timezone}, "
@@ -331,11 +332,11 @@ def print_solar_position_series_table(
         columns = []
         if index:
             columns.append("Index")
-        if user_requested_timestamps is not None:
-            time_column_name = LOCAL_TIME_COLUMN_NAME
-        else:
-            time_column_name = TIME_COLUMN_NAME
         if timestamps is not None:
+            if user_requested_timezone != ZoneInfo('UTC'):
+                time_column_name = LOCAL_TIME_COLUMN_NAME
+            else:
+                time_column_name = TIME_COLUMN_NAME
             columns.append(time_column_name)
 
         for parameter in position_parameters:
