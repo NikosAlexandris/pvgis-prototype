0055964648cb0a451bc3e3a4bb70f1951f49e553
Nikos Alexandris <nikosalexandris@duck.com>
2024-09-17 12:04:29 +0300

    refactor(algorithms): Refactor calculate_spectral_mismatch_pelland()
    Reworking on it, debugging, etc. with the goal to convert it (slowly) to an Xarray-based process!
    
    Refers to #213.

--- pvgisprototype/algorithms/pelland/spectral_mismatch.py
+++ pvgisprototype/algorithms/pelland/spectral_mismatch.py
@@ -1,4 +1,6 @@
 from pandas import DataFrame
+from xarray import DataArray
+from pvgisprototype.log import log_function_call, logger
 
 
 # def calculate_spectral_mismatch(
@@ -27,8 +29,9 @@ from pandas import DataFrame
 #         return uf_irradiance / uf_reference
 
 
+@log_function_call
 def calculate_spectral_mismatch_pelland(
-    irradiance: DataFrame,
+    irradiance: DataArray,
     responsivity: DataFrame,
     reference_spectrum: DataFrame,
 ) -> DataFrame:
@@ -41,10 +44,12 @@ def calculate_spectral_mismatch_pelland(
 
     """
     # Verify the reference spectrum is valid (no zero or near-zero values) ?
-    if (reference_spectrum <= 0).any().any():
-        raise ValueError(
-            "The reference spectrum contains zero or negative values, which are invalid."
-        )
+    print(f'Reference spectrum : {type(reference_spectrum)}')
+    print(f'Reference spectrum : {reference_spectrum}')
+    # if (reference_spectrum <= 0).any():
+    #     raise ValueError(
+    #         "The reference spectrum contains zero or negative values, which are invalid."
+    #     )
     # else:
     #     print(f'Reference spectrum input : {reference_spectrum}')
 
@@ -55,42 +60,77 @@ def calculate_spectral_mismatch_pelland(
     #     reference_spectrum.columns
     # )
     import numpy
-    wavelengths = numpy.intersect1d(
-        irradiance.center_wavelength.values, 
-        responsivity.index.intersection(reference_spectrum.columns)
-    )
+    logger.info(
+            f'Wavelengths in `irradiance` input : {irradiance.center_wavelength.values}',
+            )
+    
+    # Push me upstream !
+    if 'Center [nm]' in responsivity.coords:
+        responsivity = responsivity.rename({'Center [nm]': 'center_wavelength'})
+    else:
+        responsivity = responsivity.rename({'Wavelength': 'center_wavelength'})
+    # ------------------------------------------------------------------------
 
-    # Required ?
-    irradiance = irradiance.where(irradiance > -999, numpy.nan)
+    logger.info(
+            f'Wavelengths in `responsivity` input : {responsivity}',
+            )
+    logger.info(
+            f'Wavelengths in `reference_spectrum` input : {reference_spectrum.columns}'
+            )
+    common_wavelengths = numpy.intersect1d(
+        irradiance.center_wavelength.values,
+        numpy.intersect1d(responsivity.center_wavelength, reference_spectrum.columns),
+    )
+    logger.info(
+            f"Intersection of wavelengths across input data : {common_wavelengths}"
+            )
 
     # useful reference spectrum (average over the reference spectrum)
-    # useful_reference = (
-    #     responsivity.loc[wavelengths]
-    #     .mul(reference_spectrum.loc["global", wavelengths])
-    #     .sum()
-    #     / reference_spectrum.loc["global", wavelengths].sum()
-    # )
-    useful_reference = (
-        responsivity.loc[wavelengths]
-        .mul(reference_spectrum.loc["global", wavelengths].values)
-        .sum()
-        / reference_spectrum.loc["global", wavelengths].sum()
-    )
-    # useful irradiance (time-varying)
-    # useful_irradiance = responsivity.loc[wavelengths].mul(irradiance[wavelengths]).sum(
-    #     axis=1
-    # ) / irradiance[wavelengths].sum(axis=1)
+    responsivity_selected = responsivity.sel(center_wavelength=common_wavelengths)
+    logger.info(
+            f"Selected responsivity data : {responsivity_selected}"
+            )
 
-    # get irradiance over specific wavelengths once and reuse
-    irradiance_selected = irradiance.sel(center_wavelength=wavelengths)
+    reference_spectrum_selected = reference_spectrum.loc["global", common_wavelengths]
+    logger.info(
+            f"Selected reference spectrum data : {reference_spectrum_selected}"
+            )
+
+    useful_reference = (
+        responsivity_selected * reference_spectrum_selected
+    ).sum() / reference_spectrum_selected.sum()
+    logger.info(
+        f"Useful fraction of reference spectrum : {useful_reference}"
+    )
+
+    # useful irradiance (time-varying)
+    irradiance_selected = irradiance.sel(center_wavelength=common_wavelengths)
+    logger.info(
+        f"Selected irradiance data : {irradiance_selected}"
+    )
+    sum_of_responsivity_by_irradiance = (
+        responsivity_selected * irradiance_selected
+    ).sum(dim="center_wavelength")
+    logger.info(
+            f"Sum of responsivity * irradiance : {sum_of_responsivity_by_irradiance}"
+            )
+    sum_of_irradiance = (
+        irradiance_selected
+    ).sum(dim="center_wavelength")
+    logger.info(
+            f"Sum of irradiance : {sum_of_irradiance}"
+            )
+
     useful_irradiance = (
-        responsivity.loc[wavelengths].values
-        * irradiance_selected
-    ).sum(dim="center_wavelength") / irradiance_selected.sum(
-        dim="center_wavelength"
+        responsivity_selected * irradiance_selected
+    ).sum(dim="center_wavelength") / irradiance_selected.sum(dim='center_wavelength')
+    logger.info(
+        f"Useful fraction of irradiance : {useful_irradiance}"
     )
 
     spectral_mismatch = useful_irradiance / useful_reference
 
-    # return DataFrame(spectral_mismatch, index=irradiance.index, columns=["Spectral Mismatch"])
+    logger.info(
+            f"Spectral mismatch = {spectral_mismatch}"
+            )
     return spectral_mismatch.to_numpy()
