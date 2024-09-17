79216bbc3ac0da8c3e84844ce5b79b4d106e1cbd
Nikos Alexandris <nikosalexandris@duck.com>
2024-09-17 16:44:47 +0300

    refactor(algorithms): Further work on Mihaylov's spectral mismatch factor
    Refers to #213

--- pvgisprototype/algorithms/mihaylov/spectral_mismatch.py
+++ pvgisprototype/algorithms/mihaylov/spectral_mismatch.py
@@ -1,7 +1,7 @@
 from pvgisprototype.api.irradiance.kato_bands import KATO_BANDS
 from pvgisprototype.log import logger
 from xarray import DataArray
-from numpy import nan_to_num, concatenate, array
+from numpy import nan_to_num, concatenate, array, clip, intersect1d, inf
 from pandas import DataFrame
 import matplotlib.pyplot as plt
 from scipy.integrate import simpson
@@ -22,10 +22,6 @@ def is_kato_banded(spectral_data: DataArray, wavelengths: str) -> bool:
         True if the irradiance data appears to be Kato-banded, False otherwise.
     """
     from numpy import allclose
-
-    from devtools import debug
-    debug(locals())
-
     kato_bands_center_wavelengths = list(KATO_BANDS['Center [nm]'].values())
     if spectral_data.coords[wavelengths].size == len(
         kato_bands_center_wavelengths
@@ -37,62 +33,24 @@ def is_kato_banded(spectral_data: DataArray, wavelengths: str) -> bool:
     return False
 
 
-def integrate_reference_spectrum(reference_spectrum, kato_wavelengths):
-    """
-    Vectorized integration of reference spectrum over Kato bands.
-    
-    Parameters
-    ----------
-    reference_spectrum : xarray.DataArray
-        The reference spectrum as a DataArray with wavelength and global irradiance values.
-    kato_wavelengths : np.ndarray
-        Array of Kato center wavelengths.
-
-    Returns
-    -------
-    integrated_spectrum : np.ndarray
-        Reference spectrum integrated over Kato bands.
-    """
-    # Calculate band edges from center wavelengths
-    band_edges = (kato_wavelengths[:-1] + kato_wavelengths[1:]) / 2.0
-    band_edges = concatenate([[kato_wavelengths[0] - (band_edges[0] - kato_wavelengths[0])], band_edges])
-    band_edges = concatenate([band_edges, [kato_wavelengths[-1] + (kato_wavelengths[-1] - band_edges[-1])]])
-
-    # Extract wavelength and irradiance values
-    wavelengths = reference_spectrum.wavelength.values
-    irradiance_values = reference_spectrum.values
-
-    # Vectorized integration over each band
-    integrated_spectrum = array([
-        simpson(
-            y=irradiance_values[(wavelengths >= lower_edge) & (wavelengths <= upper_edge)],
-            x=wavelengths[(wavelengths >= lower_edge) & (wavelengths <= upper_edge)]
-        ) if len(irradiance_values[(wavelengths >= lower_edge) & (wavelengths <= upper_edge)]) > 0 else 0
-        for lower_edge, upper_edge in zip(band_edges[:-1], band_edges[1:])
-    ])
-
-    return integrated_spectrum
-
-
 def calculate_spectral_mismatch_factor_mihaylow(
     irradiance,
     responsivity,
     reference_spectrum,
 ):
     """ """
+
+    # Preparatory steps --- Push Me Upstream ?
+
+    ## Prepare Reference Spectrum
+
     reference_spectrum = reference_spectrum.to_xarray()
     logger.info(
-        f'`reference_spectrum` input :\n{reference_spectrum}'
-    )
-    logger.info(
-        f'`reference_spectrum` wavelengths :\n{reference_spectrum.wavelength}'
+        f'Reference spectrum input :\n{reference_spectrum}',
+        alt=f'[bold]Reference spectrum input[/bold] :\n{reference_spectrum}'
     )
 
-    # Prepare Responsivity ---------------------------------------------------
-
-    logger.info(
-        f'`responsivity` input :\n{responsivity}',
-    )
+    ## Prepare Responsivity
 
     # Push me upstream ? -----------------------------------------------------
     if 'Center [nm]' in responsivity.coords:
@@ -101,85 +59,127 @@ def calculate_spectral_mismatch_factor_mihaylow(
         responsivity = responsivity.rename({'Wavelength': 'wavelength'})
 
     logger.info(
-        f'Wavelengths in `responsivity` input : {responsivity.wavelength}',
+        f'Responsivity input :\n{responsivity}',
+        alt=f'[bold]Responsivity[/bold] input :\n{responsivity}',
     )
     # ------------------------------------------------------------------------
 
-    # Interpolate Responsivity to Reference Spectrum
+    ## Restrict interpolation to common range
+    common_wavelengths = intersect1d(
+        irradiance.center_wavelength.values,
+        intersect1d(responsivity.wavelength,
+                          reference_spectrum.wavelength),
+    )
+    logger.info(
+            f"Intersection of wavelengths across input data : {common_wavelengths}"
+            )
+
+    responsivity = responsivity.sel(wavelength=common_wavelengths)
+    reference_spectrum = reference_spectrum.sel(wavelength=common_wavelengths)
+    irradiance = irradiance.sel(center_wavelength=common_wavelengths)
+    logger.info(
+        f"Input data limited to common wavelengths:"
+        f"Responsivity :\n{responsivity}"
+        f"Reference spectrum :\n{reference_spectrum}"
+        f"Irradiance :\n{irradiance}"
+    )
+
+    # Responsivity -----------------------------------------------------------
+
+
     if not is_kato_banded(
             spectral_data=responsivity,
             wavelengths='wavelength',
             ):
-
         logger.warning(
                 f'Responsivity is not Kato-banded !',
+                alt=f'[bold yellow]Responsivity is not Kato-banded ![/bold yellow]'
                 )
-
+        ## Interpolate Responsivity to Reference Spectrum
         from scipy.interpolate import pchip_interpolate
         responsivity = pchip_interpolate(
             xi=responsivity.wavelength,
             yi=responsivity,
-            x=reference_spectrum.wavelength,
+            x=common_wavelengths,
         )
-        responsivity = nan_to_num(responsivity)
-
+        # responsivity = nan_to_num(responsivity)
+        responsivity = clip(responsivity, 0, inf)
         logger.info(
                 f"Interpolated responsivity :\n{responsivity}",
                 alt=f"[yellow]Interpolated[/yellow] responsivity :\n{responsivity}",
                 )
+
     # Reference Spectrum -----------------------------------------------------
 
     # Total Reference Spectrum
     total_reference_spectrum = simpson(
-        y=reference_spectrum['global'], x=reference_spectrum.wavelength
+        y=reference_spectrum['global'],
+        # x=reference_spectrum.wavelength
+        x=common_wavelengths
+    )
+    logger.info(
+        f"Total Reference Spectrum :\n{total_reference_spectrum}",
+        alt=f"[bold]Total Reference Spectrum[/bold] :\n{total_reference_spectrum}"
     )
 
     # Useful Reference Spectrum
     effective_reference_spectrum = reference_spectrum * responsivity
+    logger.info(
+            f"Effective Reference Spectrum :\n{effective_reference_spectrum}",
+            alt=f"[bold]Effective Reference Spectrum[/bold] :\n{effective_reference_spectrum}"
+            )
     useful_reference_spectrum = simpson(
         y=effective_reference_spectrum['global'],
-        x=reference_spectrum.wavelength,
+        # x=reference_spectrum.wavelength
+        x=common_wavelengths
     )
+    logger.info(
+            f"Useful reference spectrum :\n{useful_reference_spectrum}",
+            alt=f"[bold][yellow]Useful[/yellow] reference spectrum[/bold] :\n{useful_reference_spectrum}"
+            )
 
     # Observed Irradiance ----------------------------------------------------
-
-    logger.info(
-            f'`irradiance` input :\n{irradiance}'
-            )
 
     # Interpolate Observed Irradiance to Reference Spectrum
     if is_kato_banded(
         spectral_data=irradiance, wavelengths='center_wavelength'
     ):
         irradiance = irradiance.interp(
-            center_wavelength=reference_spectrum.wavelength,  # target
+            # center_wavelength=reference_spectrum.wavelength,  # target
+            center_wavelength=common_wavelengths,  # target
             method="linear",
         )
-        print()
-        print(f"Irradiance : {irradiance}")
-        print()
-    logger.info(
-            f"Interpolated iradiance :\n{irradiance}",
-            alt=f"[yellow]Interpolated[/yellow] irradiance :\n{irradiance}",
-            )
+        logger.info(
+                f"Interpolated iradiance :\n{irradiance}",
+                alt=f"[bold][yellow]Interpolated[/yellow] irradiance[/bold] :\n{irradiance}",
+                )
 
     # Total Observed Irradiance
     total_observed_irradiance = simpson(
         y=irradiance,
-        x=reference_spectrum.wavelength,
+        # x=reference_spectrum.wavelength
+        x=common_wavelengths
     )
+    logger.info(
+            f"Total Observed Irradiance :\n{total_observed_irradiance}",
+            alt=f"[bold]Total Observed Irradiance[/bold] :\n{total_observed_irradiance}"
+            )
 
     # Useful Observed Irradiance
     effective_observed_irradiance = irradiance * responsivity
+    logger.info(
+            f"Effective Observed Irradiance :\n{effective_reference_spectrum}",
+            alt=f"[bold]Effective Observed Irradiance[/bold] :\n{effective_reference_spectrum}"
+            )
     useful_observed_irradiance = simpson(
         y=effective_observed_irradiance,
-        x=reference_spectrum.wavelength,
+        # x=reference_spectrum.wavelength
+        x=common_wavelengths
     )
-
-    logger.info(f"Total reference spectrum: {total_reference_spectrum}")
-    logger.info(f"Useful reference spectrum: {useful_reference_spectrum}")
-    logger.info(f"Total observed irradiance: {total_observed_irradiance}")
-    logger.info(f"Useful observed irradiance: {useful_observed_irradiance}")
+    logger.info(
+            f"Useful observed irradiance: {useful_observed_irradiance}",
+            alt=f"[bold][yellow]Useful[/yellow] observed irradiance[/bold] :\n{useful_observed_irradiance}"
+            )
 
     # Spectral Mismatch Factor
 
