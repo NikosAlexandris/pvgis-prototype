3c667c37a4c28c06a9b8123d8b62090e14b0b01b
falanal <Alexandros.FALANGAS@ext.ec.europa.eu>
2024-09-25 12:58:56 +0200

    perf: Refactor caching and add support for custom dataclasses
    - Add new LFU cache as default method for caching
    - Remove convertion of custom objects to str in caching
    - Add new __eq__ method to custom dataclasses to handle numpy array equality between these objects for caching
    
    Refers to #72

--- pvgisprototype/caching.py
+++ pvgisprototype/caching.py
@@ -1,66 +1,21 @@
 from functools import wraps
 
-import numpy
-from cachetools import cached
+from cachetools import cached, LFUCache
 from cachetools.keys import hashkey
 from pandas import DatetimeIndex, Index
-
-from pvgisprototype import (
-    LinkeTurbidityFactor,
-    OpticalAirMass,
-    RefractedSolarAltitude,
-    SolarAltitude,
-    SolarHourAngle,
-    SurfaceOrientation,
-    SurfaceTilt,
-)
+from numpy import ndarray
 
 
 def generate_custom_hashkey(*args, **kwargs):
-    args = tuple(
-        (
-            # tuple(arg.tolist()) if isinstance(arg, numpy.ndarray)
-            # tuple(str(id(arg))) if isinstance(arg, numpy.ndarray)
-            # else str(arg)
-            str(arg)
-            if isinstance(
-                arg,
-                (
-                    numpy.ndarray,
-                    DatetimeIndex,
-                    LinkeTurbidityFactor,
-                    OpticalAirMass,
-                    RefractedSolarAltitude,
-                    SolarAltitude,
-                    Index,
-                    SolarHourAngle,
-                    SurfaceOrientation,
-                    SurfaceTilt,
-                ),
-            )
-            else arg
-        )
-        for arg in args
-    )
     kwargs = {
         k: (
-            # tuple(v.tolist()) if isinstance(v, numpy.ndarray)
-            # tuple(str(id(v))) if isinstance(v, numpy.ndarray)
-            # else str(v)
             str(v)
             if isinstance(
                 v,
                 (
-                    numpy.ndarray,
+                    ndarray,
                     DatetimeIndex,
-                    LinkeTurbidityFactor,
-                    OpticalAirMass,
-                    RefractedSolarAltitude,
-                    SolarAltitude,
                     Index,
-                    SolarHourAngle,
-                    SurfaceOrientation,
-                    SurfaceTilt,
                 ),
             )
             else v
@@ -72,7 +27,7 @@ def generate_custom_hashkey(*args, **kwargs):
 
 def custom_cached(func):
     @wraps(func)
-    @cached(cache={}, key=generate_custom_hashkey)
+    @cached(cache=LFUCache(maxsize=100), key=generate_custom_hashkey) # NOTE Least Frequently Used eviction policy, useful when some results are used repeatedly
     def wrapper(*args, **kwargs):
         return func(*args, **kwargs)
 
