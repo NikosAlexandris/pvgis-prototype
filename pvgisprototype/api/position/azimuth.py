edfcbaeb3c66b2e83c32f914391a27730d5916c8
Nikos Alexandris <nikosalexandris@duck.com>
2024-10-06 19:26:59 +0300

    refactor!: Restructure the package, again.
    Refers to #230

--- pvgisprototype/api/position/azimuth.py
+++ pvgisprototype/api/position/azimuth.py
@@ -62,7 +62,7 @@ from pvgisprototype.constants import (
     VALIDATE_OUTPUT_DEFAULT,
 )
 from pvgisprototype.log import log_function_call, logger
-from pvgisprototype.core.validation.functions import (
+from pvgisprototype.validation.functions import (
     ModelSolarAzimuthTimeSeriesInputModel,
     validate_with_pydantic,
 )
--- pvgisprototype/api/position/overview.py
+++ pvgisprototype/api/position/overview.py
@@ -108,7 +108,7 @@ from pvgisprototype.constants import (
     VALIDATE_OUTPUT_DEFAULT,
 )
 from pvgisprototype.log import log_function_call, logger
-from pvgisprototype.core.validation.functions import (
+from pvgisprototype.validation.functions import (
     ModelSolarPositionOverviewSeriesInputModel,
     validate_with_pydantic,
 )
