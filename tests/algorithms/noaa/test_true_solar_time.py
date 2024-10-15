54c28ace52bc132d4bcf2c8b1aa6c5f1a640ce68
Nikos Alexandris <nikosalexandris@duck.com>
2024-10-15 16:19:05 +0200

    test: Update the test configuration for algorithms
    Update :
    - algorithms/conftest.py
    - class name
    - terminology
    
    Refers to #184

--- tests/algorithms/noaa/test_true_solar_time.py
+++ tests/algorithms/noaa/test_true_solar_time.py
@@ -4,15 +4,15 @@ from pvgisprototype.algorithms.noaa.solar_time import calculate_true_solar_time_
 from .cases.true_solar_time import cases_true_solar_time_noaa
 from .cases.true_solar_time import cases_true_solar_time_ids
 
-from ..conftest import GenericCheckCustomObjects
+from ..conftest import ValidateDataModel
 
 
-class TestCalculateTrueSolarTimeNOAA(GenericCheckCustomObjects):
+class TestCalculateTrueSolarTimeNOAA(ValidateDataModel):
 
     @pytest.fixture(params=cases_true_solar_time_noaa, ids=cases_true_solar_time_ids)
     def cases(self, request):
         return request.param
 
     @pytest.fixture
-    def operation(self):
+    def function(self):
         return calculate_true_solar_time_series_noaa
