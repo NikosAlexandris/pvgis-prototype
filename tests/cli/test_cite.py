ab952e74dc07a3663d547885da172eb0ab3c29b2
Nikos Alexandris <nikosalexandris@duck.com>
2024-10-16 07:27:08 +0200

    test(cli): Add ValidateTyperApplication, update CLI prototype testing
    Refers to #184

--- tests/cli/test_cite.py
+++ tests/cli/test_cite.py
@@ -1,27 +1,19 @@
-from rich import print
+import pytest
 from typer.testing import CliRunner
+from .conftest import ValidateTyperApplication
 from pvgisprototype.cli.cli import app
 
 runner = CliRunner()
+arguments = (["cite"], ["cite", "--help"], ["cite", "--bibtex"])
 
 
-def test_cite():
-    result = runner.invoke(app, ["cite"])
-    assert result.exit_code == 0  # Ensure the command ran successfully
-    assert "PVGIS" in result.output  # Check for a key part of the citation
-    print(f"Result : {result.output}")  # Optional: Print the result for debugging
+class TestCliApp(ValidateTyperApplication):
 
-def test_cite_bibtex():
-    result = runner.invoke(app, ["cite", "--bibtex"])
-    assert result.exit_code == 0
-    assert "@misc{pvgis" in result.output
-    assert "title" in result.output
-    assert "subtitle" in result.output
-    assert "version" in result.output
-    assert "author" in result.output
-    assert "howpublished" in result.output
-    assert "note" in result.output
-    assert "institution" in result.output
-    assert "address" in result.output
-    assert "year" in result.output
-    print(f"Result : {result.output}")  # Optional: Print the result for debugging
+    # override default app !
+    @pytest.fixture
+    def app(self):
+        return app
+
+    @pytest.fixture(params=arguments)  # , ids=something)
+    def arguments(self, request):
+        return request.param
