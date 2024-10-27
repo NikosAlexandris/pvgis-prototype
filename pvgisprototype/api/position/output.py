3bfbe30310297f79a12b1a0f1af0229192860d8b
Nikos Alexandris <nikosalexandris@duck.com>
2024-10-27 08:56:09 +0100

    refactor!: Update the position overview to consider shading !
    Add and use :
    - the ShadingModel
    - two input parameters : horizon_height, shading_model.  Perhaps the "height" here should be "profile" ?
    - generate_dictionary_of_surface_in_shade_series() in api/position/output.py.
    - SHADE_COLUMN_NAME and SHADING_ALGORITHM_COLUMN_NAME in the output dictionary.
    
    Refers to #248

---
+++ pvgisprototype/api/position/output.py
@@ -0,0 +1,31 @@
+from pvgisprototype.constants import (
+    BEHIND_HORIZON_NAME,
+    HORIZON_HEIGHT_NAME,
+    NOT_AVAILABLE,
+    SHADING_ALGORITHM_NAME,
+    VISIBLE_NAME,
+)
+from pvgisprototype import LocationShading
+from pvgisprototype.log import log_function_call
+
+
+@log_function_call
+def generate_dictionary_of_surface_in_shade_series(
+        surface_in_shade_series: LocationShading,
+        angle_output_units,
+    ):
+    """ """
+    return {
+        HORIZON_HEIGHT_NAME: (
+            surface_in_shade_series.horizon_height
+            if surface_in_shade_series
+            else NOT_AVAILABLE
+        ),
+        BEHIND_HORIZON_NAME: (
+            surface_in_shade_series.value if surface_in_shade_series else NOT_AVAILABLE
+        ),
+        VISIBLE_NAME: (
+            ~surface_in_shade_series.value if surface_in_shade_series else NOT_AVAILABLE
+        ),
+        SHADING_ALGORITHM_NAME: surface_in_shade_series.shading_algorithm,
+    }
