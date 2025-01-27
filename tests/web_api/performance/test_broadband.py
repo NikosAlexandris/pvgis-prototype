cdbf79faaa9a2067e56029e4b0823ef3ff0f528a
falanal <Alexandros.FALANGAS@ext.ec.europa.eu>
2025-01-27 10:32:10 +0100

    test(web-api): Update tests for performance broadband endpoint
    - Use `ValidateWebAPI`, a generic class to be shared for testing all endpoints.
    - Add a generator for test cases specific to this web API endpoint.
    
    Refers to #312

--- tests/web_api/performance/test_broadband.py
+++ tests/web_api/performance/test_broadband.py
@@ -1,123 +1,14 @@
-import random
-from datetime import datetime, timedelta
-from zoneinfo import ZoneInfo
-
 import pytest
-from fastapi.testclient import TestClient
-from pandas import Timestamp
-from pytz import NonExistentTimeError
 
-from pvgisprototype.api.performance.models import PhotovoltaicModulePerformanceModel
-from pvgisprototype.api.position.models import ShadingModel
-from pvgisprototype.api.power.photovoltaic_module import PhotovoltaicModuleModel
-from pvgisprototype.api.quick_response_code import QuickResponseCode
-from pvgisprototype.web_api.schemas import AnalysisLevel, AngleOutputUnit, Timezone
-from pvgisprototype.webapi import app
+from ..conftest import ValidateWebAPI
+from ...generators.web_api.generate_cases_performance_broadband import generate_cases_solar_performance_broadband
 
-random.seed(22227)
-NUMBER_OF_TESTS = 20000
-
-client = TestClient(app)
-
-# Define the ranges and choices for random sampling
-LONGITUDE = [8.628]
-LATITUDE = [45.812]
-ELEVATION_RANGE = (200, 250)
-SURFACE_ORIENTATION_RANGE = (0, 360)
-SURFACE_TILT_RANGE = (0, 180)
-DATE_RANGE_START = datetime(2005, 1, 1)
-DATE_RANGE_END = datetime(2020, 12, 31)
-TIMEZONES = [timezone.value for timezone in Timezone]
-HORIZON_PROFILE_CHOICES = ["PVGIS", "80,80,80,80", "0,0,0,0", "10,20,30,40"]
-SHADING_MODELS = [shading_model for shading_model in ShadingModel]
-NOT_IMPLEMENTED_SHADING_MODELS = [ShadingModel.all, ShadingModel.pvlib]
-PHOTOVOLTAIC_MODULE_MODELS = [module.value for module in PhotovoltaicModuleModel]
-SYSTEM_EFFICIENCY_RANGE = (0, 1)
-POWER_MODELS = [model.value for model in PhotovoltaicModulePerformanceModel]
-PEAK_POWER_RANGE = (0, 10)
-ANGLE_OUTPUT_UNITS = [unit.value for unit in AngleOutputUnit]
-ANALYSIS_LEVELS = [level.value for level in AnalysisLevel]
-CSV_CHOICES = [None, "test"]
-BOOLEAN_CHOICES = [True, False]
-VERBOSE_RANGE = (0, 9)
-
-
-def validate_time(datetime_object, timezone):
-    """Check if a datetime is valid in the given timezone."""
-    try:
-        Timestamp(datetime_object, tz=ZoneInfo(timezone))  # Force timezone localization
-        return True
-    except NonExistentTimeError:
-        return False
-
-
-def generate_random_date_pair(start, end):
-    """Generate a random start_date and end_date such that start_date < end_date."""
-    start_date = start + timedelta(days=random.randint(0, (end - start).days))
-
-    # Ensure there is at least one valid day for the end_date
-    if start_date >= end:
-        return start_date, start_date + timedelta(days=1)
-
-    end_date = start_date + timedelta(days=random.randint(1, (end - start_date).days))
-    return start_date, end_date
-
-
-parameters_list = []
-for _ in range(NUMBER_OF_TESTS):
+class TestSolarPositionOverview(ValidateWebAPI):
     
-    expected_status_code = 200
-
-    start_date, end_date = generate_random_date_pair(DATE_RANGE_START, DATE_RANGE_END)
-    timezone = random.choice(TIMEZONES)
-    if not validate_time(start_date, timezone) or not validate_time(end_date, timezone):
-        expected_status_code = 400
-
-    shading_model = random.choice(SHADING_MODELS)
-    if shading_model in NOT_IMPLEMENTED_SHADING_MODELS:
-        expected_status_code = 400
-        
-    parameters_list.append(
-        {
-            "longitude": random.choice(LONGITUDE),
-            "latitude": random.choice(LATITUDE),
-            "elevation": random.randint(*ELEVATION_RANGE),
-            "surface_orientation": random.uniform(*SURFACE_ORIENTATION_RANGE),
-            "surface_tilt": random.uniform(*SURFACE_TILT_RANGE),
-            "start_time": start_date,
-            "end_time": end_date,
-            "timezone": timezone,
-            "horizon_profile": random.choice(HORIZON_PROFILE_CHOICES),
-            "shading_model": shading_model.value,
-            "photovoltaic_module": random.choice(PHOTOVOLTAIC_MODULE_MODELS),
-            "system_efficiency": random.uniform(*SYSTEM_EFFICIENCY_RANGE),
-            "power_model": random.choice(POWER_MODELS),
-            "peak_power": random.randint(*PEAK_POWER_RANGE),
-            "angle_output_units": random.choice(ANGLE_OUTPUT_UNITS),
-            "analysis": random.choice(ANALYSIS_LEVELS),
-            "csv": random.choice(CSV_CHOICES),
-            "verbose": random.randint(*VERBOSE_RANGE),
-            "index": random.choice(BOOLEAN_CHOICES),
-            "quiet": random.choice(BOOLEAN_CHOICES),
-            "fingerprint": random.choice(BOOLEAN_CHOICES),
-            "quick_response_code": random.choice(
-                [code.value for code in QuickResponseCode]
-            ),
-            "metadata": random.choice(BOOLEAN_CHOICES),
-            "expected_status_code": expected_status_code,
-        }
-    )
-
-
-@pytest.mark.parametrize("parameters", parameters_list)
-def test_status(parameters):
-    expected_status_code = parameters.pop("expected_status_code")
-    print("-----------------------")
-    print(f"{parameters=}")
-    response = client.get(
-        "/performance/broadband",
-        params=parameters,
-    )
-    print(f"{response.text=}")
-    assert response.status_code == expected_status_code
-    print("-----------------------")
+    @pytest.fixture(autouse=True)
+    def endpoint(self):
+        self.endpoint="/performance/broadband"
+    
+    @pytest.fixture(params=generate_cases_solar_performance_broadband())
+    def cases(self, request):
+        return request.param
