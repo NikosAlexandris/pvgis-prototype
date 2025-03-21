8aafe0dbae973b97d6f008d8fa0b49fb348b7c6c
falanal <Alexandros.FALANGAS@ext.ec.europa.eu>
2025-03-21 11:11:10 +0100

    fix(api): Remove unused input arguments from ground reflected inclined calculation
    - Following update 628a044b, the arguments `neighbor_lookup`, `tolerance`, `mask_and_scale`, and `in_memory` were removed from the ground reflected inclined calculation in the API.
    - This commit cleans up the corresponding unused parameters.

--- pvgisprototype/api/power/broadband.py
+++ pvgisprototype/api/power/broadband.py
@@ -611,10 +611,6 @@ def calculate_photovoltaic_power_output_series(
                     timestamps=timestamps,
                     timezone=timezone,
                     global_horizontal_irradiance=global_horizontal_irradiance,  # optional
-                    neighbor_lookup=neighbor_lookup,
-                    tolerance=tolerance,
-                    mask_and_scale=mask_and_scale,
-                    in_memory=in_memory,
                     linke_turbidity_factor_series=linke_turbidity_factor_series,
                     apply_atmospheric_refraction=apply_atmospheric_refraction,
                     refracted_solar_zenith=refracted_solar_zenith,
