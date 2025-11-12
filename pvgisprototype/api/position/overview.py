e43f6922b79c4dc0256a8c6462c5b716064856cd
Nikos Alexandris <nikosalexandris@duck.com>
2025-11-12 01:21:03 +0100

    fix!: refactor context factory, relevant data model definitions
    This commit unbreaks the -aou degrees functionality.  In a rather easy way.

--- pvgisprototype/api/position/overview.py
+++ pvgisprototype/api/position/overview.py
@@ -241,7 +241,7 @@ def model_solar_position_overview_series(
                 verbose=verbose,
                 log=log,
         )
-    solar_event_type_series = solar_event_series.event
+    solar_event_type_series = solar_event_series.event_type
     solar_event_time_series = solar_event_series.value
 
     if solar_position_model.value == SolarPositionModel.noaa:
@@ -601,7 +601,7 @@ def model_solar_position_overview_series(
         solar_incidence_series if solar_incidence_series is not None else None,
         sun_horizon_position_series if sun_horizon_position_series is not None else None,
         surface_in_shade_series if surface_in_shade_series is not None else None,
-        solar_event_type_series if solar_event_series.event is not None else None,
+        solar_event_type_series if solar_event_series.event_type is not None else None,
         solar_event_time_series if solar_event_series.value is not None else None,
     )
     if verbose > DEBUG_AFTER_THIS_VERBOSITY_LEVEL:
@@ -629,6 +629,7 @@ def calculate_solar_position_overview_series(
     solar_time_model: SolarTimeModel = SolarTimeModel.noaa,
     eccentricity_phase_offset: float = ECCENTRICITY_PHASE_OFFSET,
     eccentricity_amplitude: float = ECCENTRICITY_CORRECTION_FACTOR,
+    angle_output_units: str = RADIANS,
     dtype: str = DATA_TYPE_DEFAULT,
     array_backend: str = ARRAY_BACKEND_DEFAULT,
     validate_output: bool = VALIDATE_OUTPUT_DEFAULT,
@@ -740,7 +741,11 @@ def calculate_solar_position_overview_series(
                 #
                 angle_output_units=solar_incidence_series.unit,
             )
-            solar_position_overview.build_output(verbose=verbose, fingerprint=fingerprint)
+            solar_position_overview.build_output(
+                verbose=verbose,
+                fingerprint=fingerprint,
+                angle_output_units=angle_output_units,
+            )
             results = {
                 solar_position_model.name: solar_position_overview.output
             }
